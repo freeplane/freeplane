cbef9648509529e52e3245cf760c0a94d6f4888b
dpolivaev <>
2009-06-15 14:01:15 +0000

    added quote characters in batch file

--- freeplane_framework/script/freeplane.bat
+++ freeplane_framework/script/freeplane.bat
@@ -5,4 +5,4 @@ if not exist %fwdir% set xargs=init.xargs
 set defines= "-Dorg.freeplane.param1=%1" "-Dorg.freeplane.param2=%2" "-Dorg.freeplane.param3=%3" "-Dorg.freeplane.param4=%4"
 set freeplanedir=%~dp0
 @echo on
-java "-Dorg.osgi.framework.dir=%fwdir%" "-Dorg.knopflerfish.gosg.jars=reference:file:%freeplanedir%plugins/" %defines% -jar %~dp0\framework.jar -xargs %freeplanedir%props.xargs -xargs %freeplanedir%%xargs%
+java "-Dorg.osgi.framework.dir=%fwdir%" "-Dorg.knopflerfish.gosg.jars=reference:file:%freeplanedir%plugins/" %defines% -jar "%~dp0\framework.jar" -xargs "%freeplanedir%props.xargs" -xargs "%freeplanedir%%xargs%"
