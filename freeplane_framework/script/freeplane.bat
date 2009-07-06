d38b4ef150f4042ecc969cf09a3079b8c8b2b849
dpolivaev <>
2009-07-06 05:01:31 +0000

    fix in batch file

--- freeplane_framework/script/freeplane.bat
+++ freeplane_framework/script/freeplane.bat
@@ -1,8 +1,8 @@
 @echo off
-set fwdir=%~dp0\fwdir
+set freeplanedir=%~dp0
+set fwdir=%freeplanedir%fwdir
 set xargs=restart.xargs
 if not exist %fwdir% set xargs=init.xargs
 set defines= "-Dorg.freeplane.param1=%1" "-Dorg.freeplane.param2=%2" "-Dorg.freeplane.param3=%3" "-Dorg.freeplane.param4=%4"
-set freeplanedir=%~dp0
 @echo on
-java "-Dorg.osgi.framework.dir=%fwdir%" "-Dorg.knopflerfish.gosg.jars=reference:file:%freeplanedir%plugins/" %defines% -jar "%~dp0\framework.jar" -xargs "%freeplanedir%props.xargs" -xargs "%freeplanedir%%xargs%"
+java "-Dorg.osgi.framework.dir=%fwdir%" "-Dorg.knopflerfish.gosg.jars=reference:file:%freeplanedir%plugins/" %defines% -jar "%freeplanedir%framework.jar" -xargs "%freeplanedir%props.xargs" -xargs "%freeplanedir%%xargs%"
