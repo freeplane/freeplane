b064f530589f0f6d79a88b3d28cb40d1da575bab
dpolivaev <>
2009-07-20 17:25:41 +0000

    automatic bug report can be initiated by user by pressing "ALT+SHIFT+CONTROL b" if Freeplane does not respond

--- freeplane_plugin_bugreport/src/org/freeplane/plugin/bugreport/Activator.java
+++ freeplane_plugin_bugreport/src/org/freeplane/plugin/bugreport/Activator.java
@@ -1,7 +1,15 @@
 package org.freeplane.plugin.bugreport;
 
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.KeyEvent;
 import java.util.logging.Logger;
 
+import javax.swing.JOptionPane;
+
+import org.freeplane.core.resources.ResourceBundles;
+import org.freeplane.core.ui.components.UITools;
+import org.freeplane.core.util.LogTool;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
@@ -18,6 +26,18 @@ public class Activator implements BundleActivator {
 		parentLogger = Logger.getAnonymousLogger().getParent();
 		handler = new XmlRpcHandler();
 		parentLogger.addHandler(handler);
+		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher(){
+
+			public boolean dispatchKeyEvent(KeyEvent e) {
+				if(e.getID() == KeyEvent.KEY_PRESSED && e.isAltDown() && e.isAltDown() && e.isShiftDown() && e.getKeyCode() == KeyEvent.VK_B){
+					final RuntimeException runtimeException = new RuntimeException(ResourceBundles.getText("user_initiated_bug_report"));
+					LogTool.severe(runtimeException);
+				}
+				return false;
+            }
+			
+		});
+
 	}
 
 	/*
