6e1d4f26fe899619f77d030e096721e8162dba98
dpolivaev <>
2009-08-09 10:48:46 +0000

    paint edge like links only if both target and source are not filtered out,

--- freeplane/src/org/freeplane/view/swing/map/MapView.java
+++ freeplane/src/org/freeplane/view/swing/map/MapView.java
@@ -954,17 +954,15 @@ public class MapView extends JPanel implements Printable, Autoscroll, IMapChange
 				final ArrowLinkModel ref = (ArrowLinkModel) next;
 				if (alreadyPaintedLinks.add(ref)) {
 					final NodeModel target = ref.getTarget();
-					if (target == null || ! target.isVisible()) {
+					if (target == null) {
 						continue;
 					}
 					final NodeModel source = ref.getSource();
-					if (! source.isVisible()) {
-						continue;
-					}
 					final NodeView sourceView = getNodeView(source);
 					final NodeView targetView = getNodeView(target);
 					final ILinkView arrowLink;
-					if(ref.isEdgeLike() && sourceView != null && targetView != null){
+					if(ref.isEdgeLike() && sourceView != null && targetView != null
+							&& source.isVisible() && target.isVisible()){
 						arrowLink = new EdgeLinkView(ref, sourceView, targetView);
 					}
 					else{
