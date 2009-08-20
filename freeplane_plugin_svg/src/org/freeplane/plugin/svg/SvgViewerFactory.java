888a2985430b224eafd6ec6f7c8944a3c9bbbf04
dpolivaev <>
2009-08-20 13:03:06 +0000

    batik 1.7

--- freeplane_plugin_svg/src/org/freeplane/plugin/svg/SvgViewerFactory.java
+++ freeplane_plugin_svg/src/org/freeplane/plugin/svg/SvgViewerFactory.java
@@ -1,36 +1,17 @@
 package org.freeplane.plugin.svg;
 
-import java.awt.Color;
-import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.Dimension2D;
 import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import javax.swing.JComponent;
 import javax.swing.SwingUtilities;
-import javax.swing.border.MatteBorder;
 import javax.swing.filechooser.FileFilter;
 
-import org.apache.batik.dom.svg.SAXSVGDocumentFactory;
 import org.apache.batik.swing.JSVGCanvas;
 import org.apache.batik.swing.gvt.GVTTreeRendererAdapter;
 import org.apache.batik.swing.gvt.GVTTreeRendererEvent;
-import org.apache.batik.swing.gvt.GVTTreeRendererListener;
-import org.apache.batik.swing.svg.GVTTreeBuilderAdapter;
-import org.apache.batik.swing.svg.GVTTreeBuilderEvent;
-import org.apache.batik.swing.svg.JSVGComponent;
-import org.apache.batik.swing.svg.SVGDocumentLoaderAdapter;
-import org.apache.batik.swing.svg.SVGDocumentLoaderEvent;
-import org.apache.batik.util.XMLResourceDescriptor;
 import org.freeplane.view.swing.addins.filepreview.IPreviewComponentFactory;
 import org.freeplane.view.swing.map.MapView;
-import org.w3c.dom.Document;
-import org.w3c.dom.svg.SVGDocument;
-import org.w3c.dom.svg.SVGSVGElement;
 
 public class SvgViewerFactory implements IPreviewComponentFactory {
 
