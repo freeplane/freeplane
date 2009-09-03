a3398b4869b8a1bbf307623089891230a08c4db4
dpolivaev <>
2009-09-03 20:49:46 +0000

    bug fix for opening file URLs with spaces

--- freeplane/src/org/freeplane/main/application/ApplicationViewController.java
+++ freeplane/src/org/freeplane/main/application/ApplicationViewController.java
@@ -266,10 +266,6 @@ class ApplicationViewController extends ViewController {
 	 */
 	@Override
 	public void openDocument(final URL url) throws Exception {
-		String correctedUrl = url.toExternalForm();
-		if (url.getProtocol().equals("file")) {
-			correctedUrl = correctedUrl.replace('\\', '/').replaceAll(" ", "%20");
-		}
 		final String osName = System.getProperty("os.name");
 		if (osName.substring(0, 3).equals("Win")) {
 			String propertyString = "default_browser_command_windows";
@@ -279,23 +275,24 @@ class ApplicationViewController extends ViewController {
 			else {
 				propertyString += "_nt";
 			}
-			String command = null;
+			String[] command = null;
 			try {
 				final Object[] messageArguments = { url.toString() };
 				final MessageFormat formatter = new MessageFormat(ResourceController.getResourceController()
 				    .getProperty(propertyString));
 				String browserCommand = formatter.format(messageArguments);
 				if (url.getProtocol().equals("file")) {
-					command = "rundll32 url.dll,FileProtocolHandler " + url.toString();
+					command = new String[]{"rundll32", "url.dll,FileProtocolHandler", url.toString()};
 					if (System.getProperty("os.name").startsWith("Windows 2000")) {
-						command = "rundll32 shell32.dll,ShellExec_RunDLL " + url.toString();
+						command = new String[]{"rundll32", "shell32.dll,ShellExec_RunDLL",  url.toString()};
 					}
 				}
 				else if (url.toString().startsWith("mailto:")) {
-					command = "rundll32 url.dll,FileProtocolHandler " + url.toString();
+					command = new String[]{"rundll32", "url.dll,FileProtocolHandler", url.toString()};
 				}
 				else {
-					command = browserCommand;
+					Runtime.getRuntime().exec(browserCommand);
+					return;
 				}
 				Runtime.getRuntime().exec(command);
 			}
@@ -312,6 +309,10 @@ class ApplicationViewController extends ViewController {
 		else if (osName.startsWith("Mac OS")) {
 			String browserCommand = null;
 			try {
+				String correctedUrl = url.toExternalForm();
+				if (url.getProtocol().equals("file")) {
+					correctedUrl = correctedUrl.replace('\\', '/').replaceAll(" ", "%20");
+				}
 				final Object[] messageArguments = { correctedUrl, url.toString() };
 				final MessageFormat formatter = new MessageFormat(ResourceController.getResourceController()
 				    .getProperty("default_browser_command_mac"));
@@ -329,6 +330,10 @@ class ApplicationViewController extends ViewController {
 		else {
 			String browserCommand = null;
 			try {
+				String correctedUrl = url.toExternalForm();
+				if (url.getProtocol().equals("file")) {
+					correctedUrl = correctedUrl.replace('\\', '/').replaceAll(" ", "%20");
+				}
 				final Object[] messageArguments = { correctedUrl, url.toString() };
 				final MessageFormat formatter = new MessageFormat(ResourceController.getResourceController()
 				    .getProperty("default_browser_command_other_os"));
