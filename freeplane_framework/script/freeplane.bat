1314909b544286801e530b04e6ca79d989b85726
dpolivaev <>
2009-09-20 17:20:14 +0000

    Use up to 3/4 of available memory for windows OS (512m for batch) Update of Knopflerfish to 2.3.3

--- freeplane_framework/script/freeplane.bat
+++ freeplane_framework/script/freeplane.bat
@@ -3,4 +3,4 @@ set freeplanedir=%~dp0
 set xargs=init.xargs
 set defines= "-Dorg.freeplane.param1=%1" "-Dorg.freeplane.param2=%2" "-Dorg.freeplane.param3=%3" "-Dorg.freeplane.param4=%4"
 @echo on
-java "-Dorg.knopflerfish.framework.bundlestorage=memory" "-Dorg.knopflerfish.gosg.jars=reference:file:%freeplanedir%plugins/" %defines% -jar "%freeplanedir%framework.jar" -xargs "%freeplanedir%props.xargs" -xargs "%freeplanedir%%xargs%"
+java -Xmx512m "-Dorg.knopflerfish.framework.bundlestorage=memory" "-Dorg.knopflerfish.gosg.jars=reference:file:%freeplanedir%plugins/" %defines% -jar "%freeplanedir%framework.jar" -xargs "%freeplanedir%props.xargs" -xargs "%freeplanedir%%xargs%"
