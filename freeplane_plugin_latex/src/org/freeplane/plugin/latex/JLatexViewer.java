3d9120e3feb0e45229cdd05e6c22223619b6ad9b
dpolivaev <>
2009-12-27 14:05:36 +0100

    JLatexMath repelces HotEqn

---
+++ freeplane_plugin_latex/src/org/freeplane/plugin/latex/JLatexViewer.java
@@ -0,0 +1,122 @@
+package org.freeplane.plugin.latex;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+import org.freeplane.core.resources.ResourceBundles;
+import org.freeplane.view.swing.map.MapView;
+import org.scilab.forge.jlatexmath.ParseException;
+import org.scilab.forge.jlatexmath.TeXConstants;
+import org.scilab.forge.jlatexmath.TeXFormula;
+
+class JLatexViewer extends JComponent {
+	private static final int DEFAULT_FONT_SIZE = 16;
+	static String editorTitle = null;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private float zoom = 0f;
+	final private LatexNodeHook latexController;
+	private LatexExtension model;
+	private TeXFormula teXFormula; 
+
+	JLatexViewer(final LatexNodeHook latexController, final LatexExtension latexExtension) {
+		this.latexController = latexController;
+		setBorder( BorderFactory.createLineBorder(Color.BLACK));
+		setModel(latexExtension);
+		if (JLatexViewer.editorTitle == null) {
+			JLatexViewer.editorTitle = ResourceBundles.getText("plugins/latex/LatexNodeHook.editorTitle");
+		}
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(final MouseEvent e) {
+				if (e.getButton() == MouseEvent.BUTTON1) {
+					edit();
+					e.consume();
+					return;
+				}
+			}
+		});
+	}
+
+	private void edit() {
+		final JTextArea textArea = new JTextArea(model.getEquation());
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		final JScrollPane editorScrollPane = new JScrollPane(textArea);
+		editorScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		editorScrollPane.setPreferredSize(new Dimension(500, 160));
+		final JDialog edit = new JDialog(JOptionPane.getFrameForComponent(this), JLatexViewer.editorTitle, true);
+		edit.getContentPane().add(editorScrollPane);
+		edit.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		edit.pack();
+		edit.setLocationRelativeTo(this);
+		edit.setVisible(true);
+		final String eq = textArea.getText();
+		latexController.setEquationUndoable(model, eq);
+	}
+
+	private void calculateSize() {
+		MapView mapView = (MapView) SwingUtilities.getAncestorOfClass(MapView.class, this);
+		final float mapZoom = mapView.getZoom();
+		if(mapZoom == zoom){
+			return;
+		}
+		this.zoom = mapZoom;
+		Icon latexIcon = teXFormula.createTeXIcon(TeXConstants.STYLE_DISPLAY, DEFAULT_FONT_SIZE * zoom);
+		final Insets insets = getInsets();
+		final Dimension dimension = new Dimension (latexIcon.getIconWidth() + insets.left + insets.right, 
+			latexIcon.getIconHeight() + insets.top + insets.bottom);
+		setPreferredSize(dimension);
+    }
+	
+	@Override
+	public void paint(final Graphics g) {
+		Icon latexIcon = teXFormula.createTeXIcon(TeXConstants.STYLE_DISPLAY, DEFAULT_FONT_SIZE * zoom);
+		final Insets insets = getInsets();
+		latexIcon.paintIcon(this, g, insets.left, insets.top);
+		super.paint(g);
+	}
+
+	public void setModel(final LatexExtension latexExtension) {
+		model = latexExtension;
+		try {
+	        teXFormula = new TeXFormula(model.getEquation());
+        }
+        catch (ParseException e) {
+			try {
+		        teXFormula = new TeXFormula("\\mbox{" +e.getMessage() +"}");
+	        }
+	        catch (ParseException e1) {
+		        teXFormula = new TeXFormula("\\mbox{Can not parse given equation}");
+	        }
+        }
+        zoom = 0;
+		revalidate();
+		repaint();
+	}
+
+	@Override
+    public Dimension getPreferredSize() {
+		calculateSize();
+	    return super.getPreferredSize();
+    }
+	
+	
+}
