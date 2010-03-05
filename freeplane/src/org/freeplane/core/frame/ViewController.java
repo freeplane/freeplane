ddb2967af2e5089139505c5de7ec667fe36ee061
dpolivaev@users.sourceforge.net <>
2010-03-05 21:07:12 +0100

    bug fix for full screen under linux

--- freeplane/src/org/freeplane/core/frame/ViewController.java
+++ freeplane/src/org/freeplane/core/frame/ViewController.java
@@ -31,6 +31,7 @@ import java.awt.Frame;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
+import java.awt.Toolkit;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ItemEvent;
@@ -732,6 +733,8 @@ abstract public class ViewController implements IMapViewChangeListener, IFreepla
 			winState = frame.getExtendedState();
 			frame.dispose();
 			frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+			Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+			frame.setBounds(0, 0, screenSize.width, screenSize.height);
 			frame.setUndecorated(true);
 			frame.setResizable(false);
 			getFreeplaneMenuBar().setVisible(isMenubarVisible());
