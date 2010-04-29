4181224e318b41eb649763714b706a542c74b95c
dpolivaev@users.sourceforge.net <>
2010-04-29 09:00:42 +0200

    -

--- freeplane/src/org/freeplane/main/applet/AppletResourceController.java
+++ freeplane/src/org/freeplane/main/applet/AppletResourceController.java
@@ -52,7 +52,6 @@ class AppletResourceController extends ResourceController {
 		catch (final Exception ex) {
 			System.err.println("Could not load properties.");
 		}
-		userProps.setProperty("browsemode_initial_map", "map.mm");
 		final Enumeration allKeys = userProps.propertyNames();
 		while (allKeys.hasMoreElements()) {
 			final String key = (String) allKeys.nextElement();
