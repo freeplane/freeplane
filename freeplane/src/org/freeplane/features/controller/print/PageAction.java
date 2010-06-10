6fa024ac4037b1e4de98e9ab06cf0caf7b0b51a3
dpolivaev <>
2010-06-10 22:06:37 +0200

    Refactoring and bug fix in PrintController: 662]: Printing all works wrongly after printing defined page(s) (windows java bug)

--- freeplane/src/org/freeplane/features/controller/print/PageAction.java
+++ freeplane/src/org/freeplane/features/controller/print/PageAction.java
@@ -27,7 +27,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.awt.print.PageFormat;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -165,15 +164,6 @@ class PageAction extends AbstractPrintAction {
 			return;
 		}
 		final PrintController printController = getPrintController();
-		printController.setPageFormat(printController.getPrinterJob().pageDialog(printController.getPageFormat()));
-		if (printController.getPageFormat().getOrientation() == PageFormat.LANDSCAPE) {
-			ResourceController.getResourceController().setProperty("page_orientation", "landscape");
-		}
-		else if (printController.getPageFormat().getOrientation() == PageFormat.PORTRAIT) {
-			ResourceController.getResourceController().setProperty("page_orientation", "portrait");
-		}
-		else if (printController.getPageFormat().getOrientation() == PageFormat.REVERSE_LANDSCAPE) {
-			ResourceController.getResourceController().setProperty("page_orientation", "reverse_landscape");
-		}
+		printController.pageDialog();
 	}
 }
--- freeplane/src/org/freeplane/features/controller/print/PrintController.java
+++ freeplane/src/org/freeplane/features/controller/print/PrintController.java
@@ -20,12 +20,13 @@
 package org.freeplane.features.controller.print;
 
 import java.awt.print.PageFormat;
+import java.awt.print.Printable;
+import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
 
-import org.apache.commons.lang.StringUtils;
 import org.freeplane.core.controller.Controller;
 import org.freeplane.core.extension.IExtension;
-import org.freeplane.core.resources.ResourceController;
+import org.freeplane.core.util.Compat;
 
 /**
  * @author Dimitry Polivaev
@@ -63,7 +64,7 @@ public class PrintController implements IExtension {
 	}
 
 	boolean acquirePrinterJobAndPageFormat() {
-		if (printerJob == null) {
+		if (printerJob == null || Compat.isWindowsOS()) {
 			try {
 				printerJob = PrinterJob.getPrinterJob();
 			}
@@ -76,21 +77,6 @@ public class PrintController implements IExtension {
 				return false;
 			}
 		}
-		if (pageFormat == null) {
-			pageFormat = printerJob.defaultPage();
-			if (StringUtils.equals(ResourceController.getResourceController().getProperty("page_orientation"),
-			    "landscape")) {
-				pageFormat.setOrientation(PageFormat.LANDSCAPE);
-			}
-			else if (StringUtils.equals(ResourceController.getResourceController().getProperty("page_orientation"),
-			    "portrait")) {
-				pageFormat.setOrientation(PageFormat.PORTRAIT);
-			}
-			else if (StringUtils.equals(ResourceController.getResourceController().getProperty("page_orientation"),
-			    "reverse_landscape")) {
-				pageFormat.setOrientation(PageFormat.REVERSE_LANDSCAPE);
-			}
-		}
 		return true;
 	}
 
@@ -98,11 +84,14 @@ public class PrintController implements IExtension {
 		return controller;
 	}
 
-	public PageFormat getPageFormat() {
+	PageFormat getPageFormat() {
+		if (pageFormat == null) {
+			pageFormat = printerJob.defaultPage();
+		}
 		return pageFormat;
 	}
 
-	PrinterJob getPrinterJob() {
+	private PrinterJob getPrinterJob() {
 		return printerJob;
 	}
 
@@ -110,11 +99,21 @@ public class PrintController implements IExtension {
 		return printingAllowed;
 	}
 
-	void setPageFormat(final PageFormat pageFormat) {
-		this.pageFormat = pageFormat;
+	public void pageDialog() {
+		this.pageFormat = getPrinterJob().pageDialog(getPageFormat());	    
+    }
+
+	public boolean printDialog() {
+	    return getPrinterJob().printDialog();
 	}
 
-	void setPrinterJob(final PrinterJob printerJob) {
-		this.printerJob = printerJob;
+	public void print(Printable mapView, boolean showDlg) throws PrinterException {
+		if (!acquirePrinterJobAndPageFormat()) {
+			return;
+		}
+		getPrinterJob().setPrintable(mapView, getPageFormat());
+		if(! showDlg || printDialog()){
+			getPrinterJob().print();
+		}
 	}
 }
