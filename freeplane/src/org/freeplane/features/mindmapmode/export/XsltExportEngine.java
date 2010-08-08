46698d0bac3792c4455006284828cca46bff235d
dpolivaev@users.sourceforge.net <>
2010-08-08 15:37:50 +0200

    -

--- freeplane/src/org/freeplane/features/mindmapmode/export/XsltExportEngine.java
+++ freeplane/src/org/freeplane/features/mindmapmode/export/XsltExportEngine.java
@@ -1,14 +1,20 @@
 package org.freeplane.features.mindmapmode.export;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 
+import javax.xml.transform.Result;
 import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
 import org.freeplane.core.controller.Controller;
+import org.freeplane.core.util.LogUtils;
 import org.freeplane.features.common.map.MapModel;
 import org.freeplane.features.common.map.ModeController;
 import org.freeplane.features.common.map.MapWriter.Mode;
@@ -22,9 +28,28 @@ public class XsltExportEngine implements IExportEngine {
 
 	public void export(MapModel map, File toFile) {
 		final Source xsltSource = new StreamSource(xsltFile);
-		final XmlExporter xe = new XmlExporter();
 		final Source xmlSource = getMapXml(map);
-		xe.transform(xmlSource, xsltSource, toFile);
+		FileOutputStream outputStream = null;
+        try {
+        	outputStream = new FileOutputStream(toFile);
+        	final Result result = new StreamResult(outputStream);
+        	final TransformerFactory transFact = TransformerFactory.newInstance();
+        	final Transformer trans = transFact.newTransformer(xsltSource);
+        	trans.transform(xmlSource, result);
+        }
+        catch (final Exception e) {
+        	LogUtils.severe(e);
+        }
+        finally {
+        	try {
+        		if (outputStream != null) {
+        			outputStream.close();
+        		}
+        	}
+        	catch (final IOException e) {
+        		e.printStackTrace();
+        	}
+        }
 	}
 	/**
 	 * @param mode 
