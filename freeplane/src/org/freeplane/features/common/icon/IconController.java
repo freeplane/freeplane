b70a4d9e3fc970c6f14a1980a2c1e115a823857b
dpolivaev@users.sourceforge.net <>
2010-09-04 12:43:01 +0200

    Display icons from default style

--- freeplane/src/org/freeplane/features/common/icon/IconController.java
+++ freeplane/src/org/freeplane/features/common/icon/IconController.java
@@ -89,8 +89,11 @@ public class IconController implements IExtension {
 		addIconGetter(IPropertyHandler.STYLE, new IPropertyHandler<List<MindIcon>, NodeModel>() {
 			public List<MindIcon> getProperty(final NodeModel node, final List<MindIcon> currentValue) {
 				final MapStyleModel model = MapStyleModel.getExtension(node.getMap());
-				final NodeModel styleNode = model.getStyleNode(LogicalStyleController.getController(modeController).getStyle(node));
+				NodeModel styleNode = model.getStyleNode(LogicalStyleController.getController(modeController).getStyle(node));
 				final List<MindIcon> styleIcons;
+				if(styleNode == null){
+					styleNode = model.getStyleNode(MapStyleModel.DEFAULT_STYLE);
+				}
 				if (styleNode == null || styleNode.equals(node)) {
 					styleIcons = Collections.emptyList();
 				}
