820ea5b469b55d88c971cda6f246da572d4853a8
dpolivaev@users.sourceforge.net <>
2010-12-09 22:58:33 +0100

    860]: Export Java Applet 1.2.1_30 not working : use org.apache.commons.lang by reflection so that applet can work without it

--- freeplane/src/org/freeplane/features/common/filter/condition/ASelectableCondition.java
+++ freeplane/src/org/freeplane/features/common/filter/condition/ASelectableCondition.java
@@ -1,15 +1,27 @@
 package org.freeplane.features.common.filter.condition;
 
+import java.lang.reflect.Method;
+
 import javax.swing.JComponent;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.freeplane.n3.nanoxml.XMLElement;
 
 
 public abstract class ASelectableCondition  implements ICondition{
 	transient private String description;
 	transient private JComponent renderer;
+	private static Method EQUALS;
+	private static Method HASH;
+	static{
+		try{
+			final ClassLoader classLoader = ASelectableCondition.class.getClassLoader();
+			EQUALS = classLoader.loadClass("org.apache.commons.lang.builder.EqualsBuilder").getMethod("reflectionEquals", Object.class, Object.class);
+			HASH = classLoader.loadClass("org.apache.commons.lang.builder.HashCodeBuilder").getMethod("reflectionHashCode", Object.class);
+		}
+		catch(Exception e){
+			
+		}
+	}
 
 	public ASelectableCondition() {
 		super();
@@ -18,12 +30,30 @@ public abstract class ASelectableCondition  implements ICondition{
 
 	@Override
     public int hashCode() {
-		return HashCodeBuilder.reflectionHashCode(this);
+		if(HASH == null){
+			return super.hashCode();
+		}
+		try {
+	        return (Integer) HASH.invoke(null, this);
+        }
+        catch (Exception e) {
+	        e.printStackTrace();
+	        return super.hashCode();
+        }
     }
 
 	@Override
     public boolean equals(Object obj) {
-		return EqualsBuilder.reflectionEquals(this, obj);
+		if(EQUALS == null){
+			return super.equals(obj);
+		}
+		try {
+	        return (Boolean) EQUALS.invoke(null, this, obj);
+        }
+        catch (Exception e) {
+	        e.printStackTrace();
+	        return super.equals(obj);
+        }
     }
 	protected abstract String createDesctiption();
 	
