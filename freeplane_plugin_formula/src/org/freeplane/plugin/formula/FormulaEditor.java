06c98da589f751c869d2aa63105d351498375692
dpolivaev@users.sourceforge.net <>
2011-03-03 11:24:41 +0100

    refactoring: extract GlassPaneManager from FormulaEditor

--- freeplane_plugin_formula/src/org/freeplane/plugin/formula/FormulaEditor.java
+++ freeplane_plugin_formula/src/org/freeplane/plugin/formula/FormulaEditor.java
@@ -19,143 +19,22 @@
  */
 package org.freeplane.plugin.formula;
 
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Point;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
-import javax.swing.JScrollBar;
-import javax.swing.SwingUtilities;
-import javax.swing.event.AncestorEvent;
-import javax.swing.event.AncestorListener;
 
-import org.freeplane.core.controller.Controller;
-import org.freeplane.core.ui.DelayedMouseListener;
-import org.freeplane.core.ui.IMouseListener;
-import org.freeplane.features.common.map.MapController;
+import org.freeplane.core.ui.GlassPaneManager;
+import org.freeplane.core.ui.INodeSelector;
 import org.freeplane.features.common.map.NodeModel;
 import org.freeplane.features.mindmapmode.text.EditNodeDialog;
-import org.freeplane.view.swing.map.MainView;
-import org.freeplane.view.swing.map.MapView;
 
 /**
  * @author Dimitry Polivaev
  * Nov 20, 2010
  */
-class FormulaEditor extends EditNodeDialog {
+class FormulaEditor extends EditNodeDialog implements INodeSelector {
 	
-	class MouseListenerImpl extends MouseAdapter implements IMouseListener{
-
-	    public void mouseMoved(MouseEvent e) {
-	    }
-
-	    public void mouseDragged(MouseEvent e) {
-	        final Component component = findMapComponent(e);
-	        if(canRedispatchEventFor(component)){
-	        	redispatchMouseEvent(e, component);
-	        }
-	    }
-
-	    public void mouseClicked(MouseEvent e) {
-	    	if(e.getButton() != 1){
-	    		return;
-	    	}
-	        final Component component = findMapComponent(e);
-	        if(! (component instanceof MainView)){
-	        	return;
-	        }
-	        MainView mainView = (MainView) component;
-	        final NodeModel model = mainView.getNodeView().getModel();
-	        switch(e.getClickCount()){
-	        	case 1:
-	        		final MapController mapController = Controller.getCurrentModeController().getMapController();
-					mapController.setFolded(model, ! model.isFolded());
-	        		break;
-	        	case 2:
-					final String id = model.getID();
-	    	        textEditor.replaceSelection(id);
-	    	        textEditor.requestFocus();
-	        		break;
-	        }
-	    }
-
-		public boolean canRedispatchEventFor(final Component component) {
-	        if (component instanceof MapView)
-	        	return true;
-	        if (component instanceof JScrollBar || SwingUtilities.getAncestorOfClass(JScrollBar.class, component) != null)
-	        	return true;
-	        return false;
-        }
-
-	    public void mouseEntered(MouseEvent e) {
-	    }
-
-	    public void mouseExited(MouseEvent e) {
-	    }
-
-	    public void mousePressed(MouseEvent e) {
-	    	if(e.getButton() != 1){
-	    		return;
-	    	}
-	        final Component component = findMapComponent(e);
-	        if(canRedispatchEventFor(component)){
-	        	redispatchMouseEvent(e, component);
-	        }
-	    }
-
-	    public void mouseReleased(MouseEvent e) {
-	    	if(e.getButton() != 1){
-	    		return;
-	    	}
-	        final Component component = findMapComponent(e);
-	        if(canRedispatchEventFor(component)){
-	        	redispatchMouseEvent(e, component);
-	        }
-	    }
-
-	    //A basic implementation of redispatching events.
-	    private Component findMapComponent(MouseEvent e) {
-	    	final Component glassPane = e.getComponent();
-	    	final Point glassPanePoint = e.getPoint();
-	    	final Container container = jframe.getRootPane().getContentPane();
-	    	Point containerPoint = SwingUtilities.convertPoint(
-	    		glassPane,
-	    		glassPanePoint,
-	    		container);
-	    	Component component = 
-	    		SwingUtilities.getDeepestComponentAt(
-	    			container,
-	    			containerPoint.x,
-	    			containerPoint.y);
-	    	if(component instanceof MainView){
-		    	return component;
-	    	}
-	    	return SwingUtilities.getAncestorOfClass(MapView.class, component);
-	    }
-		    public void redispatchMouseEvent(MouseEvent e, final Component component) {
-	        final Component glassPane = e.getComponent();
-	        final Point glassPanePoint = e.getPoint();
-	        Point componentPoint = SwingUtilities.convertPoint(
-	        	glassPane,
-	        	glassPanePoint,
-	        	component);
-	        component.dispatchEvent(new MouseEvent(component,
-	        	e.getID(),
-	        	e.getWhen(),
-	        	e.getModifiers(),
-	        	componentPoint.x,
-	        	componentPoint.y,
-	        	e.getClickCount(),
-	        	e.isPopupTrigger()));
-        }
-
-	}
-
-    private JFrame jframe;
 	private JEditorPane textEditor;
 
 	FormulaEditor(NodeModel nodeModel, String text, KeyEvent firstEvent, IEditControl editControl,
@@ -163,36 +42,17 @@ class FormulaEditor extends EditNodeDialog {
 	    super(nodeModel, text, firstEvent, editControl, enableSplit, textEditor);
 	    super.setModal(false);
 	    this.textEditor = textEditor;
-	    final IMouseListener mouseListener = new DelayedMouseListener(new MouseListenerImpl(), 2, 1);
-	    textEditor.addAncestorListener(new AncestorListener() {
-
-			public void ancestorRemoved(AncestorEvent event) {
-				jframe.getJMenuBar().setEnabled(true);
-				final Component glassPane = jframe.getRootPane().getGlassPane();
-				glassPane.removeMouseListener(mouseListener);
-				glassPane.removeMouseMotionListener(mouseListener);
-				glassPane.setVisible(false);
-				jframe.setFocusableWindowState(true);
-			}
-			
-			public void ancestorMoved(AncestorEvent event) {
-			}
-			
-			public void ancestorAdded(AncestorEvent event) {
-				jframe.getJMenuBar().setEnabled(false);
-				final Component glassPane = jframe.getRootPane().getGlassPane();
-				glassPane.addMouseListener(mouseListener);
-				glassPane.addMouseMotionListener(mouseListener);
-				glassPane.setVisible(true);
-				jframe.setFocusableWindowState(false);
-			}
-		});
-	    
     }
 
 	@Override
     public void show(JFrame frame) {
-		jframe = (JFrame) frame;
+	    textEditor.addAncestorListener(new GlassPaneManager(frame.getRootPane(), this));
 	    super.show(frame);
+    }
+
+	public void nodeSelected(final NodeModel model) {
+		final String id = model.getID();
+		textEditor.replaceSelection(id);
+	    textEditor.requestFocus();
     }
 }
