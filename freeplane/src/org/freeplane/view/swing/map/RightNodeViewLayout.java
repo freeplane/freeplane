68baef1c0c735960bb119ba168f4eacd09c4a5f5
dpolivaev@users.sourceforge.net <>
2011-04-11 19:17:57 +0200

    right layout: inline some methods

--- freeplane/src/org/freeplane/view/swing/map/RightNodeViewLayout.java
+++ freeplane/src/org/freeplane/view/swing/map/RightNodeViewLayout.java
@@ -48,9 +48,83 @@ public class RightNodeViewLayout extends NodeViewLayoutAdapter {
 
 	@Override
 	protected void layout() {
-		final int contentHeight = getChildContentHeight(false);
-		int childVerticalShift = getChildVerticalShift(false);
-		final int childHorizontalShift = getChildHorizontalShift();
+		final int contentHeight;
+		{
+			final int childCount = getChildCount();
+			if (childCount == 0) {
+				contentHeight = 0;
+			}
+			else{
+			int height = 0;
+			int count = 0;
+			for (int i = 0; i < childCount; i++) {
+				final NodeView child = (NodeView) getView().getComponent(i);
+				if (child.isLeft() == false) {
+					final int additionalCloudHeigth = getAdditionalCloudHeigth(child);
+					final int h = child.getContent().getHeight();
+					height += h + additionalCloudHeigth;
+					if (child.getHeight() - 2 * getSpaceAround() != 0) {
+						count++;
+					}
+				}
+			}
+			if (count <= 1) {
+				contentHeight = height;
+			}
+			else{
+				contentHeight = height + getVGap() * (count - 1);
+			}
+			}
+			
+		}
+		int childVerticalShift;
+		{
+			if (getChildCount() == 0) {
+				childVerticalShift = 0;
+			}
+			else{
+			int shift = 0;
+			for (int i = 0; i < getChildCount(); i++) {
+				final NodeView child = (NodeView) getView().getComponent(i);
+				if (child.isLeft() == false) {
+					final int childShift = child.getShift();
+					if (childShift < 0 || i == 0) {
+						shift += childShift;
+					}
+					final int contentShift = child.getContent().getY() - getSpaceAround();
+					shift -= contentShift;
+				}
+			}
+			childVerticalShift = shift;
+			}
+		}
+		final int childHorizontalShift;
+		{
+			if (getChildCount() == 0) {
+				childHorizontalShift = 0;
+			}
+			else{
+				int shift = 0;
+				for (int i = 0; i < getChildCount(); i++) {
+					final NodeView child = (NodeView) getView().getComponent(i);
+					int shiftCandidate;
+					if (child.isLeft()) {
+						shiftCandidate = -child.getContent().getX() - child.getContent().getWidth();
+						if (child.isContentVisible()) {
+							shiftCandidate -= child.getHGap() + getAdditionalCloudHeigth(child) / 2;
+						}
+					}
+					else {
+						shiftCandidate = -child.getContent().getX();
+						if (child.isContentVisible()) {
+							shiftCandidate += child.getHGap();
+						}
+					}
+					shift = Math.min(shift, shiftCandidate);
+				}
+				childHorizontalShift = shift;
+			}
+		}
 		if (getView().isContentVisible()) {
 			getContent().setVisible(true);
 			final Dimension contentPreferredSize = getContent().getPreferredSize();
@@ -66,7 +140,48 @@ public class RightNodeViewLayout extends NodeViewLayoutAdapter {
 			final int y = getSpaceAround() + Math.max(0, -childVerticalShift);
 			getContent().setBounds(x, y, 0, contentHeight);
 		}
-		placeRightChildren(childVerticalShift);
+		final int[] lx = new int[getChildCount()];
+		final int[] ly = new int[getChildCount()];
+		final int baseX = getContent().getX() + getContent().getWidth();
+		int y = getContent().getY() - getSpaceAround() + childVerticalShift;
+		int right = baseX + getSpaceAround();;
+		NodeView child = null;
+		for (int i = 0; i < getChildCount(); i++) {
+			final NodeView component = (NodeView) getView().getComponent(i);
+			if (component.isLeft()) {
+				continue;
+			}
+			child = component;
+			final int additionalCloudHeigth = getAdditionalCloudHeigth(child) / 2;
+			y += additionalCloudHeigth;
+			final int shiftY = child.getShift();
+			final int childHGap = child.getContent().isVisible() ? child.getHGap() : 0;
+			final int x = baseX + childHGap - child.getContent().getX();
+			if(i == 0){
+				child.setLocation(x, y);
+			}
+			else if (shiftY < 0) {
+				child.setLocation(x, y);
+				y -= shiftY;
+			}
+			else {
+				y += shiftY;
+				child.setLocation(x, y);
+			}
+			final int childHeight = child.getHeight() - 2 * getSpaceAround();
+			if (childHeight != 0) {
+				y += childHeight + getVGap() + additionalCloudHeigth;
+			}
+			right = Math.max(right, x + child.getWidth() + additionalCloudHeigth);
+		}
+		final int bottom = getContent().getY() + getContent().getHeight() + getSpaceAround();
+		if (child != null) {
+			getView().setSize(right,
+			    Math.max(bottom, child.getY() + child.getHeight() + getAdditionalCloudHeigth(child) / 2));
+		}
+		else {
+			getView().setSize(right, bottom);
+		}
 	}
 
 	public void layoutNodeMotionListenerView(final NodeMotionListenerView view) {
