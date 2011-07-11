833a9bbf59bf9f451f314d10e3d39081e17dc850
Stefan Langer <langer@docear.org>
2011-07-11 11:14:58 +0200

    initial

---
+++ docear_plugin_core/src/org/docear/plugin/core/CoreConfiguration.java
@@ -0,0 +1,50 @@
+package org.docear.plugin.core;
+
+import java.net.URL;
+
+import org.freeplane.core.resources.ResourceBundles;
+import org.freeplane.core.resources.ResourceController;
+import org.freeplane.core.util.LogUtils;
+import org.freeplane.features.mode.Controller;
+import org.freeplane.features.mode.ModeController;
+import org.freeplane.features.mode.mindmapmode.MModeController;
+
+public class CoreConfiguration {
+	public CoreConfiguration(ModeController modeController) {
+		LogUtils.info("org.docear.plugin.core.CoreConfiguration() initializing...");
+		init(modeController);
+	}
+
+	private void init(ModeController modeController) {
+		addPluginDefaults();
+		addPropertiesToOptionPanel();
+	}
+	
+	private void addPluginDefaults() {
+		final URL defaults = this.getClass().getResource(ResourceController.PLUGIN_DEFAULTS_RESOURCE);
+		if (defaults == null)
+			throw new RuntimeException("cannot open " + ResourceController.PLUGIN_DEFAULTS_RESOURCE);
+		Controller.getCurrentController().getResourceController().addDefaults(defaults);
+	}
+	
+	private void addPropertiesToOptionPanel() {
+		final URL preferences = this.getClass().getResource("preferences.xml");
+		if (preferences == null)
+			throw new RuntimeException("cannot open preferences");
+		MModeController modeController = (MModeController) Controller.getCurrentModeController();
+		
+		ResourceBundles resBundle = ((ResourceBundles)modeController.getController().getResourceController().getResources());
+		
+		String lang = resBundle.getLanguageCode();
+		if (lang == null || lang.equals(ResourceBundles.LANGUAGE_AUTOMATIC)) {
+			lang = "en";
+		}
+		
+		final URL res = this.getClass().getResource("/translations/Resources_"+lang+".properties");
+		resBundle.addResources(resBundle.getLanguageCode(), res);		
+		modeController.getOptionPanelBuilder().load(preferences);
+
+	}
+}
+
+
