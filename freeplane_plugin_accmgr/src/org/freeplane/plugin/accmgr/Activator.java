e19c552eff5df87b334b32059615ff5935275fe6
Genzmehr <genzmehr@docear.org>
2011-07-11 11:31:18 +0200

    initial

---
+++ freeplane_plugin_accmgr/src/org/freeplane/plugin/accmgr/Activator.java
@@ -0,0 +1,24 @@
+package org.freeplane.plugin.accmgr;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Hello World!!");
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Goodbye World!!");
+	}
+
+}
