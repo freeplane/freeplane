29afdb093bfb512bc04c682da4ae28ebe96afcbd
stefan <>
2011-07-11 14:51:20 +0200

    use of Communcations-plugin

--- docear_plugin_backup/src/org/docear/plugin/backup/DocearBackupStarter.java
+++ docear_plugin_backup/src/org/docear/plugin/backup/DocearBackupStarter.java
@@ -6,6 +6,7 @@ import java.util.Properties;
 
 import javax.swing.JOptionPane;
 
+import org.docear.plugin.communications.CommunicationsConfiguration;
 import org.freeplane.core.resources.ResourceController;
 import org.freeplane.core.resources.components.IValidator;
 import org.freeplane.core.resources.components.IValidator.ValidationResult;
@@ -36,7 +37,7 @@ public class DocearBackupStarter implements IMapChangeListener {
 		StringWriter sw = new StringWriter();
 		try {
 			if (Controller.getCurrentModeController().getController().getResourceController().getProperty("docear_save_backup").equals("true")) {
-				validateOptions();
+				CommunicationsConfiguration config = new CommunicationsConfiguration();
 			}
 			System.out.println("docear_save_backup="+Controller.getCurrentModeController().getController().getResourceController().getProperty("docear_save_backup"));
 		} catch (Exception e) {
@@ -45,61 +46,6 @@ public class DocearBackupStarter implements IMapChangeListener {
 		System.out.println(sw.toString());		
 	}
 	
-	private IValidator optionValidator;
-	
-	public void validateOptions() {		
-		this.setOptionValidator(new IValidator() {
-			public ValidationResult validate(Properties properties) {
-				properties.list(System.out);
-				final ValidationResult result = new ValidationResult();
-				
-				final String backup_server_path = properties
-						.getProperty(BackupOptions.RESOURCES_BACKUP_SERVER_PATH);
-				Integer connection_timeout = 0;
-				try {
-					connection_timeout = Integer.parseInt(properties
-							.getProperty(BackupOptions.RESOURCES_CONNECTION_TIMEOUT));
-				}catch(NumberFormatException e) {
-					connection_timeout = 0;
-				}
-				
-				if (backup_server_path == null || backup_server_path.length() == 0) {
-					result.addError("backup_server_url_not_defined");
-				}
-				if (connection_timeout == 0) {
-					result.addWarning("connection_timeout_not_defined");
-				}
-				
-				return result;
-			}
-		});
-		
-		final ResourceController resourceController = Controller.getCurrentController().getResourceController();
-		ValidationResult result = getOptionValidator().validate(resourceController.getProperties());
-		if (result.isValid()) {
-			Backup backup = new Backup();
-			backup.doBackup();
-		}
-		else {
-			String message = TextUtils.getText("critical_error");
-			for (String s : result.getErrors()) {
-				message += "\n"+TextUtils.getText(s);
-			}
-			
-			JOptionPane.showMessageDialog(Controller.getCurrentController().getViewController().getJFrame(), message);
-			LogUtils.info("DOCEAR: hasErrors");
-		}		
-		
-	}
-	
-	private IValidator getOptionValidator() {
-	    return this.optionValidator;
-    }
-
-	private void setOptionValidator(IValidator validator) {
-		this.optionValidator = validator;
-    }
-
 	public void onNodeDeleted(NodeModel parent, NodeModel child, int index) {
 		// TODO Auto-generated method stub
 		LogUtils.info("Docear: onNodeDeleted");
