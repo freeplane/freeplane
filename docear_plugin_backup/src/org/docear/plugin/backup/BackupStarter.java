c720531ebc67d3fc2e66ee9deb0339922ac95ed4
Stefan Langer <langer@docear.org>
2011-07-19 10:46:03 +0200

    run nackups in threads

--- docear_plugin_backup/src/org/docear/plugin/backup/BackupStarter.java
+++ docear_plugin_backup/src/org/docear/plugin/backup/BackupStarter.java
@@ -1,9 +1,11 @@
 package org.docear.plugin.backup;
 
+import java.awt.EventQueue;
 import java.io.StringWriter;
 import java.net.URL;
 
 import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
 
 import org.docear.plugin.communications.CommunicationsConfiguration;
 import org.docear.plugin.communications.Filetransfer;
@@ -23,99 +25,147 @@ import org.freeplane.features.url.mindmapmode.MFileManager;
 import org.freeplane.plugin.accountmanager.Account;
 
 public class BackupStarter implements IMapChangeListener {
-	
+
 	public final static String MINDMAP_ID = "mindmapId";
-	
+
 	private CommunicationsConfiguration config = new CommunicationsConfiguration();
-	
+
 	BackupStarter() {
 		LogUtils.info("starting DocearBackupStarter()");
 		Controller.getCurrentModeController().getMapController().addMapChangeListener(this);
 		addPluginDefaults();
 	}
-	
+
 	private void addPluginDefaults() {
 		final URL defaults = this.getClass().getResource(ResourceController.PLUGIN_DEFAULTS_RESOURCE);
 		if (defaults == null)
 			throw new RuntimeException("cannot open " + ResourceController.PLUGIN_DEFAULTS_RESOURCE);
 		Controller.getCurrentController().getResourceController().addDefaults(defaults);
 	}
-	
+
 	public void mapChanged(MapChangeEvent event) {
 		LogUtils.info("Docear: mapChanged");
 		StringWriter sw = new StringWriter();
-		Controller controller = Controller.getCurrentController();		
-		
+		Controller controller = Controller.getCurrentController();
+
 		try {
 			if (!controller.getResourceController().getProperty("docear_save_backup").equals("true")) {
 				return;
 			}
 			backup();
-						
-			System.out.println("docear_save_backup="+Controller.getCurrentModeController().getController().getResourceController().getProperty("docear_save_backup"));
-		} catch (Exception e) {
+
+			System.out.println("docear_save_backup="
+					+ Controller.getCurrentModeController().getController().getResourceController()
+							.getProperty("docear_save_backup"));
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println(sw.toString());		
+		System.out.println(sw.toString());
 	}
-	
+
 	private void backup() {
 		Account account = config.getAccount();
+		System.out.println("account.hasUsername: " + (account.hasUsername() ? "true" : "false"));
+		System.out.println("account.hasPassword: " + (account.hasPassword() ? "true" : "false"));
+		System.out.println("account.hasConnectionString: " + (account.hasConnectionString() ? "true" : "false"));
+
+		System.out.println(account);
+
 		if (!account.hasUsername() || !account.hasPassword() || !account.hasConnectionString()) {
-			JOptionPane.showMessageDialog(null,
-					TextUtils.getText("account_credentials_invalid"), "error",
+			JOptionPane.showMessageDialog(null, TextUtils.getText("account_credentials_not_found"), "error",
 					JOptionPane.ERROR_MESSAGE);
+			return;
 		}
-		
+
 		Controller controller = Controller.getCurrentController();
-		ModeController modeController = Controller.getCurrentModeController(); 
-		
+		ModeController modeController = Controller.getCurrentModeController();
+
 		final MapStyleModel styleModel = MapStyleModel.getExtension(controller.getMap());
 		String mindmapId = styleModel.getProperty(MINDMAP_ID);
-		
+
 		StringWriter sw = new StringWriter();
-		
+
 		try {
 			modeController.getMapController().getFilteredXml(controller.getMap(), sw, Mode.EXPORT, true);
 			String filename = controller.getMap().getFile().getName();
-			
+
 			String xml = sw.toString();
-			
+
 			if (mindmapId == null || mindmapId.isEmpty()) {
-				System.out.println("org.docear.plugin.backup: inserting new mindmap: "+filename);
+				System.out.println("org.docear.plugin.backup: inserting new mindmap: " + filename);
 				insert(xml, filename);
 			}
 			else {
-				System.out.println("org.docear.plugin.backup: updating mindmap: "+filename+" (ID: "+mindmapId+")");
+				System.out.println("org.docear.plugin.backup: updating mindmap: " + filename + " (ID: " + mindmapId + ")");
 				update(mindmapId, xml, filename);
 			}
-			
-			boolean saved = ((MFileManager) UrlManager.getController())
-					.save(controller.getMap());
-	
+
+			boolean saved = ((MFileManager) UrlManager.getController()).save(controller.getMap());
+
 			System.out.println("debug save: " + (saved ? "true" : "false"));
 		}
-		catch(Exception e) {
+		catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	private void insert(String xml, String filename) {
-		Controller controller = Controller.getCurrentController();
-		ModeController modeController = Controller.getCurrentModeController(); 
-		
-		String mindmap_id = Filetransfer.insertMindmap(config, xml, filename);
-		MapStyle mapStyle = (MapStyle) modeController.getExtension(MapStyle.class);
-		mapStyle.setProperty(controller.getMap(), MINDMAP_ID, mindmap_id);
-		
-		((MFileManager) UrlManager.getController()).save(controller.getMap());
+
+	private void insert(final String xml, final String filename) {
+		final Controller controller = Controller.getCurrentController();
+		final ModeController modeController = Controller.getCurrentModeController();
+
+		Thread runner = new Thread() {
+			public void run() {
+				synchronized (this) {
+					Runnable runnable = new Runnable() {
+						public void run() {
+							String mindmap_id;
+							try {
+								mindmap_id = Filetransfer.insertMindmap(config, xml, filename);
+								MapStyle mapStyle = (MapStyle) modeController.getExtension(MapStyle.class);
+								mapStyle.setProperty(controller.getMap(), MINDMAP_ID, mindmap_id);
+								((MFileManager) UrlManager.getController()).save(controller.getMap());
+							}
+							catch (Exception e) {
+								e.printStackTrace();
+								JOptionPane.showMessageDialog(null, TextUtils.getText("error_backup_insert"), "error",
+										JOptionPane.ERROR_MESSAGE);
+							}
+						}
+					};
+					EventQueue.invokeLater(runnable);
+				}
+			}
+		};
+		runner.start();
 	}
-	
-	private void update(String mindmapId, String xml, String filename) {
-		Filetransfer.updateMindmap(config, xml, mindmapId, filename);
+
+	private void update(final String mindmapId, final String xml, final String filename) {
+		Thread runner = new Thread() {
+			public void run() {
+				synchronized (this) {
+					Runnable runnable = new Runnable() {
+
+						public void run() {
+							try {
+								Filetransfer.updateMindmap(config, xml, mindmapId, filename);
+
+							}
+							catch (Exception e) {
+								e.printStackTrace();
+								JOptionPane.showMessageDialog(null, TextUtils.getText("error_backup_update"), "error",
+										JOptionPane.ERROR_MESSAGE);
+							}						
+						}
+					};
+					SwingUtilities.invokeLater(runnable);
+				}
+			}
+		};
+		runner.start();
 	}
-	
+
 	public void onNodeDeleted(NodeModel parent, NodeModel child, int index) {
 		// TODO Auto-generated method stub
 		LogUtils.info("Docear: onNodeDeleted");
