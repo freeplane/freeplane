fc9d1de10567586c8d368546911259af6f036e87
Marcel Genzmehr <genzmehr@docear.org>
2011-07-20 16:51:03 +0200

    new package

---
+++ freeplane_plugin_workspace/src/org/freeplane/plugin/workspace/controller/NodeSelectionListener.java
@@ -0,0 +1,25 @@
+package org.freeplane.plugin.workspace.controller;
+
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class NodeSelectionListener implements TreeSelectionListener {
+	
+	public void valueChanged(TreeSelectionEvent event) {
+	    DefaultMutableTreeNode node = (DefaultMutableTreeNode)(event.getPath().getLastPathComponent());
+	    if(hasInterface(node.getUserObject())) {
+        	((TreeSelectionListener)node.getUserObject()).valueChanged(event);
+        }
+	}
+	
+	private boolean hasInterface(Object obj) {
+    	Class<?>[] interfaces = obj.getClass().getInterfaces();
+    	for(Class<?> interf : interfaces) {
+    		if(TreeSelectionListener.class.getName().equals(interf.getName())) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+}
