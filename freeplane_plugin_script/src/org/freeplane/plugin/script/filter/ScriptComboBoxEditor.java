45ef31740a0403dcec2675291fec8fa09aaa7b56
dpolivaev <dpolivaev@users.sourceforge.net>
2011-07-23 11:56:47 +0200

    Script based filter conditions

---
+++ freeplane_plugin_script/src/org/freeplane/plugin/script/filter/ScriptComboBoxEditor.java
@@ -0,0 +1,128 @@
+/*
+ *  Freeplane - mind map editor
+ *  Copyright (C) 2009 Dimitry Polivaev
+ *
+ *  This file author is Dimitry Polivaev
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.freeplane.plugin.script.filter;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.ComboBoxEditor;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JEditorPane;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+
+import org.freeplane.core.ui.components.JRestrictedSizeScrollPane;
+import org.freeplane.core.util.TextUtils;
+
+/**
+ * @author Dimitry Polivaev
+ * Mar 5, 2009
+ */
+class ScriptComboBoxEditor implements ComboBoxEditor {
+
+	final private JButton showEditorBtn;
+	final private List<ActionListener> actionListeners;
+	private String script;
+
+	public ScriptComboBoxEditor() {
+		showEditorBtn = new JButton();
+		showEditorBtn.setHorizontalAlignment(SwingConstants.LEFT);
+		showEditorBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				editScript(false);
+			}
+		});
+		final Dimension preferredSize = showEditorBtn.getPreferredSize();
+		preferredSize.width = 100;
+		showEditorBtn.setPreferredSize(preferredSize);
+		actionListeners = new LinkedList<ActionListener>();
+	}
+
+	protected void editScript(boolean selectAll) {
+		JEditorPane textEditor = new JEditorPane();
+		final JRestrictedSizeScrollPane scrollPane = new JRestrictedSizeScrollPane(textEditor);
+		scrollPane.setMinimumSize(new Dimension(100, 60));
+		textEditor.setContentType("text/groovy");
+		textEditor.setText(script);
+		if(selectAll){
+			textEditor.selectAll();
+		}
+		String title = TextUtils.getText("plugins/ScriptEditor/window.title");
+		final JOptionPane optionPane = new JOptionPane(scrollPane, JOptionPane.QUESTION_MESSAGE, JOptionPane.OK_CANCEL_OPTION);
+		final JDialog dialog = optionPane.createDialog(showEditorBtn, title);
+		dialog.setResizable(true);
+		dialog.setVisible(true);
+		final Integer result = ((Integer)optionPane.getValue());
+		if(result != JOptionPane.OK_OPTION)
+			return;
+		script = textEditor.getText();
+		showEditorBtn.setText(script);
+	    final ActionEvent actionEvent = new ActionEvent(this, 0, null);
+	    for (final ActionListener l : actionListeners) {
+	    	l.actionPerformed(actionEvent);
+	    }
+    }
+
+	public Component getEditorComponent() {
+		return showEditorBtn;
+	}
+	public void setItem(Object anObject) {
+		if(anObject == null)
+			script = "";
+		else
+			this.script = (String)anObject;
+		if("".equals(anObject))
+			showEditorBtn.setText(TextUtils.getText("EditFilterAction.text") + "...");
+		else
+			showEditorBtn.setText(script);
+    }
+
+	public Object getItem() {
+	    return script;
+    }
+	public void selectAll() {
+		editScript(true);
+    }
+	public void addActionListener(final ActionListener l) {
+		actionListeners.add(l);
+	}
+
+	public void removeActionListener(final ActionListener l) {
+		actionListeners.remove(l);
+	}
+}
+
+class  ScriptRenderer extends DefaultListCellRenderer{
+    private static final long serialVersionUID = 1L;
+
+	@Override
+    public Dimension getPreferredSize() {
+	    final Dimension preferredSize = super.getPreferredSize();
+	    preferredSize.width = 100;
+		return preferredSize;
+    }
+	
+}
---
+++ freeplane_plugin_script/src/org/freeplane/plugin/script/filter/ScriptConditionController.java
@@ -0,0 +1,119 @@
+/*
+ *  Freeplane - mind map editor
+ *  Copyright (C) 2008 Dimitry Polivaev
+ *
+ *  This file author is Dimitry Polivaev
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.freeplane.plugin.script.filter;
+
+import javax.swing.ComboBoxEditor;
+import javax.swing.ComboBoxModel;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListModel;
+import javax.swing.ListCellRenderer;
+import javax.swing.ListModel;
+
+import org.freeplane.core.resources.NamedObject;
+import org.freeplane.core.util.LogUtils;
+import org.freeplane.core.util.TextUtils;
+import org.freeplane.features.filter.condition.ASelectableCondition;
+import org.freeplane.features.filter.condition.IElementaryConditionController;
+import org.freeplane.n3.nanoxml.XMLElement;
+
+/**
+ * @author Dimitry Polivaev
+ * 21.12.2008
+ */
+public class ScriptConditionController implements IElementaryConditionController {
+	static final String FILTER_SCRIPT = "filter_script";
+	private final ComboBoxEditor editor = new ScriptComboBoxEditor();
+	private final ListCellRenderer renderer = new ScriptRenderer();
+	private final ComboBoxModel values = new DefaultComboBoxModel();
+
+	public ScriptConditionController() {
+		super();
+	}
+
+	public boolean canEditValues(final Object property, final NamedObject simpleCond) {
+		return true;
+	}
+
+	public boolean canHandle(final Object selectedItem) {
+		if (!(selectedItem instanceof NamedObject)) {
+			return false;
+		}
+		final NamedObject namedObject = (NamedObject) selectedItem;
+		return namedObject.objectEquals(ScriptConditionController.FILTER_SCRIPT);
+	}
+
+	public boolean canSelectValues(final Object property, final NamedObject simpleCond) {
+		return true;
+	}
+
+	public ASelectableCondition createCondition(final Object selectedItem, final NamedObject simpleCond,
+	                                            final Object value, final boolean ignoreCase) {
+		if(value == null)
+			return null;
+		final String string = (String) value;
+		if("".equals(string))
+			return null;
+		return new ScriptCondition(string);
+	}
+
+	public ComboBoxModel getConditionsForProperty(final Object property) {
+		return new DefaultComboBoxModel(getScriptConditionNames());
+	}
+
+	public ListModel getFilteredProperties() {
+		final DefaultListModel list = new DefaultListModel();
+		list.addElement(TextUtils.createTranslatedString(FILTER_SCRIPT));
+		return list;
+	}
+
+	public Object[] getScriptConditionNames() {
+		return new NamedObject[] { new NamedObject(ScriptCondition.NAME, " ")};
+	}
+
+	public ComboBoxEditor getValueEditor(Object selectedProperty, NamedObject selectedCondition) {
+		return editor;
+	}
+
+	public ComboBoxModel getValuesForProperty(final Object selectedItem, NamedObject simpleCond) {
+		values.setSelectedItem("");
+		return values;
+	}
+
+	public boolean isCaseDependent(final Object property, final NamedObject simpleCond) {
+		return false;
+	}
+
+	public ASelectableCondition loadCondition(final XMLElement element) {
+		try {
+			if (element.getName().equalsIgnoreCase(ScriptCondition.NAME)) {
+				final String script = element.getAttribute(ScriptCondition.SCRIPT, null);
+				return new ScriptCondition(script);
+			}
+		}
+		catch (final Exception e) {
+			LogUtils.severe(e);
+		}
+		return null;
+	}
+
+	public ListCellRenderer getValueRenderer(Object selectedProperty, NamedObject selectedCondition) {
+	    return renderer;
+    }
+}
