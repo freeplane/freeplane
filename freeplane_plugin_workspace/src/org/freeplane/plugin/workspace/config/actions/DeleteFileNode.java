859956a8e07bb87d6cc5181c9f870b69cc2050d5
Stefan Langer <langer@docear.org>
2011-07-28 16:57:23 +0200

    delete confirmation dialog

--- freeplane_plugin_workspace/src/org/freeplane/plugin/workspace/config/actions/DeleteFileNode.java
+++ freeplane_plugin_workspace/src/org/freeplane/plugin/workspace/config/actions/DeleteFileNode.java
@@ -2,11 +2,15 @@ package org.freeplane.plugin.workspace.config.actions;
 
 import java.awt.event.ActionEvent;
 
+import javax.swing.JOptionPane;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 
 import org.freeplane.core.ui.components.JFreeplaneMenuItem;
+import org.freeplane.core.util.TextUtils;
+import org.freeplane.features.mode.Controller;
+import org.freeplane.plugin.workspace.WorkspacePreferences;
 import org.freeplane.plugin.workspace.io.node.DefaultFileNode;
 import org.freeplane.plugin.workspace.view.WorkspacePopupMenu;
 
@@ -20,20 +24,30 @@ public class DeleteFileNode extends AWorkspaceAction {
 	public DeleteFileNode() {
 		super("DeleteFileNode");
 	}
-	
-	public void actionPerformed(final ActionEvent e) {
-        System.out.println("DeleteFileNode: "+e.getActionCommand()+" : "+e.getID());
-        DefaultMutableTreeNode node = this.getNodeFromActionEvent(e);
-        if (node.getUserObject() instanceof DefaultFileNode) {
-        	((DefaultFileNode) node.getUserObject()).delete();
-        }
-        
-        WorkspacePopupMenu menu = (WorkspacePopupMenu) ((JFreeplaneMenuItem) e.getSource()).getParent();
-		JTree tree = (JTree) menu.getInvoker();
-		DefaultTreeModel treeModel = (DefaultTreeModel) tree.getModel();
-		treeModel.removeNodeFromParent(node);
-		
-    }
 
+	public void actionPerformed(final ActionEvent e) {
+		System.out.println("DeleteFileNode: " + e.getActionCommand() + " : " + e.getID());
+
+		int yesorno = JOptionPane.OK_OPTION;
+		yesorno = JOptionPane.showConfirmDialog(Controller.getCurrentController().getViewController().getContentPane(),
+				TextUtils.getText("confirm_delete_file_action"),
+				TextUtils.getText("confirm_delete_file_action_title"), JOptionPane.OK_CANCEL_OPTION);
+		if (yesorno == JOptionPane.OK_OPTION) {
+			deleteFile(e);
+		}		
+	}
+
+	private void deleteFile(final ActionEvent e) {
+		DefaultMutableTreeNode node = this.getNodeFromActionEvent(e);
+		 if (node.getUserObject() instanceof DefaultFileNode) {
+		 ((DefaultFileNode) node.getUserObject()).delete();
+		 }
+		
+		 WorkspacePopupMenu menu = (WorkspacePopupMenu) ((JFreeplaneMenuItem)
+		 e.getSource()).getParent();
+		 JTree tree = (JTree) menu.getInvoker();
+		 DefaultTreeModel treeModel = (DefaultTreeModel) tree.getModel();
+		 treeModel.removeNodeFromParent(node);
+	}
 
 }
