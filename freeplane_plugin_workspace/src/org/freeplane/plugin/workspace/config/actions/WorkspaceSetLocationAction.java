6fe8549d498c4629d3fc6712de79f32774d42d79
Stefan Langer <langer@docear.org>
2011-07-29 14:24:02 +0200

    new Action: Set Workspace Location

--- freeplane_plugin_workspace/src/org/freeplane/plugin/workspace/config/actions/WorkspaceSetLocationAction.java
+++ freeplane_plugin_workspace/src/org/freeplane/plugin/workspace/config/actions/WorkspaceSetLocationAction.java
@@ -1,8 +1,16 @@
 package org.freeplane.plugin.workspace.config.actions;
 
 import java.awt.event.ActionEvent;
+import java.io.File;
 
+import javax.swing.JFileChooser;
+
+import org.freeplane.core.resources.ResourceController;
 import org.freeplane.core.ui.AFreeplaneAction;
+import org.freeplane.core.ui.components.UITools;
+import org.freeplane.features.mode.Controller;
+import org.freeplane.plugin.workspace.WorkspaceController;
+import org.freeplane.plugin.workspace.WorkspacePreferences;
 
 public class WorkspaceSetLocationAction extends AFreeplaneAction {
 
@@ -14,10 +22,26 @@ public class WorkspaceSetLocationAction extends AFreeplaneAction {
 	public WorkspaceSetLocationAction() {
 		super("WorkspaceSetLocationAction");
 	}
-	
-	public void actionPerformed(final ActionEvent e) {
-        System.out.println("WorkspaceSetLocationAction: "+e.getActionCommand()+" : "+e.getID());
-    }
 
+	public void actionPerformed(final ActionEvent e) {
+		System.out.println("WorkspaceSetLocationAction: " + e.getActionCommand() + " : " + e.getID());
+		showLocationSwitcherDialog();
+	}
+
+	private void showLocationSwitcherDialog() {
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+		int retVal = fileChooser.showOpenDialog(UITools.getFrame());
+		if (retVal == JFileChooser.APPROVE_OPTION) {
+			File selectedfile = fileChooser.getSelectedFile();
+			ResourceController.getResourceController().setProperty(WorkspacePreferences.WORKSPACE_LOCATION_NEW,
+					selectedfile.getPath());
+			Controller.getCurrentController().getResourceController()
+					.setProperty(WorkspacePreferences.SHOW_WORKSPACE_PROPERTY_KEY, true);
+			WorkspaceController.getCurrentWorkspaceController().refreshWorkspace();
+		}
+
+	}
 
 }
