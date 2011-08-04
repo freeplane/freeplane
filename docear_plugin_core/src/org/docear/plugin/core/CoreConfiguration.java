87b8f07824df7edde86f6a735078ba042b563b57
Christoph Müller <mueller@docear.org>
2011-08-04 17:26:10 +0200

    Began changing Freeplane String ton Docear Strings

--- docear_plugin_core/src/org/docear/plugin/core/CoreConfiguration.java
+++ docear_plugin_core/src/org/docear/plugin/core/CoreConfiguration.java
@@ -1,13 +1,15 @@
 package org.docear.plugin.core;
 
 import java.net.URL;
+import java.util.Enumeration;
 
 import org.freeplane.core.resources.ResourceBundles;
 import org.freeplane.core.resources.ResourceController;
+import org.freeplane.core.ui.AFreeplaneAction;
+import org.freeplane.core.ui.MenuBuilder;
 import org.freeplane.core.util.LogUtils;
 import org.freeplane.features.mode.Controller;
 import org.freeplane.features.mode.ModeController;
-import org.freeplane.features.mode.mindmapmode.MModeController;
 
 public class CoreConfiguration {
 	
@@ -18,9 +20,35 @@ public class CoreConfiguration {
 	}
 
 	private void init(ModeController modeController) {
-		addPluginDefaults();		
+		addPluginDefaults();
+		addDocearProperties();		
 	}
 	
+	private void addDocearProperties() {
+		ResourceBundles bundles = ((ResourceBundles)ResourceController.getResourceController().getResources());
+		Controller controller = Controller.getCurrentController();
+		for (Enumeration<?> i = bundles.getKeys(); i.hasMoreElements();){
+			String key = i.nextElement().toString();
+			String value = bundles.getResourceString(key);
+			if(value.matches(".*[Ff][Rr][Ee][Ee][Pp][Ll][Aa][Nn][Ee].*")){
+				value = value.replaceAll("[Ff][Rr][Ee][Ee][Pp][Ll][Aa][Nn][Ee]", "Docear");
+				bundles.putResourceString(key, value);
+				if(key.matches(".*[.text]")){
+					key = key.replace(".text", "");
+					AFreeplaneAction action = controller.getAction(key);
+					if(action != null){
+						MenuBuilder.setLabelAndMnemonic(action, value);
+					}
+				}				
+			}			
+		}
+		
+		
+		//.putResourceString("mode_title", "Docear - {0}");
+		//((ResourceBundles)ResourceController.getResourceController().getResources()).putResourceString("OpenFreeplaneSiteAction.text", "Docear &Webseite");
+		
+	}
+
 	private void addPluginDefaults() {
 		final URL defaults = this.getClass().getResource(ResourceController.PLUGIN_DEFAULTS_RESOURCE);
 		if (defaults == null)
