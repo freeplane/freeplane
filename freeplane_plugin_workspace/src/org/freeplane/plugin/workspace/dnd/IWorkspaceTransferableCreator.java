d7ffffb4c038f23358495fde2873ca4577891e85
Marcel Genzmehr <genzmehr@docear.org>
2011-08-15 10:53:29 +0200

    dnd refactoring

---
+++ freeplane_plugin_workspace/src/org/freeplane/plugin/workspace/dnd/IWorkspaceTransferableCreator.java
@@ -0,0 +1,15 @@
+/**
+ * author: Marcel Genzmehr
+ * 12.08.2011
+ */
+package org.freeplane.plugin.workspace.dnd;
+
+import java.awt.datatransfer.Transferable;
+
+/**
+ * 
+ */
+public interface IWorkspaceTransferableCreator {
+	public Transferable getTransferable(); 
+
+}
