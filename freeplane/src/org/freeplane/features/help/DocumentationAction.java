34c908d2ae12b93f4909a6ddae9a41ae38099ad9
dpolivaev <>
2011-09-27 12:05:40 +0200

    HotKeyInfoAction

--- freeplane/src/org/freeplane/features/help/DocumentationAction.java
+++ freeplane/src/org/freeplane/features/help/DocumentationAction.java
@@ -51,12 +51,10 @@ import org.freeplane.features.styles.StyleFactory;
 class DocumentationAction extends AFreeplaneAction {
 	private static final long serialVersionUID = 1L;
 	private final String document;
-	private final boolean addHotKeys;
 
-	DocumentationAction( final String actionName, final String document, final boolean addHotKeys) {
+	DocumentationAction( final String actionName, final String document) {
 		super(actionName);
 		this.document = document;
-		this.addHotKeys = addHotKeys;
 	}
 
 	public void actionPerformed(final ActionEvent e) {
@@ -86,9 +84,6 @@ class DocumentationAction extends AFreeplaneAction {
 					try {
 						if (endUrl.getFile().endsWith(".mm")) {
 							 Controller.getCurrentController().selectMode(BModeController.MODENAME);
-							if (Controller.getCurrentModeController().getMapController().newMap(endUrl, false) && addHotKeys) {
-								appendAcceleratableMenuEntries();
-							}
 						}
 						else {
 							Controller.getCurrentController().getViewController().openDocument(endUrl);
@@ -109,96 +104,4 @@ class DocumentationAction extends AFreeplaneAction {
 	public void afterMapChange(final Object newMap) {
 	}
 
-	private void appendAcceleratableMenuEntries() {
-		// use the MModeController for the mindmap mode menu if possible - the browse menu doesn't have much entries!
-		final ModeController modeController = ResourceController.getResourceController().isApplet() ? Controller
-		    .getCurrentModeController() : MModeController.getMModeController();
-		final MenuBuilder menuBuilder = modeController.getUserInputListenerFactory().getMenuBuilder();
-		final DefaultMutableTreeNode menuEntryTree = MenuUtils.createAcceleratebleMenuEntryTree(
-		    FreeplaneMenuBar.MENU_BAR_PREFIX, menuBuilder);
-		final MapController mapController = Controller.getCurrentModeController().getMapController();
-		final NodeModel rootNode = mapController.getRootNode();
-		final NodeModel newNode = mapController.newNode(TextUtils.getText("hot_keys"), rootNode.getMap());
-		newNode.setFolded(true);
-		newNode.setLeft(false);
-		LogicalStyleModel.createExtension(newNode).setStyle(StyleFactory.create("Title"));
-		final int HOT_KEYS_INDEX = 2;
-		mapController.insertNodeIntoWithoutUndo(newNode, rootNode, HOT_KEYS_INDEX);
-		insertAcceleratorHtmlTable(newNode, menuEntryTree);
-		MenuUtils.insertAsNodeModelRecursively(newNode, menuEntryTree.children(), mapController);
-	}
-
-	@SuppressWarnings("unchecked")
-	private void insertAcceleratorHtmlTable(final NodeModel newNode, final DefaultMutableTreeNode menuEntryTree) {
-		final MapController mapController = Controller.getCurrentModeController().getMapController();
-		final String title = TextUtils.getText("hot_keys_table");
-		final MapModel map = mapController.getRootNode().getMap();
-		final NodeModel titleNode = mapController.newNode(title, map);
-		titleNode.setFolded(true);
-		newNode.insert(titleNode);
-		final NodeModel tableNode = mapController.newNode(DocumentationAction.formatAsHtml(menuEntryTree.children(),
-		    title), map);
-		titleNode.insert(tableNode);
-	}
-
-	// ==========================================================================
-	//                 format accelerator map as html text
-	// ==========================================================================
-	private static String formatAsHtml(final Enumeration<DefaultMutableTreeNode> children, final String title) {
-		final StringBuilder builder = new StringBuilder();
-		builder.append("<html><head><style type=\"text/css\">" //
-		        //doesn't work: + "  table { margin: 1px 0px; border-spacing: 0px; }"//
-		        + "  h1 { background-color: #B5C8DB; margin-bottom: 0px; margin-top: 1ex; }"//
-		        + "  h2 { background-color: #B5C8DB; margin-bottom: 0px; margin-top: 1ex; }"//
-		        + "  h3 { background-color: #B5C8DB; margin-bottom: 0px; margin-top: 1ex; }"//
-		        + "</head><body>");
-		DocumentationAction.appendAsHtml(builder, children, title, 2);
-		builder.append("</body></html>");
-		return builder.toString();
-	}
-
-	private static void appendAsHtml(final StringBuilder builder, final Enumeration<DefaultMutableTreeNode> children,
-	                                 final String title, final int level) {
-		builder.append("<h").append(level).append('>').append(title).append("</h1>");
-		DocumentationAction.appendChildrenAsHtml(builder, children, title, level);
-	}
-
-	@SuppressWarnings("unchecked")
-	private static void appendChildrenAsHtml(final StringBuilder builder,
-	                                         final Enumeration<DefaultMutableTreeNode> children, final String title,
-	                                         final int level) {
-		final ArrayList<MenuEntry> menuEntries = new ArrayList<MenuEntry>();
-		final ArrayList<DefaultMutableTreeNode> submenus = new ArrayList<DefaultMutableTreeNode>();
-		// sort and divide
-		while (children.hasMoreElements()) {
-			final DefaultMutableTreeNode node = children.nextElement();
-			if (node.isLeaf()) {
-				menuEntries.add((MenuEntry) node.getUserObject());
-			}
-			else {
-				submenus.add(node);
-			}
-		}
-		// actions
-		if (!menuEntries.isEmpty()) {
-			builder.append("<table cellspacing=\"0\" cellpadding=\"0\">");
-			for (final MenuEntry entry : menuEntries) {
-				final String keystroke = entry.getKeyStroke() == null ? "" //
-				        : MenuUtils.formatKeyStroke(entry.getKeyStroke());
-				builder.append(DocumentationAction.el("tr", DocumentationAction.el("td", entry.getLabel() + "&#xa0;")
-				        + DocumentationAction.el("td", keystroke)
-				        + DocumentationAction.el("td", entry.getToolTipText())));
-			}
-			builder.append("</table>");
-		}
-		// submenus
-		for (final DefaultMutableTreeNode node : submenus) {
-			final String subtitle = (level > 2 ? title + "&#8594;" : "") + String.valueOf(node.getUserObject());
-			DocumentationAction.appendAsHtml(builder, node.children(), subtitle, level + 1);
-		}
-	}
-
-	private static String el(final String name, final String content) {
-		return HtmlUtils.element(name, content);
-	}
 }
