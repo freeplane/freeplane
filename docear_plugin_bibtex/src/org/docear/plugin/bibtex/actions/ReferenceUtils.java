974c4dd0cdfa78ffed4dce58a9989f175945a98c
Stefan Langer <langer@docear.org>
2011-10-25 14:54:52 +0200

    generate BibTeXkey when reference without key was added to mindmap node

--- docear_plugin_bibtex/src/org/docear/plugin/bibtex/actions/ReferenceUtils.java
+++ docear_plugin_bibtex/src/org/docear/plugin/bibtex/actions/ReferenceUtils.java
@@ -1,13 +1,14 @@
 package org.docear.plugin.bibtex.actions;
 
 import java.io.File;
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 
 import net.sf.jabref.BibtexEntry;
+import net.sf.jabref.Globals;
+import net.sf.jabref.labelPattern.LabelPatternUtil;
 
+import org.docear.plugin.bibtex.ReferencesController;
 import org.docear.plugin.pdfutilities.util.NodeUtils;
 import org.freeplane.features.link.LinkController;
 import org.freeplane.features.link.mindmapmode.MLinkController;
@@ -17,15 +18,25 @@ import org.freeplane.features.mode.Controller;
 public class ReferenceUtils {
 	public static void addReferenceToNode(BibtexEntry entry) {
 		NodeModel currentNode = Controller.getCurrentModeController().getMapController().getSelectedNode();
-
-		renewAttribute(currentNode, "jabref_id", entry.getId());
+		
+		
+		if (entry.getField("bibtexkey")==null) {
+			LabelPatternUtil.makeLabel(Globals.prefs.getKeyPattern(), ReferencesController.getController().getJabrefWrapper().getDatabase(), entry);
+		}
+		
+		//renewAttribute(currentNode, "jabref_id", entry.getId());
 		renewAttribute(currentNode, "bibtex_key", entry.getField("bibtexkey"));
 		renewAttribute(currentNode, "jabref_author", entry.getField("author"));
 		renewAttribute(currentNode, "jabref_title", entry.getField("title"));
 		renewAttribute(currentNode, "jabref_year", entry.getField("year"));
 		renewAttribute(currentNode, "jabref_journal", entry.getField("journal"));
-
+		
+		
+		
 		String path = entry.getField("file");
+		System.out.println("debug path: "+path);
+		
+		
 		if (path != null) {
 			NodeUtils.setLinkFrom(new File(path).toURI(), currentNode);
 		}
