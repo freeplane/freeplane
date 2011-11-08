c7010734fd42472d4acdc846d4f6e0fc74780a88
Stefan Langer <langer@docear.org>
2011-11-08 17:18:18 +0100

    update references in SwingWorker thread

--- docear_plugin_bibtex/src/org/docear/plugin/bibtex/listeners/JabRefChangeListener.java
+++ docear_plugin_bibtex/src/org/docear/plugin/bibtex/listeners/JabRefChangeListener.java
@@ -8,9 +8,10 @@ import org.docear.plugin.bibtex.JabRefAttributes;
 import org.docear.plugin.bibtex.ReferencesController;
 import org.freeplane.features.map.NodeModel;
 import org.freeplane.features.mode.Controller;
+import org.jdesktop.swingworker.SwingWorker;
 
 public class JabRefChangeListener implements DatabaseChangeListener {
-	
+
 	private static boolean locked = false;
 
 	@Override
@@ -18,54 +19,71 @@ public class JabRefChangeListener implements DatabaseChangeListener {
 		if (locked) {
 			return;
 		}
-		
+
 		locked = true;
-		
+
 		BibtexEntry entry = e.getEntry();
 		NodeModel root = Controller.getCurrentModeController().getMapController().getRootNode();
-		
+
 		if (e.getType() == DatabaseChangeEvent.REMOVED_ENTRY) {
-			System.out.println("debug removed: "+e.getEntry().getCiteKey());
+			System.out.println("debug removed: " + e.getEntry().getCiteKey());
 			deleteNodeAttributes(root, entry);
 		}
 		else if (e.getType() == DatabaseChangeEvent.CHANGED_ENTRY) {
-			System.out.println("debug changed: "+e.getEntry().getCiteKey());
+			System.out.println("debug changed: " + e.getEntry().getCiteKey());
 			updateNodeAttributes(root, entry);
 		}
 		else if (e.getType() == DatabaseChangeEvent.CHANGING_ENTRY) {
-			System.out.println("debug changing: "+e.getEntry().getCiteKey());			
+			System.out.println("debug changing: " + e.getEntry().getCiteKey());
 		}
 		else if (e.getType() == DatabaseChangeEvent.ADDED_ENTRY) {
-			System.out.println("debug added: "+e.getEntry().getCiteKey());
+			System.out.println("debug added: " + e.getEntry().getCiteKey());
 		}
-		
+
 		locked = false;
 	}
-	
-	public void updateNodeAttributes(NodeModel node, BibtexEntry entry) {
-		JabRefAttributes jabRefAttributes = ReferencesController.getController().getJabRefAttributes();
-		
-		if (jabRefAttributes.isReferencing(entry, node)) {
-			jabRefAttributes.setReferenceToNode(entry, node);
-		}
-		
-		for (NodeModel child : node.getChildren()) {
-			updateNodeAttributes(child, entry);
-		}
+
+	public void updateNodeAttributes(final NodeModel node, final BibtexEntry entry) {
+		SwingWorker<Void, Void> thread = new SwingWorker<Void, Void>() {
+			@Override
+			protected Void doInBackground() throws Exception {
+				JabRefAttributes jabRefAttributes = ReferencesController.getController().getJabRefAttributes();
+
+				if (jabRefAttributes.isReferencing(entry, node)) {
+					jabRefAttributes.setReferenceToNode(entry, node);
+				}
+
+				for (NodeModel child : node.getChildren()) {
+					updateNodeAttributes(child, entry);
+				}
+
+				return null;
+			}
+		};
+
+		thread.run();
 	}
-	
-	public void deleteNodeAttributes(NodeModel node, BibtexEntry entry) {
-		JabRefAttributes jabRefAttributes = ReferencesController.getController().getJabRefAttributes();
-		
-		if (jabRefAttributes.isReferencing(entry, node)) {
-			jabRefAttributes.removeReferenceFromNode(entry, node);
-		}
-		
-		for (NodeModel child : node.getChildren()) {
-			updateNodeAttributes(child, entry);
-		}
-		
+
+	public void deleteNodeAttributes(final NodeModel node, final BibtexEntry entry) {
+		SwingWorker<Void, Void> thread = new SwingWorker<Void, Void>() {
+			@Override
+			protected Void doInBackground() throws Exception {
+				JabRefAttributes jabRefAttributes = ReferencesController.getController().getJabRefAttributes();
+
+				if (jabRefAttributes.isReferencing(entry, node)) {
+					jabRefAttributes.removeReferenceFromNode(entry, node);
+				}
+
+				for (NodeModel child : node.getChildren()) {
+					updateNodeAttributes(child, entry);
+				}
+
+				return null;
+			}
+		};
+
+		thread.run();
+
 	}
-	
 
 }
