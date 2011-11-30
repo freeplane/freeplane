db476f9a681fed6a7ebf0e5eb11c2ce4288aaf6d
Marcel Genzmehr <genzmehr@docear.org>
2011-11-30 11:42:20 +0100

    bugfix: null pointer if trying to add a new reference to a node that has no file link

--- docear_plugin_bibtex/src/org/docear/plugin/bibtex/actions/AddNewReferenceAction.java
+++ docear_plugin_bibtex/src/org/docear/plugin/bibtex/actions/AddNewReferenceAction.java
@@ -55,10 +55,11 @@ public class AddNewReferenceAction extends AFreeplaneAction{
 		else {			
 			NodeModel node = Controller.getCurrentModeController().getMapController().getSelectedNode();
 			URI link = NodeLinks.getLink(node);
-			String path = WorkspaceUtils.resolveURI(link).getAbsolutePath();
+			
 			JabrefWrapper jabrefWrapper = ReferencesController.getController().getJabrefWrapper();
 
 			if (link != null && link.getPath().toLowerCase().endsWith(".pdf")) {
+				String path = WorkspaceUtils.resolveURI(link).getAbsolutePath();
 				new PdfImporter(jabrefWrapper.getJabrefFrame(), jabrefWrapper.getJabrefFrame()
 						.basePanel(), null, 0).importPdfFiles(new String[] { path }, Controller.getCurrentController()
 						.getViewController().getFrame(), true);
