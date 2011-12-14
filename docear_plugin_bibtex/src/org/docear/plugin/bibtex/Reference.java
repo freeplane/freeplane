af8045f0bbba9e7a79211299cc0dc401ec794147
Stefan Langer <langer@docear.org>
2011-12-14 15:20:52 +0100

    - update links as well - only use new methods for inserting / updating references to a node - deal with urls : C:\... in linux --> catch exception - deal with multiple urls returned by jabref - deal with urls without protocol: assume http

--- docear_plugin_bibtex/src/org/docear/plugin/bibtex/Reference.java
+++ docear_plugin_bibtex/src/org/docear/plugin/bibtex/Reference.java
@@ -1,9 +1,16 @@
 package org.docear.plugin.bibtex;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Map.Entry;
 
 import net.sf.jabref.BibtexEntry;
+
+import org.freeplane.features.map.NodeModel;
+import org.freeplane.plugin.workspace.WorkspaceUtils;
 
 public class Reference {
 	public class Item {
@@ -26,8 +33,9 @@ public class Reference {
 	
 	private final Item key;
 	private final ArrayList<Item> attributes;
+	private URI uri = null;
 	
-	public Reference(BibtexEntry entry) { 
+	public Reference(BibtexEntry entry, NodeModel node) { 
 		JabRefAttributes jabRefAttributes = ReferencesController.getController().getJabRefAttributes();		
 		
 		attributes = new ArrayList<Reference.Item>();
@@ -35,6 +43,49 @@ public class Reference {
 		for (Entry<String, String> valueAttributes : jabRefAttributes.getValueAttributes().entrySet()) {
 			attributes.add(new Item(valueAttributes.getKey(), entry.getField(valueAttributes.getValue())));
 		}
+		
+		boolean isFile = true;
+		String url = entry.getField("file");
+		if (url != null) {
+			uri = jabRefAttributes.parsePath(entry, url.toString());
+			if (uri != null) {
+				url = WorkspaceUtils.resolveURI(uri, node.getMap()).getPath();
+			}
+			else {
+				url = null;
+			}
+		}
+		
+		if (url == null) {
+			isFile = false;
+			url = entry.getField("url");
+			if (url != null) {
+				url = url.trim();
+			}
+			if (url != null && url.length()>0) {
+				url = url.split(" ")[0];
+			}
+		}
+		
+		if (isFile) {
+			uri = new File(url.trim()).toURI();
+		}
+		else {
+			if (url != null) {
+				try {
+					try {
+						uri = new URL(url).toURI();
+					}			
+					catch (MalformedURLException e) {				
+						uri = new URL("http://"+url).toURI();
+					}
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+				}
+			}			
+		}
+		
 	}
 
 	public Item getKey() {
@@ -44,4 +95,14 @@ public class Reference {
 	public ArrayList<Item> getAttributes() {
 		return attributes;
 	}
+
+	public URI getUri() {
+		return uri;
+	}
+
+	public void setUri(URI uri) {
+		this.uri = uri;
+	}
+
+	
 }
