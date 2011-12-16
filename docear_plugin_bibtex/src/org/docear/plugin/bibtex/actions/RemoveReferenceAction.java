efa5df0ef96473817a9f059a35ac0d89101b85cd
Stefan Langer <langer@docear.org>
2011-12-16 15:21:22 +0100

    #172: disabled action if node has AnnotationModel and annotation id

--- docear_plugin_bibtex/src/org/docear/plugin/bibtex/actions/RemoveReferenceAction.java
+++ docear_plugin_bibtex/src/org/docear/plugin/bibtex/actions/RemoveReferenceAction.java
@@ -3,10 +3,12 @@ package org.docear.plugin.bibtex.actions;
 import java.awt.event.ActionEvent;
 
 import org.docear.plugin.bibtex.ReferencesController;
+import org.docear.plugin.pdfutilities.features.AnnotationModel;
 import org.freeplane.core.ui.AFreeplaneAction;
 import org.freeplane.core.ui.EnabledAction;
 import org.freeplane.features.map.NodeModel;
 import org.freeplane.features.mode.Controller;
+import org.freeplane.view.swing.map.MapView;
 
 @EnabledAction(checkOnPopup = true)
 public class RemoveReferenceAction extends AFreeplaneAction {
@@ -23,6 +25,8 @@ public class RemoveReferenceAction extends AFreeplaneAction {
 	public void actionPerformed(ActionEvent e) {
 		for (NodeModel node : Controller.getCurrentModeController().getMapController().getSelectedNodes()) {
 			ReferencesController.getController().getJabRefAttributes().removeReferenceFromNode(node);
+			
+			((MapView) Controller.getCurrentController().getViewController().getMapView()).getNodeView(node).updateAll();
 		}
 		
 	}
@@ -33,6 +37,13 @@ public class RemoveReferenceAction extends AFreeplaneAction {
 			setEnabled(false);
 			return;
 		}
+		
+		AnnotationModel annotation = (AnnotationModel) node.getExtension(AnnotationModel.class);
+		if (annotation != null && annotation.getAnnotationID() != null) {
+			setEnabled(false);
+			return;
+		}
+		
 		final String bibtexKey = ReferencesController.getController().getJabRefAttributes().getBibtexKey(node);
 		
 		if (bibtexKey != null && bibtexKey.length()>0) {
