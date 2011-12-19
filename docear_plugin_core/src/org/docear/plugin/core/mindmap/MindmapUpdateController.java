57914322db7c19a8f82be156f1ceeeea079103d1
Stefan Langer <langer@docear.org>
2011-12-19 14:56:15 +0100

    wrong character

--- docear_plugin_core/src/org/docear/plugin/core/mindmap/MindmapUpdateController.java
+++ docear_plugin_core/src/org/docear/plugin/core/mindmap/MindmapUpdateController.java
@@ -233,7 +233,7 @@ public class MindmapUpdateController {
 					if (mapFile != null) {
 						URI mapUri = mapFile.toURI();
 						for (URI uri : uris) {
-							if (uri.equals(mapUri)) {3
+							if (uri.equals(mapUri)) {
 								opened = true;
 							}
 						}
