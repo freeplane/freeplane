aca3117c4597efaff2b7101ee6be1fb745790469
Marcel Genzmehr <genzmehr@docear.org>
2011-12-22 16:12:58 +0100

    #208: Disabled Menu Items
    modified some actions from pdfutilities to catch null selections

--- docear_plugin_pdfutilities/src/org/docear/plugin/pdfutilities/actions/AddMonitoringFolderAction.java
+++ docear_plugin_pdfutilities/src/org/docear/plugin/pdfutilities/actions/AddMonitoringFolderAction.java
@@ -75,6 +75,10 @@ public class AddMonitoringFolderAction extends AbstractMonitoringAction {
 
 	@Override
 	public void setEnabled(){
+		if(Controller.getCurrentController().getSelection() == null) {
+			this.setEnabled(false);
+			return;
+		}
 		NodeModel selected = Controller.getCurrentController().getSelection().getSelected();
 		if(selected == null){
 			this.setEnabled(false);
