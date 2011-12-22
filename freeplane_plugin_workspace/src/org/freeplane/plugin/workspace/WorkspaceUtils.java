3d4a6462e112c19773aec7daa6320f5bc7848b89
Marcel Genzmehr <genzmehr@docear.org>
2011-12-22 13:03:27 +0100

    -#194: pdf monitoring does not work after changing the link types of the mindmap
    caused by a path translation mistake in org.docear.plugin.core.mindmap.MindmapLinkTypeUpdater:30
    fixed by removing the old failsave logic that does not work anymore
    
    -modified the WorkspaceUtils.resolveURI() a bit to deal with null and schemeless URIs
    -replaced local URI translation with WorkspaceUtils.resolveURI

--- freeplane_plugin_workspace/src/org/freeplane/plugin/workspace/WorkspaceUtils.java
+++ freeplane_plugin_workspace/src/org/freeplane/plugin/workspace/WorkspaceUtils.java
@@ -193,15 +193,29 @@ public class WorkspaceUtils {
 	}
 
 	public static URI absoluteURI(final URI uri) {
+		return absoluteURI(uri, null);
+
+	}
+	
+	public static URI absoluteURI(final URI uri, MapModel map) {
 		if(uri == null) {
 			return null;
 		}
 		try {
 			URLConnection urlConnection;
 			// windows drive letters are interpreted as uri schemes -> make a file from the scheme-less uri string and use this to resolve the path
-			if(Compat.isWindowsOS() && (uri.getScheme() == null || uri.getScheme().length() == 1)) { 
+			if(Compat.isWindowsOS() && (uri.getScheme() != null && uri.getScheme().length() == 1)) { 
 				urlConnection = (new File(uri.toString())).toURL().openConnection();
-			} else {
+			} 
+			else if(uri.getScheme() == null && !uri.getPath().startsWith(File.separator)) {
+				if(map != null) {
+					urlConnection = (new File(uri.toString())).toURL().openConnection();
+				} 
+				else {
+					urlConnection = UrlManager.getController().getAbsoluteUri(map, uri).toURL().openConnection();
+				}
+			}
+			else {
 				urlConnection = uri.toURL().openConnection();				
 			}
 			
@@ -230,6 +244,9 @@ public class WorkspaceUtils {
 	}
 
 	public static File resolveURI(final URI uri, final MapModel map) {
+		if(uri == null || map == null) {
+			return null;
+		}
 		try {
 			return resolveURI(UrlManager.getController().getAbsoluteUri(map, uri));
 		} 
