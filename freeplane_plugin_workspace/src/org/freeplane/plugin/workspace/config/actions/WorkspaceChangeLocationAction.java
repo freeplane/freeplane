890e29c38da1fcd52ec45c711594a0b90f70650f
Marcel Genzmehr <genzmehr@docear.org>
2012-01-24 18:12:10 +0100

    #282: close all mindmaps before changing the workspace
    -added a closeAllMindMaps method before switching to the new workspace

--- freeplane_plugin_workspace/src/org/freeplane/plugin/workspace/config/actions/WorkspaceChangeLocationAction.java
+++ freeplane_plugin_workspace/src/org/freeplane/plugin/workspace/config/actions/WorkspaceChangeLocationAction.java
@@ -2,6 +2,7 @@ package org.freeplane.plugin.workspace.config.actions;
 
 import java.awt.event.ActionEvent;
 
+import org.freeplane.features.mode.Controller;
 import org.freeplane.plugin.workspace.WorkspaceController;
 import org.freeplane.plugin.workspace.WorkspaceUtils;
 import org.freeplane.plugin.workspace.model.action.AWorkspaceAction;
@@ -23,13 +24,20 @@ public class WorkspaceChangeLocationAction extends AWorkspaceAction {
 		showLocationSwitcherDialog();		
 	}
 	
-	private void showLocationSwitcherDialog() {		
+	private void showLocationSwitcherDialog() {
+		closeAllMindMaps();	
 		WorkspaceUtils.showWorkspaceChooserDialog();
 		
 		WorkspaceController workspaceController = WorkspaceController.getController();
 		workspaceController.reloadWorkspace();
 		
 
+	}
+	
+	private void closeAllMindMaps() {
+		while(Controller.getCurrentController().getMap() != null) {
+			Controller.getCurrentController().close(false);
+		}	
 	}
 
 }
