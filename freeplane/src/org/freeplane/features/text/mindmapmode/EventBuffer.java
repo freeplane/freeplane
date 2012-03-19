e505e8edd3b645ceb2b0e4020cc49611e926a2cf
dpolivaev <>
2012-03-20 00:55:53 +0100

    try to prevent freeplane freeze on EventBuffer

--- freeplane/src/org/freeplane/features/text/mindmapmode/EventBuffer.java
+++ freeplane/src/org/freeplane/features/text/mindmapmode/EventBuffer.java
@@ -29,6 +29,8 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
+import org.freeplane.core.util.LogUtils;
+
 /**
  * @author Dimitry Polivaev
  * Aug 23, 2011
@@ -38,49 +40,59 @@ public class EventBuffer implements KeyEventDispatcher, FocusListener {
 	private Component textComponent;
 	boolean isActive = false;
 	private InputEvent firstEvent;
-	
+
 	public boolean isActive() {
-    	return isActive;
-    }
+		return isActive;
+	}
 	EventBuffer(){}
 	public Component getTextComponent() {
-    	return textComponent;
-    }
+		return textComponent;
+	}
 
 	public void setTextComponent(Component c) {
 		if(textComponent != null)
 			textComponent.removeFocusListener(this);
-    	this.textComponent = c;
-    	if(textComponent != null)
-    		textComponent.addFocusListener(this);
-    }
-
-	public boolean dispatchKeyEvent(final KeyEvent ke) {
-	    if(events.contains(ke)){
-	        return false;
-	    }
-	    if(textComponent != null){
-	    	KeyEvent newEvent = new KeyEvent(textComponent, ke.getID(), ke.getWhen(), ke.getModifiers(), ke.getKeyCode(), ke.getKeyChar(), ke.getKeyLocation());
-	    	events.add(newEvent);
-	    }
-	    else
-	    	events.add(ke);
-	    ke.consume();
-		return true;
+		this.textComponent = c;
+		if(textComponent != null)
+			textComponent.addFocusListener(this);
 	}
 
-	public void focusGained(final FocusEvent e) {
-		textComponent.removeFocusListener(this);
-		for (int i = 0; i < events.size(); i++) {
-            final KeyEvent ke = events.get(i);
-            if(ke.getComponent().equals(textComponent))
-            	e.getComponent().dispatchEvent(ke);
-            else{
-            	KeyEvent newEvent = new KeyEvent(textComponent, ke.getID(), ke.getWhen(), ke.getModifiers(), ke.getKeyCode(), ke.getKeyChar(), ke.getKeyLocation());
-            	e.getComponent().dispatchEvent(newEvent);
-            }
+	public boolean dispatchKeyEvent(final KeyEvent ke) {
+		if(events.contains(ke)){
+			return false;
 		}
-		deactivate();
+		if(textComponent != null){
+			KeyEvent newEvent = new KeyEvent(textComponent, ke.getID(), ke.getWhen(), ke.getModifiers(), ke.getKeyCode(), ke.getKeyChar(), ke.getKeyLocation());
+			events.add(newEvent);
+		}
+		else
+			events.add(ke);
+		
+		// Prevent Freeplane freeze
+		if(ke.getKeyCode() == KeyEvent.VK_ESCAPE 
+				&& ke.getID() == KeyEvent.KEY_RELEASED){
+			KeyboardFocusManager.getCurrentKeyboardFocusManager().removeKeyEventDispatcher(this);
+		}
+		ke.consume();
+		return true;
+	}
+	
+	public void focusGained(final FocusEvent e) {
+		try{
+			textComponent.removeFocusListener(this);
+			for (int i = 0; i < events.size(); i++) {
+				final KeyEvent ke = events.get(i);
+				if(ke.getComponent().equals(textComponent))
+					e.getComponent().dispatchEvent(ke);
+				else{
+					KeyEvent newEvent = new KeyEvent(textComponent, ke.getID(), ke.getWhen(), ke.getModifiers(), ke.getKeyCode(), ke.getKeyChar(), ke.getKeyLocation());
+					e.getComponent().dispatchEvent(newEvent);
+				}
+			}
+		}
+		finally{
+			deactivate();
+		}
 	}
 
 	public void focusLost(final FocusEvent e) {
@@ -90,29 +102,29 @@ public class EventBuffer implements KeyEventDispatcher, FocusListener {
 			return;
 		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
 		isActive = true;
-    }
+	}
 	public void deactivate() {
 		if(! isActive)
 			return;
 		KeyboardFocusManager.getCurrentKeyboardFocusManager().removeKeyEventDispatcher(this);
 		isActive = false;
-    	if(textComponent != null)
-    		textComponent.removeFocusListener(this);
+		if(textComponent != null)
+			textComponent.removeFocusListener(this);
 		textComponent = null;
 		events.clear();
 		firstEvent = null;
-    }
+	}
 	public void activate(InputEvent e) {
-	    activate();
-	    if(e instanceof KeyEvent)
-	    	dispatchKeyEvent((KeyEvent) e);
-	    else if(e instanceof MouseEvent)
-	        setFirstEvent(e);
-    }
+		activate();
+		if(e instanceof KeyEvent)
+			dispatchKeyEvent((KeyEvent) e);
+		else if(e instanceof MouseEvent)
+			setFirstEvent(e);
+	}
 	public void setFirstEvent(InputEvent e) {
-	    firstEvent = e;
-    }
-	
+		firstEvent = e;
+	}
+
 	public KeyEvent getFirstEvent(){
 		if(firstEvent instanceof KeyEvent)
 			return (KeyEvent) firstEvent;
@@ -120,11 +132,11 @@ public class EventBuffer implements KeyEventDispatcher, FocusListener {
 			return null;
 		return events.get(0);
 	}
-	
+
 	public MouseEvent getMouseEvent() {
 		if(firstEvent instanceof MouseEvent)
 			return (MouseEvent) firstEvent;
 		else
 			return null;
-    }
+	}
 }
