decf57b3851e3769851ae345cec02cc537e489af
boercher <>
2012-05-20 16:28:06 +0200

    apply standard format after guess object - only if format wasn't set manually

--- freeplane/src/org/freeplane/features/text/mindmapmode/MTextController.java
+++ freeplane/src/org/freeplane/features/text/mindmapmode/MTextController.java
@@ -343,12 +343,15 @@ public class MTextController extends TextController {
 		if (HtmlUtils.isHtmlNode(newText))
 			setNodeObject(node, newText);
         else {
-	        final Object guessedObject = guessObject(newText, NodeStyleModel.getNodeFormat(node));
+	        final String nodeFormat = NodeStyleModel.getNodeFormat(node);
+            final Object guessedObject = guessObject(newText, nodeFormat);
 	        if(guessedObject instanceof IFormattedObject) {
                 setNodeObject(node, ((IFormattedObject) guessedObject).getObject());
                 // parser applies standard format - use it
-                ((MNodeStyleController) NodeStyleController.getController()).setNodeFormat(node,
-                    PatternFormat.STANDARD_FORMAT_PATTERN);
+                if (nodeFormat != null) {
+                    ((MNodeStyleController) NodeStyleController.getController()).setNodeFormat(node,
+                        PatternFormat.STANDARD_FORMAT_PATTERN);
+                }
 	        }
 	        else {
 	            setNodeObject(node, newText);
