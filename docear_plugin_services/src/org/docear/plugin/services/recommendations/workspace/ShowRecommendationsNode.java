fac6b4d778562fc736cf6dcd6034cb7f4a7aecb5
Marcel Genzmehr <genzmehr@docear.org>
2012-06-12 17:57:36 +0200

    integrated recommendations action into the workspace

---
+++ docear_plugin_services/src/org/docear/plugin/services/recommendations/workspace/ShowRecommendationsNode.java
@@ -0,0 +1,67 @@
+package org.docear.plugin.services.recommendations.workspace;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.tree.DefaultTreeCellRenderer;
+
+import org.docear.plugin.services.ServiceController;
+import org.freeplane.core.resources.ResourceController;
+import org.freeplane.plugin.workspace.components.menu.WorkspacePopupMenu;
+import org.freeplane.plugin.workspace.components.menu.WorkspacePopupMenuBuilder;
+import org.freeplane.plugin.workspace.model.AWorkspaceTreeNode;
+import org.freeplane.plugin.workspace.nodes.AActionNode;
+
+public class ShowRecommendationsNode extends AActionNode {
+
+	private static final Icon DEFAULT_ICON = new ImageIcon(ShowRecommendationsNode.class.getResource("/icons/books.png"));
+
+	private static final long serialVersionUID = 1L;
+	
+	private WorkspacePopupMenu popupMenu = null;
+	
+	
+	public ShowRecommendationsNode(String type) {
+		super(type);
+	}
+
+	@Override
+	public void initializePopup() {
+		if (popupMenu == null) {						
+			popupMenu  = new WorkspacePopupMenu();
+			WorkspacePopupMenuBuilder.addActions(popupMenu, new String[] {
+					"workspace.action.node.refresh"
+			});
+		}
+		
+	}
+
+	@Override
+	public WorkspacePopupMenu getContextMenu() {
+		if (popupMenu == null) {
+			initializePopup();
+		}
+		return popupMenu;
+	}
+	
+	public boolean setIcons(DefaultTreeCellRenderer renderer) {
+		renderer.setOpenIcon(DEFAULT_ICON);
+		renderer.setClosedIcon(DEFAULT_ICON);
+		renderer.setLeafIcon(DEFAULT_ICON);
+		return true;
+	}
+	
+	public void refresh() {
+		//Controller.getCurrentController().selectMode(DocearRecommendationsModeController.MODENAME);
+		ServiceController.getController().getRecommenationMode().getMapController().refreshRecommendations();
+	}
+	
+	protected AWorkspaceTreeNode clone(ShowRecommendationsNode node) {
+		return super.clone(node);
+	}
+
+	@Override
+	public AWorkspaceTreeNode clone() {
+		ShowRecommendationsNode node = new ShowRecommendationsNode(getType());
+		return clone(node);
+	}
+}
