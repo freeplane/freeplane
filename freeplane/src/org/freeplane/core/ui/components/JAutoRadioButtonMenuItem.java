ffa98a72c888d2e547bce6006bf02715526cdc4b
dpolivaev <>
2012-06-17 21:58:20 +0200

    Implement selection state of JAutoRadioButtonMenuItem similar to JAutoCheckBoxMenuItem

--- freeplane/src/org/freeplane/core/ui/components/JAutoRadioButtonMenuItem.java
+++ freeplane/src/org/freeplane/core/ui/components/JAutoRadioButtonMenuItem.java
@@ -43,10 +43,16 @@ public class JAutoRadioButtonMenuItem extends JRadioButtonMenuItem implements Pr
 	private PropertyChangeListener defaultPropertyChanegListener;
 
 	public JAutoRadioButtonMenuItem(final IFreeplaneAction a) {
-		super(a);
+		super();
 		setModel(new ActionToggleButtonModel(a));
-		setSelected(a.isSelected());
+		setAction(a);
 	}
+	
+	@Override
+    protected void configurePropertiesFromAction(Action a) {
+	    super.configurePropertiesFromAction(a);
+	    setSelected(((IFreeplaneAction)a).isSelected());
+    }
 
 	@Override
 	protected PropertyChangeListener createActionPropertyChangeListener(final Action a) {
