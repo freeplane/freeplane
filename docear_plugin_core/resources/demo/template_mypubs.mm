bc44410c592e825e05f548917b42b40b83f88fd3
Stefan Langer <langer@docear.org>
2012-08-23 18:44:11 +0200

    forgotten files

---
+++ docear_plugin_core/resources/demo/template_mypubs.mm
@@ -0,0 +1,183 @@
+<map version="docear 1.0">
+<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
+<attribute_registry SHOW_ATTRIBUTES="hide"/>
+<node TEXT="My Publications" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1345646282035"><hook NAME="MapStyle" zoom="1.21">
+    <properties show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6"/>
+<node POSITION="right" ID="ID_1626968962" CREATED="1344504053189" MODIFIED="1345708801857"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Example
+    </p>
+    <p style="text-align: center">
+      <font size="1">(Press DEL on your keyboard to </font>
+    </p>
+    <p style="text-align: center">
+      <font size="1">delete this node and its children)</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#ff0000" WIDTH="3"/>
+<node TEXT="By year" ID="ID_1272394717" CREATED="1345646644459" MODIFIED="1345646647848">
+<node TEXT="2011" OBJECT="java.lang.Long|2011" ID="ID_37380019" CREATED="1345646336806" MODIFIED="1345719735262">
+<hook NAME="FirstGroupNode"/>
+<node TEXT="Docear -- An Academic Literature Suite.pdf" ID="ID_34223233" CREATED="1345719440587" MODIFIED="1345720997798" LINK="workspace:/literature_repository/docear_example_pdfs/Docear%20--%20An%20Academic%20Literature%20Suite.pdf">
+<pdf_annotation type="PDF_FILE"/>
+<attribute NAME="key" VALUE="Beel2011c"/>
+<attribute NAME="authors" VALUE="{B}eel, {J}oeran and {G}ipp, {B}ela and {L}anger, {S}tefan and {G}enzmehr, {M}arcel"/>
+<attribute NAME="title" VALUE="{D}ocear: {A}n {A}cademic {L}iterature {S}uite for {S}earching, {O}rganizing and {C}reating {A}cademic {L}iterature"/>
+<attribute NAME="year" VALUE="2011"/>
+</node>
+</node>
+<node TEXT="2010" OBJECT="java.lang.Long|2010" ID="ID_110842003" CREATED="1345646338998" MODIFIED="1345646340658">
+<node TEXT="Academic Search Engine Optimization (ASEO) -- Optimizing&#xa;Scholarly Literature for Google Scholar and Co.pdf" ID="ID_61635320" CREATED="1345719440505" MODIFIED="1345721011437" LINK="workspace:/literature_repository/docear_example_pdfs/Academic%20Search%20Engine%20Optimization%20(ASEO)%20--%20Optimizing%20Scholarly%20Literature%20for%20Google%20Scholar%20and%20Co.pdf">
+<pdf_annotation type="PDF_FILE"/>
+<attribute NAME="key" VALUE="Beel10"/>
+<attribute NAME="journal" VALUE="Journal of Scholarly Publishing"/>
+<attribute NAME="authors" VALUE="{J}oeran {B}eel and {B}ela {G}ipp and {E}rik {W}ilde"/>
+<attribute NAME="title" VALUE="{A}cademic {S}earch {E}ngine {O}ptimization ({ASEO}): {O}ptimizing {S}cholarly {L}iterature for {G}oogle {S}cholar and {C}o."/>
+<attribute NAME="year" VALUE="2010"/>
+</node>
+<node TEXT="Academic search engine spam and Google Scholars&#xa;resilience against it.pdf" ID="ID_492113293" CREATED="1345719440544" MODIFIED="1345721015877" LINK="workspace:/literature_repository/docear_example_pdfs/Academic%20search%20engine%20spam%20and%20Google%20Scholars%20resilience%20against%20it.pdf">
+<pdf_annotation type="PDF_FILE"/>
+<attribute NAME="key" VALUE="Beel2010"/>
+<attribute NAME="journal" VALUE="Journal of Electronic Publishing"/>
+<attribute NAME="authors" VALUE="{J}oeran {B}eel and {B}ela {G}ipp"/>
+<attribute NAME="title" VALUE="{A}cademic search engine spam and {G}oogle {S}cholar&apos;s resilience against it"/>
+<attribute NAME="year" VALUE="2010"/>
+</node>
+</node>
+<node TEXT="2009" OBJECT="java.lang.Long|2009" ID="ID_692315417" CREATED="1345646345829" MODIFIED="1345646348858">
+<node TEXT="Google Scholar&apos;s Ranking Algorithm -- An Introductory Overview.pdf" ID="ID_508046962" CREATED="1345719440406" MODIFIED="1345721018957" LINK="workspace:/literature_repository/docear_example_pdfs/Google%20Scholar&apos;s%20Ranking%20Algorithm%20--%20An%20Introductory%20Overview.pdf">
+<pdf_annotation type="PDF_FILE"/>
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Google Scholar&apos;s Ranking Algorithm -- The Impact of Citation Counts.pdf" ID="ID_1449904402" CREATED="1345719440421" MODIFIED="1345721022533" LINK="workspace:/literature_repository/docear_example_pdfs/Google%20Scholar&apos;s%20Ranking%20Algorithm%20--%20The%20Impact%20of%20Citation%20Counts.pdf">
+<pdf_annotation type="PDF_FILE"/>
+</node>
+<node TEXT="Information Retrieval on Mind Maps -- What could it be good for.pdf" ID="ID_1346712303" CREATED="1345719440437" MODIFIED="1345721025901" LINK="workspace:/literature_repository/docear_example_pdfs/Information%20Retrieval%20on%20Mind%20Maps%20--%20What%20could%20it%20be%20good%20for.pdf">
+<pdf_annotation type="PDF_FILE"/>
+</node>
+</node>
+</node>
+<node TEXT="By category" ID="ID_1559932570" CREATED="1345646368422" MODIFIED="1345646641790">
+<node TEXT="Academic Search" ID="ID_1840009540" CREATED="1345646389806" MODIFIED="1345646392018">
+<node TEXT="Google Scholar&apos;s Ranking Algorithm -- An Introductory&#xa;Overview.pdf" ID="ID_859276746" CREATED="1345719440406" MODIFIED="1345721032077" LINK="workspace:/literature_repository/docear_example_pdfs/Google%20Scholar&apos;s%20Ranking%20Algorithm%20--%20An%20Introductory%20Overview.pdf">
+<pdf_annotation type="PDF_FILE"/>
+</node>
+<node TEXT="Google Scholar&apos;s Ranking Algorithm -- The Impact of&#xa;Citation Counts.pdf" ID="ID_896226000" CREATED="1345719440421" MODIFIED="1345721040028" LINK="workspace:/literature_repository/docear_example_pdfs/Google%20Scholar&apos;s%20Ranking%20Algorithm%20--%20The%20Impact%20of%20Citation%20Counts.pdf">
+<pdf_annotation type="PDF_FILE"/>
+</node>
+<node TEXT="Academic Search Engine Optimization (ASEO) --&#xa;Optimizing Scholarly Literature for Google Scholar and Co.pdf" ID="ID_1949034820" CREATED="1345719440505" MODIFIED="1345721050789" LINK="workspace:/literature_repository/docear_example_pdfs/Academic%20Search%20Engine%20Optimization%20(ASEO)%20--%20Optimizing%20Scholarly%20Literature%20for%20Google%20Scholar%20and%20Co.pdf">
+<pdf_annotation type="PDF_FILE"/>
+<attribute NAME="key" VALUE="Beel10"/>
+<attribute NAME="journal" VALUE="Journal of Scholarly Publishing"/>
+<attribute NAME="authors" VALUE="{J}oeran {B}eel and {B}ela {G}ipp and {E}rik {W}ilde"/>
+<attribute NAME="title" VALUE="{A}cademic {S}earch {E}ngine {O}ptimization ({ASEO}): {O}ptimizing {S}cholarly {L}iterature for {G}oogle {S}cholar and {C}o."/>
+<attribute NAME="year" VALUE="2010"/>
+</node>
+<node TEXT="Academic search engine spam and Google Scholars&#xa;resilience against it.pdf" ID="ID_1324525201" CREATED="1345719440544" MODIFIED="1345721057150" LINK="workspace:/literature_repository/docear_example_pdfs/Academic%20search%20engine%20spam%20and%20Google%20Scholars%20resilience%20against%20it.pdf">
+<pdf_annotation type="PDF_FILE"/>
+<attribute NAME="key" VALUE="Beel2010"/>
+<attribute NAME="journal" VALUE="Journal of Electronic Publishing"/>
+<attribute NAME="authors" VALUE="{J}oeran {B}eel and {B}ela {G}ipp"/>
+<attribute NAME="title" VALUE="{A}cademic search engine spam and {G}oogle {S}cholar&apos;s resilience against it"/>
+<attribute NAME="year" VALUE="2010"/>
+</node>
+</node>
+<node TEXT="Mind Maps" ID="ID_243626125" CREATED="1345646392613" MODIFIED="1345646396474">
+<node TEXT="An Exploratory Analysis of Mind Maps.pdf" ID="ID_86954645" CREATED="1345646448795" MODIFIED="1345709528986" LINK="workspace:/literature_repository/docear_example_pdfs/An%20Exploratory%20Analysis%20of%20Mind%20Maps.pdf">
+<pdf_annotation type="PDF_FILE"/>
+</node>
+<node TEXT="Docear -- An Academic Literature Suite.pdf" ID="ID_1961778618" CREATED="1345719440587" MODIFIED="1345719527227" LINK="workspace:/literature_repository/docear_example_pdfs/Docear%20--%20An%20Academic%20Literature%20Suite.pdf">
+<pdf_annotation type="PDF_FILE"/>
+<attribute NAME="key" VALUE="Beel2011c"/>
+<attribute NAME="authors" VALUE="{B}eel, {J}oeran and {G}ipp, {B}ela and {L}anger, {S}tefan and {G}enzmehr, {M}arcel"/>
+<attribute NAME="title" VALUE="{D}ocear: {A}n {A}cademic {L}iterature {S}uite for {S}earching, {O}rganizing and {C}reating {A}cademic {L}iterature"/>
+<attribute NAME="year" VALUE="2011"/>
+</node>
+</node>
+<node TEXT="Information Retrieval&#xa;&amp; User Modeling" ID="ID_511389851" CREATED="1345646398613" MODIFIED="1345646417001">
+<node TEXT="Information Retrieval on Mind Maps -- What could it&#xa;be good for.pdf" ID="ID_1102927270" CREATED="1345719440437" MODIFIED="1345721063780" LINK="workspace:/literature_repository/docear_example_pdfs/Information%20Retrieval%20on%20Mind%20Maps%20--%20What%20could%20it%20be%20good%20for.pdf">
+<pdf_annotation type="PDF_FILE"/>
+</node>
+</node>
+</node>
+</node>
+<node LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="left" ID="ID_989309610" CREATED="1344498811808" MODIFIED="1345721103854" HGAP="-246" VSHIFT="-125"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      In this mind map you may organize your own publications for quick access.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <font size="1">To delete this node, select it and press DEL on your keyboard</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</map>
---
+++ docear_plugin_core/resources/demo/template_temp.mm
@@ -0,0 +1,77 @@
+<map version="docear 1.0">
+<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
+<attribute_registry SHOW_ATTRIBUTES="hide"/>
+<node TEXT="Temp" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1345646693948"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2"/>
+<node LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="left" ID="ID_989309610" CREATED="1344498811808" MODIFIED="1345646937216" HGAP="-377" VSHIFT="-147"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Copy PDFs and annotations you don't want to sort directly to the Literature &amp; Annotations map into this temporary map.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <font size="1">To delete this node, select it and press DEL on your keyboard</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</map>
