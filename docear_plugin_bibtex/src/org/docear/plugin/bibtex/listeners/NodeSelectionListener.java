7b18f2763bd34ec15121148ab47efb6981a1adf3
Marcel Genzmehr <genzmehr@docear.org>
2012-11-13 15:57:17 +0100

    #724: NPE after Docear start

--- docear_plugin_bibtex/src/org/docear/plugin/bibtex/listeners/NodeSelectionListener.java
+++ docear_plugin_bibtex/src/org/docear/plugin/bibtex/listeners/NodeSelectionListener.java
@@ -24,14 +24,19 @@ public class NodeSelectionListener implements INodeSelectionListener {
 
 
 	public void onSelect(NodeModel node) {
-		NodeAttributeTableModel attributes = AttributeController.getController(MModeController.getMModeController()).createAttributeTableModel(Controller.getCurrentModeController().getMapController().getSelectedNode());
-		attributes.addTableModelListener(ReferencesController.getController().getAttributeListener());
-		//DOCEAR - only enabled when no entry is selected 
-		if(ReferencesController.getController().getJabrefWrapper().getBasePanel().getSelectedEntries().length < 1) {
-			if(attributes.getAttributeKeyList().contains(TextUtils.getText("bibtex_key"))) {
-				String bibKey = (String)attributes.getAttribute(attributes.getAttributePosition(TextUtils.getText("bibtex_key"))).getValue();
-				JabRefCommons.showInReferenceManager(bibKey);
-			}			
+		try {
+			NodeAttributeTableModel attributes = AttributeController.getController(MModeController.getMModeController()).createAttributeTableModel(Controller.getCurrentModeController().getMapController().getSelectedNode());
+			attributes.addTableModelListener(ReferencesController.getController().getAttributeListener());
+			//DOCEAR - only enabled when no entry is selected 
+			if(ReferencesController.getController().getJabrefWrapper().getBasePanel().getSelectedEntries().length < 1) {
+				if(attributes.getAttributeKeyList().contains(TextUtils.getText("bibtex_key"))) {
+					String bibKey = (String)attributes.getAttribute(attributes.getAttributePosition(TextUtils.getText("bibtex_key"))).getValue();
+					JabRefCommons.showInReferenceManager(bibKey);
+				}			
+			}
+		}
+		catch (NullPointerException e) {
+			// in case the basePanel is not existent (anymore)
 		}
 	}
 
