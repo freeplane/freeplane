84aef22a384dae7aa5a5b096c6b04326ad6bbbe3
Marcel Genzmehr <genzmehr@docear.org>
2013-05-02 18:13:41 +0200

    fixed problem when project name and pathname are different

--- freeplane_plugin_workspace/src/org/freeplane/plugin/workspace/model/project/AWorkspaceProject.java
+++ freeplane_plugin_workspace/src/org/freeplane/plugin/workspace/model/project/AWorkspaceProject.java
@@ -1,11 +1,9 @@
 package org.freeplane.plugin.workspace.model.project;
 
-import java.io.File;
 import java.net.URI;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import org.freeplane.plugin.workspace.URIUtils;
 import org.freeplane.plugin.workspace.model.WorkspaceModelEvent;
 import org.freeplane.plugin.workspace.model.WorkspaceModelEvent.WorkspaceModelEventType;
 
@@ -102,11 +100,12 @@ public abstract class AWorkspaceProject {
 
 		public void treeNodesChanged(WorkspaceModelEvent event) {
 			if(event.getType() == WorkspaceModelEventType.RENAMED && getModel().getRoot().equals(event.getTreePath().getLastPathComponent())) {
-				File file = URIUtils.getAbsoluteFile(getProjectHome());
-				File targetFile = new File(file.getParentFile(), getModel().getRoot().getName());
-				if(file.exists()) {
-					file.renameTo(targetFile);
-				}
+				//WORKSPACE - info: if the project folder should have the same name as the project
+//				File file = URIUtils.getAbsoluteFile(getProjectHome());
+//				File targetFile = new File(file.getParentFile(), getModel().getRoot().getName());
+//				if(file.exists()) {
+//					file.renameTo(targetFile);
+//				}
 				//setProjectHome(targetFile.toURI());
 				
 			}
