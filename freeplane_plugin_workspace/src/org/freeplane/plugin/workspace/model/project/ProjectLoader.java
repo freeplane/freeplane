f8bcdddf2ae5c50138251c4c3ace8aae86089c3c
Marcel Genzmehr <genzmehr@docear.org>
2013-05-06 11:00:27 +0200

    removed "misc" folder from default project initiation

--- freeplane_plugin_workspace/src/org/freeplane/plugin/workspace/model/project/ProjectLoader.java
+++ freeplane_plugin_workspace/src/org/freeplane/plugin/workspace/model/project/ProjectLoader.java
@@ -8,14 +8,12 @@ import java.io.InputStreamReader;
 import java.io.Writer;
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.util.Locale;
 
 import org.apache.commons.io.IOExceptionWithCause;
 import org.freeplane.core.io.ReadManager;
 import org.freeplane.core.io.WriteManager;
 import org.freeplane.core.io.xml.TreeXmlReader;
 import org.freeplane.core.util.LogUtils;
-import org.freeplane.core.util.TextUtils;
 import org.freeplane.n3.nanoxml.XMLException;
 import org.freeplane.plugin.workspace.URIUtils;
 import org.freeplane.plugin.workspace.creator.ActionCreator;
@@ -31,9 +29,6 @@ import org.freeplane.plugin.workspace.io.xml.ProjectSettingsWriter;
 import org.freeplane.plugin.workspace.model.AWorkspaceNodeCreator;
 import org.freeplane.plugin.workspace.model.AWorkspaceTreeNode;
 import org.freeplane.plugin.workspace.model.IResultProcessor;
-import org.freeplane.plugin.workspace.nodes.AFolderNode;
-import org.freeplane.plugin.workspace.nodes.FolderTypeMyFilesNode;
-import org.freeplane.plugin.workspace.nodes.FolderVirtualNode;
 import org.freeplane.plugin.workspace.nodes.ProjectRootNode;
 
 public class ProjectLoader implements IProjectSettingsIOHandler {
@@ -177,10 +172,6 @@ public class ProjectLoader implements IProjectSettingsIOHandler {
 		project.getModel().setRoot(root);
 		// create and load all default nodes
 		root.initiateMyFile(project);
-		FolderVirtualNode misc = new FolderVirtualNode(AFolderNode.FOLDER_TYPE_VIRTUAL);
-		misc.setName(TextUtils.getText(FolderTypeMyFilesNode.class.getPackage().getName().toLowerCase(Locale.ENGLISH)+".miscnode.name"));
-		project.getModel().addNodeTo(misc, root);
-		//misc -> help.mm
 		root.refresh();
 	}
 	
