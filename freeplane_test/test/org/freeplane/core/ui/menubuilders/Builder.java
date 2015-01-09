439f6a1328d01d23c56356f204c2440ab0ff6804
dpolivaev <dpolivaev>
2015-01-09 17:18:54 +0100

    remove unused constand ILLEGAL_BUILDER

--- freeplane_test/test/org/freeplane/core/ui/menubuilders/Builder.java
+++ freeplane_test/test/org/freeplane/core/ui/menubuilders/Builder.java
@@ -1,13 +1,6 @@
 package org.freeplane.core.ui.menubuilders;
 
 public interface Builder {
-	public static Builder ILLEGAL_BUILDER = new Builder() {
-		@Override
-		public void build(Entry target) {
-			throw new IllegalStateException("no builder found");
-		}
-	};
-
 	public static Builder EMTPY_BUILDER = new Builder() {
 		@Override
 		public void build(Entry target) {
