eb8480906b7c30c33c571deed58717bad5398aa0
dpolivaev <dpolivaev>
2015-01-24 20:40:08 +0100

    rename acceptance test

--- freeplane/test/org/freeplane/core/ui/menubuilders/MenuBuilderAcceptanceTest.java
+++ freeplane/test/org/freeplane/core/ui/menubuilders/MenuBuilderAcceptanceTest.java
@@ -18,7 +18,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-public class JMenuBuilderTest {
+public class MenuBuilderAcceptanceTest {
 	RecursiveMenuStructureBuilder recursiveMenuStructureBuilder;
 	FreeplaneActions freeplaneActions;
 
