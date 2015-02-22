d90b2c4c9255d9d3f5fa94c7aaeaa9ec6814e051
dpolivaev <dpolivaev>
2015-02-22 21:16:50 +0100

    JComponentRemover

---
+++ freeplane/src/org/freeplane/core/ui/menubuilders/JComponentRemover.java
@@ -0,0 +1,20 @@
+package org.freeplane.core.ui.menubuilders;
+
+import java.awt.Component;
+import java.awt.Container;
+
+public class JComponentRemover implements EntryVisitor{
+
+	@Override
+	public void visit(Entry target) {
+		final Component component = (Component) target.getComponent();
+		final Container parent = component.getParent();
+		parent.remove(component);
+	}
+
+	@Override
+	public boolean shouldSkipChildren() {
+		return true;
+	}
+	
+}
