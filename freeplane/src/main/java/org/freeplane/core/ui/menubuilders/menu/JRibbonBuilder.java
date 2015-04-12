271ce0cf5ad622d9c01f75d54a9c41f3921467bc
Marcel Genzmehr <genzmehr@docear.org>
2015-04-12 18:20:28 +0200

    first ribbon builder test

--- freeplane/src/main/java/org/freeplane/core/ui/menubuilders/menu/JRibbonBuilder.java
+++ freeplane/src/main/java/org/freeplane/core/ui/menubuilders/menu/JRibbonBuilder.java
@@ -1,28 +1,17 @@
 package org.freeplane.core.ui.menubuilders.menu;
 
-import java.awt.Frame;
-
 import org.freeplane.core.ui.IUserInputListenerFactory;
-import org.freeplane.core.ui.components.UITools;
 import org.freeplane.core.ui.menubuilders.generic.Entry;
 import org.freeplane.core.ui.menubuilders.generic.EntryAccessor;
 import org.freeplane.core.ui.menubuilders.generic.EntryVisitor;
 import org.pushingpixels.flamingo.api.ribbon.JRibbon;
-import org.pushingpixels.flamingo.api.ribbon.JRibbonFrame;
 
 public class JRibbonBuilder implements EntryVisitor {
 	private JRibbon ribbon = null;
 
 	public JRibbonBuilder(IUserInputListenerFactory userInputListenerFactory) {
 		super();
-		Frame frame = UITools.getFrame();
-		if(frame instanceof JRibbonFrame) {
-			this.ribbon = ((JRibbonFrame)frame).getRibbon();
-		}
-		else {
-			//TODO RIBBONS - quick hack -> does this work?
-			this.ribbon = new JRibbon();
-		}
+		this.ribbon = userInputListenerFactory.getRibbon();
 	}
 
 	@Override
