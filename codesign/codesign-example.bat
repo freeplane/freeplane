04898e47d89bf6a1991fb1164e2c64ba3f346ad5
dpolivaev <dpolivaev>
2015-10-16 13:30:54 +0200

    codesign-example.bat

---
+++ codesign/codesign-example.bat
@@ -0,0 +1,4 @@
+set PASSWORD=yourPassword
+set KEYSTORE=freeplane.pfx
+"C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\signtool.exe" sign /t http://time.certum.pl /f "%~dp0%KEYSTORE%" /p %PASSWORD% "%~dp0..\DIST\*.exe"
+pause
