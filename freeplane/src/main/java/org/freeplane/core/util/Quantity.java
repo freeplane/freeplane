8a4142c0d40b1b56fe50a2704b72ccc5adbf95ec
dpolivaev <dpolivaev>
2015-10-20 12:29:20 +0200

    Quantity

---
+++ freeplane/src/main/java/org/freeplane/core/util/Quantity.java
@@ -0,0 +1,17 @@
+package org.freeplane.core.util;
+
+public class Quantity <U extends Enum<U> & Convertible >{
+
+	final public double value;
+	final public U unit;
+
+	public Quantity(double value, U unit) {
+		this.value = value;
+		this.unit = unit;
+	}
+
+	public double inBaseUnits() {
+		return value * unit.factor();
+	}
+
+}
