87566ddce0f37818de595a5ed42ea80244aa4253
dpolivaev <dpolivaev>
2015-10-20 12:53:46 +0200

    Convert Quantity to and from String

--- freeplane/src/main/java/org/freeplane/core/util/Quantity.java
+++ freeplane/src/main/java/org/freeplane/core/util/Quantity.java
@@ -2,6 +2,15 @@ package org.freeplane.core.util;
 
 public class Quantity <U extends Enum<U> & Convertible >{
 
+	public static <U extends Enum<U> & Convertible>  Quantity<U> fromString(String valueString, Class<U> unitClass) {
+		int separatorPosition = valueString.lastIndexOf(' ');
+		String numberString = valueString.substring(0, separatorPosition);
+		double doubleValue = Double.parseDouble(numberString);
+		String unitString = valueString.substring(separatorPosition + 1);
+		U unit = Enum.valueOf(unitClass, unitString);
+		return new Quantity<U>(doubleValue, unit);
+	}
+
 	final public double value;
 	final public U unit;
 
@@ -17,5 +26,40 @@ public class Quantity <U extends Enum<U> & Convertible >{
 	public int inBaseUnitsRounded() {
 		return (int) (inBaseUnits() + 0.5d);
 	}
+
+	@Override
+	public String toString() {
+		return value + " " + unit;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + unit.hashCode();
+		long temp;
+		temp = Double.doubleToLongBits(value);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		@SuppressWarnings("rawtypes")
+		Quantity other = (Quantity) obj;
+		if (!unit.equals(other.unit))
+			return false;
+		if (Double.doubleToLongBits(value) != Double.doubleToLongBits(other.value))
+			return false;
+		return true;
+	}
+	
+	
 
 }
