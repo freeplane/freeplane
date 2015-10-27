5df630d5833cb0250d15a8f5f42cdb996680d69a
Dimitry Polivaev <dpolivaev@gmx.de>
2015-10-27 22:18:54 +0100

    EditStylesAction should change to mapView null before opening style dialog because it has its own controller # Conflicts: #	freeplane/src/org/freeplane/main/mindmapmode/stylemode/AEditStylesAction.java

--- freeplane/src/org/freeplane/main/mindmapmode/stylemode/EditStylesAction.java
+++ freeplane/src/org/freeplane/main/mindmapmode/stylemode/EditStylesAction.java
@@ -21,8 +21,16 @@ package org.freeplane.main.mindmapmode.stylemode;
 
 import java.awt.Component;
 import java.awt.event.ActionEvent;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 
+import org.freeplane.core.resources.WindowConfigurationStorage;
+import org.freeplane.core.ui.AFreeplaneAction;
 import org.freeplane.core.ui.components.UITools;
 import org.freeplane.core.undo.IUndoHandler;
 import org.freeplane.core.util.TextUtils;
@@ -31,39 +39,52 @@ import org.freeplane.features.mode.Controller;
 import org.freeplane.features.mode.ModeController;
 import org.freeplane.features.styles.LogicalStyleController;
 import org.freeplane.features.styles.MapStyleModel;
+import org.freeplane.features.ui.IMapViewManager;
+import org.freeplane.features.ui.ViewController;
 
 /**
  * @author Dimitry Polivaev
  * 13.09.2009
  */
 @SuppressWarnings("serial")
-public class EditStylesAction extends AEditStylesAction {
+public class EditStylesAction extends AFreeplaneAction {
+	private Controller mainController = null;
+	private SModeController modeController;
+	protected JDialog dialog;
+
 	public EditStylesAction() {
 		super("EditStylesAction");
 	}
 	
 	public void actionPerformed(final ActionEvent e) {
-		final MapModel map = Controller.getCurrentController().getMap();
+		final Controller currentController = Controller.getCurrentController();
+		final MapModel map = currentController.getMap();
 		final MapStyleModel mapStyleModel = MapStyleModel.getExtension(map);
 		final MapModel styleMap = mapStyleModel.getStyleMap();
 		if(styleMap == null){
 			UITools.errorMessage(TextUtils.getText("no_styles_found_in_map"));
 			return;
 		}
+		
+		final IMapViewManager mapViewManager = currentController.getMapViewManager();
+		final Component currentMapView = mapViewManager.getMapViewComponent();
+		mapViewManager.changeToMapView((Component)null);
+		
 		final IUndoHandler undoHandler = (IUndoHandler) map.getExtension(IUndoHandler.class);
 		undoHandler.startTransaction();
-		init();
+		init(currentMapView);
 		SModeController modeController = getModeController();
 		modeController.getMapController().newMapView(styleMap);
 		Controller controller = modeController.getController();
 		Component mapViewComponent = controller.getMapViewManager().getMapViewComponent();
 		((DialogController) controller.getViewController()).setMapView(mapViewComponent);
-		dialog.setLocationRelativeTo(Controller.getCurrentController().getViewController().getJFrame());
+		dialog.setLocationRelativeTo(currentController.getViewController().getJFrame());
 		dialog.setVisible(true);
 	}
 
 	void commit() {
-	    final MapModel currentMap = Controller.getCurrentController().getMap();
+	    final Controller currentController = Controller.getCurrentController();
+		final MapModel currentMap = currentController.getMap();
 	    LogicalStyleController.getController().refreshMap(currentMap);
 	    final ModeController currentModeController = Controller.getCurrentModeController();
 		currentModeController.commit();
@@ -72,4 +93,55 @@ public class EditStylesAction extends AEditStylesAction {
 	void rollback() {
 	    Controller.getCurrentModeController().rollback();
     }
+
+	SModeController getModeController() {
+	    return modeController;
+	}
+
+	private void init(final Component currentMapView) {
+		this.mainController = Controller.getCurrentController();
+		if (dialog != null) {
+			Controller.setCurrentController ((Controller) dialog.getRootPane().getClientProperty(Controller.class));
+			return;
+		}
+		dialog = new JDialog(Controller.getCurrentController().getViewController().getJFrame());
+		final WindowConfigurationStorage windowConfigurationStorage = new WindowConfigurationStorage(getKey() + ".dialog");
+		windowConfigurationStorage.restoreDialogPositions(dialog);
+		dialog.setModal(true);
+		dialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+		dialog.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(final WindowEvent e) {
+				getModeController().tryToCloseDialog();
+			}
+		});
+		Controller styleDialogController = SModeControllerFactory.getInstance().createController(dialog);
+		modeController = (SModeController) styleDialogController.getModeController();
+		final ViewController viewController = styleDialogController.getViewController();
+		viewController.init(styleDialogController);
+		dialog.addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentHidden(final ComponentEvent e) {
+		    	final WindowConfigurationStorage windowConfigurationStorage = new WindowConfigurationStorage(getKey() + ".dialog");
+		    	windowConfigurationStorage.storeDialogPositions(dialog);
+				final IMapViewManager mapViewManager = modeController.getController().getMapViewManager();
+				final MapModel map = mapViewManager.getModel();
+				final IUndoHandler undoHandler = (IUndoHandler) map.getExtension(IUndoHandler.class);
+				mapViewManager.close(true);
+				Controller.setCurrentController(mainController);
+				super.componentHidden(e);
+				mainController.getMapViewManager().changeToMapView(currentMapView);
+				switch (modeController.getStatus()) {
+					case JOptionPane.OK_OPTION:
+						if (undoHandler.canUndo()) {
+							commit();
+							break;
+						}
+					case JOptionPane.CANCEL_OPTION:
+						rollback();
+				}
+			}
+	
+		});
+	}
 }
