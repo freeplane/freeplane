b7e5d8c9701ae710fed85c109f545282ae8fe2a0
Dimitry Polivaev <dpolivaev@gmx.de>
2015-10-29 13:22:00 +0100

    ObjectRule

---
+++ freeplane/src/test/java/org/freeplane/core/util/ConstantObjectShould.java
@@ -0,0 +1,30 @@
+package org.freeplane.core.util;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.freeplane.core.util.ConstantObject;
+import org.freeplane.core.util.ObjectRule;
+import org.junit.Test;
+
+public class ConstantObjectShould {
+	enum Rules {RULE}
+	@Test
+	public void returnItsObject() throws Exception {
+		 ObjectRule <String, Rules> objectReference = new ConstantObject<String, Rules>("string");
+		 assertThat(objectReference.getValue(), equalTo("string"));
+	}
+
+	@Test
+	public void containItsObject() throws Exception {
+		 ObjectRule <String, Rules> objectReference = new ConstantObject<String, Rules>("string");
+		 assertThat(objectReference.hasValue(), equalTo(true));
+	}
+
+	@Test
+	public void ignoreReset() throws Exception {
+		 ObjectRule <String, Rules> objectReference = new ConstantObject<String, Rules>("string");
+		 objectReference.resetCache();
+		 assertThat(objectReference.hasValue(), equalTo(true));
+	}
+}
---
+++ freeplane/src/test/java/org/freeplane/core/util/RuleReferenceShould.java
@@ -0,0 +1,54 @@
+package org.freeplane.core.util;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.freeplane.core.util.ObjectRule;
+import org.freeplane.core.util.RuleReference;
+import org.junit.Test;
+
+public class RuleReferenceShould {
+	enum Rules {RULE}
+	@Test
+	public void returnRule() throws Exception {
+		 ObjectRule <String, Rules> objectReference = new RuleReference<String, Rules>(Rules.RULE);
+		 assertThat(objectReference.getRule(), equalTo(Rules.RULE));
+	}
+
+	@Test
+	public void containsNoObjectAfterInitialization() throws Exception {
+		 ObjectRule <String, Rules> objectReference = new RuleReference<String, Rules>(Rules.RULE);
+		 assertThat(objectReference.hasValue(), equalTo(false));
+	}
+	
+
+	@Test
+	public void returnSetObject() throws Exception {
+		 ObjectRule <String, Rules> objectReference = new RuleReference<String, Rules>(Rules.RULE);
+		 objectReference.setCache("string");
+		 assertThat(objectReference.getValue(), equalTo("string"));
+	}
+	
+	@Test
+	public void containObjectAfterItIsSet() throws Exception {
+		 ObjectRule <String, Rules> objectReference = new RuleReference<String, Rules>(Rules.RULE);
+		 objectReference.setCache("string");
+		 assertThat(objectReference.hasValue(), equalTo(true));
+	}
+
+	@Test
+	public void removeValueAfterReset() throws Exception {
+		 ObjectRule <String, Rules> objectReference = new RuleReference<String, Rules>(Rules.RULE);
+		 objectReference.setCache("string");
+		 objectReference.resetCache();
+		 assertThat(objectReference.hasValue(), equalTo(false));
+	}
+
+//
+//	@Test
+//	public void ignoreReset() throws Exception {
+//		 ObjectReference <String, Rules> objectReference = new RuleReference<String, Rules>(Rules.RULE);
+//		 objectReference.reset();
+//		 assertThat(objectReference.hasValue(), equalTo(true));
+//	}
+}
