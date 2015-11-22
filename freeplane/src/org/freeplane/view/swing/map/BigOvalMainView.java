7e534bb16bcc5edd16d89e826e0c46209b05d63d
Dimitry Polivaev <dpolivaev@gmx.de>
2015-11-22 12:45:58 +0100

    Refactor : move getConnectorPoint from OvalMainView to BigOvalMainView

--- freeplane/src/org/freeplane/view/swing/map/BigOvalMainView.java
+++ freeplane/src/org/freeplane/view/swing/map/BigOvalMainView.java
@@ -1,5 +1,7 @@
 package org.freeplane.view.swing.map;
 
+import java.awt.Point;
+
 import org.freeplane.features.nodestyle.NodeStyleModel.Shape;
 
 @SuppressWarnings("serial")
@@ -12,5 +14,11 @@ public class BigOvalMainView extends OvalMainView {
     public
     Shape getShape() {
 		return Shape.oval;
+	}
+
+	@Override
+	public Point getConnectorPoint(Point p) {
+		return USE_COMMON_OUT_POINT_FOR_ROOT_NODE || ! getNodeView().isRoot() ? 
+				super.getConnectorPoint(p) : getConnectorPointAtTheOvalBorder(p);
 	}
 }
