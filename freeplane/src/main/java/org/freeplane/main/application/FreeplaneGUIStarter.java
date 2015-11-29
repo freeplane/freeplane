e1c4982cdd4206dffb6fb11c8da367f4b67fa5f7
Dimitry Polivaev <dpolivaev@gmx.de>
2015-11-29 08:28:49 +0100

    Set frame visible after maps and windows are loaded

--- freeplane/src/main/java/org/freeplane/main/application/FreeplaneGUIStarter.java
+++ freeplane/src/main/java/org/freeplane/main/application/FreeplaneGUIStarter.java
@@ -315,11 +315,11 @@ public class FreeplaneGUIStarter implements FreeplaneStarter {
 				contentPane.setVisible(false);
 				splash.dispose();
 				splash = null;
-				frame.setVisible(true);
 				if (extendedState != frame.getExtendedState()) {
 					frame.setExtendedState(extendedState);
 				}
 				loadMaps(options.getFilesToOpenAsArray());
+				frame.setVisible(true);
 				focusCurrentView();
 				viewController.getContentPane().setVisible(true);
 				frame.toFront();
