c06c848e5941d764192ee500d77282cdd4bda074
Dimitry Polivaev <dpolivaev@gmx.de>
2015-12-06 15:33:07 +0100

    Map order in teh navigation list depends on tab order

--- freeplane/src/main/java/org/freeplane/view/swing/ui/UserInputListenerFactory.java
+++ freeplane/src/main/java/org/freeplane/view/swing/ui/UserInputListenerFactory.java
@@ -74,6 +74,7 @@ import org.freeplane.features.map.MapModel;
 import org.freeplane.features.mode.Controller;
 import org.freeplane.features.mode.ModeController;
 import org.freeplane.features.ui.IMapViewManager;
+import org.freeplane.features.ui.ViewController;
 import org.freeplane.view.swing.map.MapView;
 
 public class UserInputListenerFactory implements IUserInputListenerFactory {
@@ -357,9 +358,9 @@ public class UserInputListenerFactory implements IUserInputListenerFactory {
 	}
 
 	private void createMapActions(final Entry mapsMenuEntry) {
-		rebuildMenuOnMapChange(mapsMenuEntry);
 		final IMapViewManager mapViewManager = Controller.getCurrentController().getMapViewManager();
-		final List<? extends Component> mapViewVector = mapViewManager.getMapViewVector();
+		final ViewController viewController = Controller.getCurrentController().getViewController();
+		final List<? extends Component> mapViewVector = viewController.getMapViewVector();
 		if (mapViewVector == null) {
 			return;
 		}
