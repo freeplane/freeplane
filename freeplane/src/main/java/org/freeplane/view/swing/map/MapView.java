a976712ea780ac032f863451b03b28507c3f69c1
Dimitry Polivaev <dpolivaev@gmx.de>
2016-02-20 19:47:26 +0100

    log map moves

--- freeplane/src/main/java/org/freeplane/view/swing/map/MapView.java
+++ freeplane/src/main/java/org/freeplane/view/swing/map/MapView.java
@@ -2044,6 +2044,12 @@ public class MapView extends JPanel implements Printable, Autoscroll, IMapChange
 			mapScroller.scrollView();
 	}
 
+	@Override
+	public void setLocation(int x, int y) {
+		new Exception("set map location " + x + ", " + y).printStackTrace(System.out);
+		super.setLocation(x, y);
+	}
+
 	public void scrollBy(int x, int y) {
 		mapScroller.scrollBy(x, y);
 	}
@@ -2055,4 +2061,6 @@ public class MapView extends JPanel implements Printable, Autoscroll, IMapChange
 	public void setAnchorContentLocation() {
 		mapScroller.setAnchorContentLocation();
 	}
+	
+	
 }
