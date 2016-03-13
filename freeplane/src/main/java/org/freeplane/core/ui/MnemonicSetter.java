e2ffd50d6788b6eb0f7f47b80d65f0c0fe83c014
Dimitry Polivaev <dpolivaev@gmx.de>
2016-03-13 20:14:32 +0100

    Do not try to set mnemonics on Mac OS

--- freeplane/src/main/java/org/freeplane/core/ui/MnemonicSetter.java
+++ freeplane/src/main/java/org/freeplane/core/ui/MnemonicSetter.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
+import org.freeplane.core.util.Compat;
+
 public class MnemonicSetter {
 	
 	static class MnemonicMap extends LinkedHashMap<Integer, INameMnemonicHolder> {
@@ -44,6 +46,8 @@ public class MnemonicSetter {
 	}
 
 	public void setMnemonics() {
+		if(Compat.isMacOsX())
+			return; // Mac OS generally does not support mnemonics
 		final Map<Integer, INameMnemonicHolder> usedMnemonics = extractUsedMnemonics(mnemonicHolders);
 		if(usedMnemonics.size() < mnemonicHolders.length) {
 			final Map<Integer, INameMnemonicHolder> mnemonicSet = findMnemonics(usedMnemonics, 0, 0);
