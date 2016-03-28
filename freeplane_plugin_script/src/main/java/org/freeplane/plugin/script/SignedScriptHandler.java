f58cab171984b8b0a73f7e0cbe2cb50fc4d90fbd
Dimitry Polivaev <dpolivaev@gmx.de>
2016-03-28 21:03:15 +0200

    Remove FreeMind script certificate

--- freeplane_plugin_script/src/main/java/org/freeplane/plugin/script/SignedScriptHandler.java
+++ freeplane_plugin_script/src/main/java/org/freeplane/plugin/script/SignedScriptHandler.java
@@ -20,7 +20,6 @@
  */
 package org.freeplane.plugin.script;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -29,9 +28,6 @@ import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.PrivateKey;
 import java.security.Signature;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateFactory;
-import java.util.Collection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -40,26 +36,16 @@ import org.freeplane.core.ui.components.EnterPasswordDialog;
 import org.freeplane.core.ui.components.UITools;
 import org.freeplane.core.util.LogUtils;
 import org.freeplane.features.encrypt.DesEncrypter;
-import org.freeplane.features.mode.Controller;
 
-/**
- * @author foltin
- */
+
 class SignedScriptHandler {
-	public static class ScriptContents {
-		private static Pattern sSignWithKeyPattern = null;
-		public String mKeyName;
-		public String mScript;
-		public String mSignature;
-
-		public ScriptContents() {
-			if (ScriptContents.sSignWithKeyPattern == null) {
-				ScriptContents.sSignWithKeyPattern = Pattern.compile(SignedScriptHandler.SIGN_PREFIX_REGEXP);
-			}
-		}
+	static class ScriptContents {
+		final private static Pattern SIGNATURE_WITH_KEY = Pattern.compile(SignedScriptHandler.SIGN_PREFIX_REGEXP);
+		String mKeyName;
+		String mScript;
+		String mSignature;
 
 		public ScriptContents(final String pScript) {
-			this();
 			final int indexOfSignaturePrefix = pScript.lastIndexOf(SignedScriptHandler.SIGN_PREFIX);
 			final int indexOfSignature = indexOfSignaturePrefix + SignedScriptHandler.SIGN_PREFIX.length();
 			if (indexOfSignaturePrefix > 0 && pScript.length() > indexOfSignature) {
@@ -68,7 +54,7 @@ class SignedScriptHandler {
 				mKeyName = null;
 			}
 			else {
-				final Matcher matcher = ScriptContents.sSignWithKeyPattern.matcher(pScript);
+				final Matcher matcher = ScriptContents.SIGNATURE_WITH_KEY.matcher(pScript);
 				if (matcher.find()) {
 					mScript = pScript.substring(0, matcher.start());
 					mKeyName = matcher.group(1);
@@ -99,7 +85,7 @@ class SignedScriptHandler {
 	private static KeyStore mKeyStore = null;
 	private static final String SIGN_PREFIX = "//SIGN:";
 	/** This is for / /SIGN(keyname):signature */
-	private static final String SIGN_PREFIX_REGEXP = "//SIGN\\((.*?)\\):(.*)";
+	private static final String SIGN_PREFIX_REGEXP = "//SIGN\\((.+?)\\):(.*)";
 
 	public SignedScriptHandler() {
 	}
@@ -138,42 +124,15 @@ class SignedScriptHandler {
 			try {
 				final Signature instanceVerify = Signature.getInstance("SHA1withDSA");
 				if (content.mKeyName == null) {
-					/**
-					 * This is the Freeplane public key. keytool -v -rfc
-					 * -exportcert -alias freeplanescriptkey
-					 */
-					final String cer = "-----BEGIN CERTIFICATE-----\n"
-					        + "MIIDKDCCAuWgAwIBAgIESAY2ADALBgcqhkjOOAQDBQAwdzELMAkGA1UEBhMCREUxCzAJBgNVBAgT"
-					        + "AkRFMRMwEQYDVQQHEwpPcGVuU291cmNlMRgwFgYDVQQKEw9zb3VyY2Vmb3JnZS5uZXQxETAPBgNV"
-					        + "BAsTCEZyZWVNaW5kMRkwFwYDVQQDExBDaHJpc3RpYW4gRm9sdGluMB4XDTA4MDQxNjE3MjMxMloX"
-					        + "DTA4MDcxNTE3MjMxMlowdzELMAkGA1UEBhMCREUxCzAJBgNVBAgTAkRFMRMwEQYDVQQHEwpPcGVu"
-					        + "U291cmNlMRgwFgYDVQQKEw9zb3VyY2Vmb3JnZS5uZXQxETAPBgNVBAsTCEZyZWVNaW5kMRkwFwYD"
-					        + "VQQDExBDaHJpc3RpYW4gRm9sdGluMIIBtzCCASwGByqGSM44BAEwggEfAoGBAP1/U4EddRIpUt9K"
-					        + "nC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVCl"
-					        + "pJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3R"
-					        + "SAHHAhUAl2BQjxUjC8yykrmCouuEC/BYHPUCgYEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdM"
-					        + "Cz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/"
-					        + "C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoDgYQAAoGAZm5z5EZX"
-					        + "Vhtye5jY3X9w24DJ3yNJbNl2tfkOBIc0KfgyxONTSJKtUpmLI3btUxy3pQf/T8BShlY3PAC0fp3M"
-					        + "eDG8WRq1wM3luLd1V9SS8EG6tPJBZ3mciCUymTT7n9CZNzATIpqNIXHSD/wljRABedUi8PMg4KbV"
-					        + "Pnhu6Y6b1uAwCwYHKoZIzjgEAwUAAzAAMC0CFQCFHGwe+HHOvY0MmKYHbiq7fRxMGwIUC0voAGYU"
-					        + "u6vgVFqdLI5F96JLTqk=" + "\n-----END CERTIFICATE-----\n";
-					final CertificateFactory cf = CertificateFactory.getInstance("X.509");
-					final Collection<? extends Certificate> c = cf.generateCertificates(new ByteArrayInputStream(cer
-					    .getBytes()));
-					if (c.isEmpty())
-						throw new IllegalArgumentException("Internal certificate wrong.");
-					for (Certificate cert : c) {
-						instanceVerify.initVerify(cert);
-					}
+					return false;
 				}
 				else {
 					initializeKeystore(null);
 					instanceVerify.initVerify(SignedScriptHandler.mKeyStore.getCertificate(content.mKeyName));
+					instanceVerify.update(content.mScript.getBytes());
+					final boolean verify = instanceVerify.verify(DesEncrypter.fromBase64(content.mSignature));
+					return verify;
 				}
-				instanceVerify.update(content.mScript.getBytes());
-				final boolean verify = instanceVerify.verify(DesEncrypter.fromBase64(content.mSignature));
-				return verify;
 			}
 			catch (final Exception e) {
 				LogUtils.severe(e);
@@ -201,13 +160,19 @@ class SignedScriptHandler {
 		initializeKeystore(password);
 		try {
 			final Signature instance = Signature.getInstance("SHA1withDSA");
-			String keyName = SignedScriptHandler.FREEPLANE_SCRIPT_KEY_NAME;
-			final String propertyKeyName = ResourceController.getResourceController().getProperty(
+			String keyName = FREEPLANE_SCRIPT_KEY_NAME;
+			final ResourceController resourceController = ResourceController.getResourceController();
+			String propertyKeyName = resourceController.getProperty(
 			    ScriptingPermissions.RESOURCES_SCRIPT_USER_KEY_NAME_FOR_SIGNING);
+			if (propertyKeyName == null || propertyKeyName.trim().length() == 0){
+				resourceController.setProperty(ScriptingPermissions.RESOURCES_SCRIPT_USER_KEY_NAME_FOR_SIGNING, 
+						FREEPLANE_SCRIPT_KEY_NAME);
+				propertyKeyName = keyName;
+			}
 			if (content.mKeyName != null) {
 				keyName = content.mKeyName;
 			}
-			else if (propertyKeyName != null && propertyKeyName.length() > 0) {
+			else {
 				content.mKeyName = propertyKeyName;
 				keyName = content.mKeyName;
 			}
