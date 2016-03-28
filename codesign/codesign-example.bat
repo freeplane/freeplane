aa557f0ce820a88ebcb36be39531085983f5953a
Dimitry Polivaev <dpolivaev@gmx.de>
2016-03-28 12:09:00 +0200

    adjust codesign-example.bat to reflect changes on provider site

--- codesign/codesign-example.bat
+++ codesign/codesign-example.bat
@@ -1,4 +1,4 @@
 set PASSWORD=yourPassword
 set KEYSTORE=freeplane.pfx
-"C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\signtool.exe" sign /t http://time.certum.pl /f "%~dp0%KEYSTORE%" /p %PASSWORD% "%~dp0..\DIST\*.exe"
+"C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\signtool.exe" sign /tr http://time.certum.pl /f "%~dp0%KEYSTORE%" /p %PASSWORD% "%~dp0..\DIST\*.exe"
 pause
