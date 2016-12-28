42865910dff77472c1e0b1c30636a8f28266ef39
Dimitry Polivaev <dpolivaev@gmx.de>
2016-12-28 14:41:06 +0100

    Don't remove unscalable menu icons

--- freeplane/src/main/java/org/freeplane/core/ui/menubuilders/menu/MenuIconScaling.java
+++ freeplane/src/main/java/org/freeplane/core/ui/menubuilders/menu/MenuIconScaling.java
@@ -14,15 +14,11 @@ class MenuIconScaling {
 	static void scaleIcon(final AbstractButton actionComponent) {
 		final Icon icon = actionComponent.getIcon();
 		final ImageIconFactory imageIconFactory = ImageIconFactory.getInstance();
-		if(icon != null) {
-			if (imageIconFactory.canScaleIcon(icon)) {
-				final Font font = actionComponent.getFont();
-				final int fontHeight = actionComponent.getFontMetrics(font).getHeight();
-				final Quantity<LengthUnits> iconHeight = new Quantity<LengthUnits>(fontHeight, LengthUnits.px);
-				actionComponent.setIcon(imageIconFactory.getScaledIcon(icon, iconHeight));
-			}
-			else
-				actionComponent.setIcon(null);
+		if (icon != null && imageIconFactory.canScaleIcon(icon)) {
+			final Font font = actionComponent.getFont();
+			final int fontHeight = actionComponent.getFontMetrics(font).getHeight();
+			final Quantity<LengthUnits> iconHeight = new Quantity<LengthUnits>(fontHeight, LengthUnits.px);
+			actionComponent.setIcon(imageIconFactory.getScaledIcon(icon, iconHeight));
 		}
 	}
 }
