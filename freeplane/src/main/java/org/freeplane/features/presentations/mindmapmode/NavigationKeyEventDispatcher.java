905905d498d9ac03f0fe89c7d4cec4ba1696c8a5
Dimitry Polivaev <dpolivaev@gmx.de>
2017-02-11 12:26:35 +0100

    Process home and end keys to got to first and last slides

--- freeplane/src/main/java/org/freeplane/features/presentations/mindmapmode/NavigationKeyEventDispatcher.java
+++ freeplane/src/main/java/org/freeplane/features/presentations/mindmapmode/NavigationKeyEventDispatcher.java
@@ -14,16 +14,26 @@ class NavigationKeyEventDispatcher implements KeyEventDispatcher {
 		if(e.getModifiers() != 0)
 			return false;
 		switch (e.getKeyCode()) {
+		case KeyEvent.VK_HOME:
+			e.consume();
+			if(e.getID() == KeyEvent.KEY_PRESSED)
+				state.showFirstSlide();
+			return true;
 		case KeyEvent.VK_UP:
 				e.consume();
 				if(e.getID() == KeyEvent.KEY_PRESSED)
 					state.showPreviousSlide();
 				return true;
 		case KeyEvent.VK_DOWN:
-				e.consume();
-				if(e.getID() == KeyEvent.KEY_PRESSED)
-					state.showNextSlide();
-				return true;
+			e.consume();
+			if(e.getID() == KeyEvent.KEY_PRESSED)
+				state.showNextSlide();
+			return true;
+		case KeyEvent.VK_END:
+			e.consume();
+			if(e.getID() == KeyEvent.KEY_PRESSED)
+				state.showLastSlide();
+			return true;
 		default:
 			 return false;
 		}
