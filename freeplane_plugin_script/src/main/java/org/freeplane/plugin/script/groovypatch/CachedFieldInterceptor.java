da33365a40aeb685df6adecdf234d56b6fc8b71f
Dimitry Polivaev <dpolivaev@gmx.de>
2017-04-01 15:35:15 +0200

    Use byte buddy to patch Groovy at run time

---
+++ freeplane_plugin_script/src/main/java/org/freeplane/plugin/script/groovypatch/CachedFieldInterceptor.java
@@ -0,0 +1,21 @@
+package org.freeplane.plugin.script.groovypatch;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.Callable;
+
+import net.bytebuddy.implementation.bind.annotation.FieldValue;
+import net.bytebuddy.implementation.bind.annotation.SuperCall;
+
+public class CachedFieldInterceptor {
+	public static <T> T intercept(@SuperCall Callable<T> c, @FieldValue("field") Field field){
+		try {
+			AccessPermissionChecker.checkAccessPermission(field);
+			return c.call();
+		} catch (RuntimeException e) {
+			throw e;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
