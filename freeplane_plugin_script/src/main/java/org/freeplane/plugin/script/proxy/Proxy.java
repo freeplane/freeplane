4571df77fd35d090c114235131b46cb717a09b01
Dimitry Polivaev <dpolivaev@gmx.de>
2017-07-08 10:51:21 +0200

    Declare and document new attribute proxy methods in the interface

--- freeplane_plugin_script/src/main/java/org/freeplane/plugin/script/proxy/Proxy.java
+++ freeplane_plugin_script/src/main/java/org/freeplane/plugin/script/proxy/Proxy.java
@@ -185,6 +185,14 @@ public interface Proxy {
 		/** sets name and value of the attribute at the given index. This method will not create new attributes.
 		 * @throws IndexOutOfBoundsException if index is out of range, i. e. {@code index < 0 || index >= size()}.*/
 		void set(final int index, final String name, final Object value);
+		
+		/** sets name, value and format pattern of the attribute at the given index. This method will not create new attributes.
+		 * @throws IndexOutOfBoundsException if index is out of range, i. e. {@code index < 0 || index >= size()}.*/
+		public void set(final int index, final String name, final Object value, String format);
+		
+		/** sets format pattern to  the attribute at the given index.
+		 * @throws IndexOutOfBoundsException if index is out of range, i. e. {@code index < 0 || index >= size()}.*/
+		public void setFormat(final int index, String format);
 
 		/** removes the <em>first</em> attribute with this name.
 		 * @return true on removal of an existing attribute and false otherwise.
@@ -206,6 +214,9 @@ public interface Proxy {
 
 		/** adds an attribute no matter if an attribute with the given name already exists. */
 		void add(final String name, final Object value);
+		
+		/** adds an attribute with formatting pattern no matter if an attribute with the given name already exists. */
+		public void add(final String name, final Object value, String format);
 
 		/** removes all attributes.
 		 * @since 1.2 */
