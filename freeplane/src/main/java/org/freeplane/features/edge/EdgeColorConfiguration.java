7c51a227898a9b9b5b8b1f3afb71289ea3ee81e3
Dimitry Polivaev <dpolivaev@gmx.de>
2017-08-19 10:10:30 +0200

    fix edge color configuration related bugs

--- freeplane/src/main/java/org/freeplane/features/edge/EdgeColorConfiguration.java
+++ freeplane/src/main/java/org/freeplane/features/edge/EdgeColorConfiguration.java
@@ -4,7 +4,8 @@ import java.awt.Color;
 import java.util.List;
 
 public class EdgeColorConfiguration{
-	private static final int FIRST_CYCLIC_STYLE_LEVEL = 1;
+	private static final int ROOT_COLOR_INDEX = 0;
+	private static final int FIRST_CYCLIC_COLOR_INDEX = ROOT_COLOR_INDEX + 1;
 	public List<Color> colors;
 
 	public EdgeColorConfiguration(List<Color> colors) {
@@ -19,7 +20,9 @@ public class EdgeColorConfiguration{
 	public Color getEdgeColor(int colorCounter) {
 		if(colorCounter < colors.size())
 			return colors.get(colorCounter);
+		else if(FIRST_CYCLIC_COLOR_INDEX == colors.size())
+			return EdgeController.STANDARD_EDGE_COLOR;
 		else
-			return colors.get(FIRST_CYCLIC_STYLE_LEVEL + (colorCounter - FIRST_CYCLIC_STYLE_LEVEL) % (colors.size() - FIRST_CYCLIC_STYLE_LEVEL)); 
+			return colors.get(FIRST_CYCLIC_COLOR_INDEX + (colorCounter - FIRST_CYCLIC_COLOR_INDEX) % (colors.size() - FIRST_CYCLIC_COLOR_INDEX)); 
 	}
 }
