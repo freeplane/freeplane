93211b2b3295bd74fc289abb3959984df280478b
Dimitry Polivaev <dpolivaev@gmx.de>
2017-10-29 19:58:25 +0100

    Fix test : support guava collections

--- freeplane_plugin_collaboration_client/src/main/java/org/freeplane/plugin/collaboration/client/event/json/Jackson.java
+++ freeplane_plugin_collaboration_client/src/main/java/org/freeplane/plugin/collaboration/client/event/json/Jackson.java
@@ -1,12 +1,14 @@
 package org.freeplane.plugin.collaboration.client.event.json;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 
 public class Jackson {
 	public static ObjectMapper objectMapper = new ObjectMapper(); 
 	static {
 		objectMapper.registerModule(new Jdk8Module());
+		objectMapper.registerModule(new GuavaModule());
 	}
 
 }
