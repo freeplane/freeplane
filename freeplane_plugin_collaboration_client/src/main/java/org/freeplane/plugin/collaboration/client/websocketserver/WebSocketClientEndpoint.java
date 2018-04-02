9081b5d97876f5260461c3aa115aaf5b9075f493
Felix Natter <fnatter@gmx.net>
2018-04-02 18:15:32 +0200

    Client can connect to server using JSR-356/Tyrus

---
+++ freeplane_plugin_collaboration_client/src/main/java/org/freeplane/plugin/collaboration/client/websocketserver/WebSocketClientEndpoint.java
@@ -0,0 +1,80 @@
+package org.freeplane.plugin.collaboration.client.websocketserver;
+
+import javax.websocket.ClientEndpoint;
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+
+import org.freeplane.core.util.LogUtils;
+
+@ClientEndpoint
+public class WebSocketClientEndpoint {
+
+    Session userSession = null;
+    private MessageHandler messageHandler;
+
+    /**
+     * Callback hook for Connection open events.
+     *
+     * @param userSession the userSession which is opened.
+     */
+    @OnOpen
+    public void onOpen(Session userSession) {
+    	LogUtils.info("opening websocket");
+        this.userSession = userSession;
+    }
+
+    /**
+     * Callback hook for Connection close events.
+     *
+     * @param userSession the userSession which is getting closed.
+     * @param reason the reason for connection close
+     */
+    @OnClose
+    public void onClose(Session userSession, CloseReason reason) {
+    	LogUtils.info("closing websocket");
+        this.userSession = null;
+    }
+
+    /**
+     * Callback hook for Message Events. This method will be invoked when a client send a message.
+     *
+     * @param message The text message
+     */
+    @OnMessage
+    public void onMessage(String message) {
+        if (this.messageHandler != null) {
+            this.messageHandler.handleMessage(message);
+        }
+    }
+
+    /**
+     * register message handler
+     *
+     * @param msgHandler
+     */
+    public void addMessageHandler(MessageHandler msgHandler) {
+        this.messageHandler = msgHandler;
+    }
+
+    /**
+     * Send a message.
+     *
+     * @param message
+     */
+    public void sendMessage(String message) {
+        this.userSession.getAsyncRemote().sendText(message);
+    }
+
+    /**
+     * Message handler.
+     *
+     * @author Jiji_Sasidharan
+     */
+    public static interface MessageHandler {
+
+        public void handleMessage(String message);
+    }
+}
--- freeplane_plugin_collaboration_client/src/main/java/org/freeplane/plugin/collaboration/client/websocketserver/WebSocketServer.java
+++ freeplane_plugin_collaboration_client/src/main/java/org/freeplane/plugin/collaboration/client/websocketserver/WebSocketServer.java
@@ -1,6 +1,13 @@
 package org.freeplane.plugin.collaboration.client.websocketserver;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.function.Consumer;
+
+import javax.websocket.DeploymentException;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
 
 import org.freeplane.collaboration.event.batch.ImmutableMapId;
 import org.freeplane.collaboration.event.batch.MapCreateRequest;
@@ -11,6 +18,46 @@ import org.freeplane.plugin.collaboration.client.server.Server;
 import org.freeplane.plugin.collaboration.client.server.Subscription;
 
 public class WebSocketServer implements Server{
+	
+	final static String SERVER = "ws://localhost:8080/freeplane";
+//	final WebSocketClientEndpoint clientEndPoint;
+	final Session session;
+	
+	public WebSocketServer()
+	{
+		try {
+//			ClientManager client = ClientManager.createClient();
+//			clientEndPoint = new WebSocketClientEndpoint(new URI("ws://localhost:8080/freeplane"));
+			WebSocketContainer container = 
+					javax.websocket.ContainerProvider.getWebSocketContainer();
+			session = container.connectToServer(WebSocketClientEndpoint.class, new URI(SERVER));
+			/*
+			session.addMessageHandler(new MessageHandler() {
+				@Override
+		        public void handleMessage(String message) {
+		            LogUtils.info(String.format("received Message(%s)", message));
+		            final ObjectMapper jacksonObjectMapper = new JacksonConfiguration().objectMapper();
+		            try
+		            {
+		            	UpdateBlockCompleted updateBlock = jacksonObjectMapper.readValue(message, UpdateBlockCompleted.class);
+						logger.info(updateBlock.toString());
+		            }
+		            catch (IOException ex)
+		            {
+		            	logger.error("Error on deserialization", ex);
+		            }
+		        }
+		    });
+		    */
+		} catch (URISyntaxException | DeploymentException | IOException e) {
+			throw new RuntimeException(e);
+		}
+	    try {
+			session.getBasicRemote().sendText("WebSocket Hello World!");;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	private Consumer<UpdateBlockCompleted> consumer;
 
