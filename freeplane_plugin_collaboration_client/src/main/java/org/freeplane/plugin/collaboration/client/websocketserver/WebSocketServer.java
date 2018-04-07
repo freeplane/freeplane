0ab24d7ed497f4f5f33bceba85545d10b4b886b4
Felix Natter <fnatter@gmx.net>
2018-04-07 20:13:15 +0200

    simplify client endpoint

--- freeplane_plugin_collaboration_client/src/main/java/org/freeplane/plugin/collaboration/client/websocketserver/WebSocketServer.java
+++ freeplane_plugin_collaboration_client/src/main/java/org/freeplane/plugin/collaboration/client/websocketserver/WebSocketServer.java
@@ -5,7 +5,12 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.function.Consumer;
 
+import javax.websocket.ClientEndpoint;
+import javax.websocket.CloseReason;
 import javax.websocket.DeploymentException;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.WebSocketContainer;
 
@@ -14,50 +19,55 @@ import org.freeplane.collaboration.event.batch.MapCreateRequest;
 import org.freeplane.collaboration.event.batch.MapId;
 import org.freeplane.collaboration.event.batch.MapUpdateRequest;
 import org.freeplane.collaboration.event.batch.UpdateBlockCompleted;
+import org.freeplane.core.util.LogUtils;
 import org.freeplane.plugin.collaboration.client.server.Server;
 import org.freeplane.plugin.collaboration.client.server.Subscription;
 
+@ClientEndpoint
 public class WebSocketServer implements Server{
 	
+	// TODO: use Encoder/Decoder to automatically convert JSON
+	
+	Session userSession = null;
+	
 	final static String SERVER = "ws://localhost:8080/freeplane";
-//	final WebSocketClientEndpoint clientEndPoint;
-	final Session session;
 	
 	public WebSocketServer()
 	{
 		try {
-//			ClientManager client = ClientManager.createClient();
-//			clientEndPoint = new WebSocketClientEndpoint(new URI("ws://localhost:8080/freeplane"));
 			WebSocketContainer container = 
 					javax.websocket.ContainerProvider.getWebSocketContainer();
-			session = container.connectToServer(WebSocketClientEndpoint.class, new URI(SERVER));
-			/*
-			session.addMessageHandler(new MessageHandler() {
-				@Override
-		        public void handleMessage(String message) {
-		            LogUtils.info(String.format("received Message(%s)", message));
-		            final ObjectMapper jacksonObjectMapper = new JacksonConfiguration().objectMapper();
-		            try
-		            {
-		            	UpdateBlockCompleted updateBlock = jacksonObjectMapper.readValue(message, UpdateBlockCompleted.class);
-						logger.info(updateBlock.toString());
-		            }
-		            catch (IOException ex)
-		            {
-		            	logger.error("Error on deserialization", ex);
-		            }
-		        }
-		    });
-		    */
+			container.connectToServer(WebSocketServer.class, new URI(SERVER));
 		} catch (URISyntaxException | DeploymentException | IOException e) {
 			throw new RuntimeException(e);
 		}
-	    try {
-			session.getBasicRemote().sendText("WebSocket Hello World!");;
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
 	}
+	
+    @OnOpen
+    public void onOpen(Session userSession) {
+    	LogUtils.info("opening websocket");
+        this.userSession = userSession;
+        sendMessage("hello world from WebSocketServer.onOpen()!!");
+    }
+
+    @OnClose
+    public void onClose(Session userSession, CloseReason reason) {
+    	LogUtils.info("closing websocket");
+        this.userSession = null;
+    }
+
+    /**
+     * Message is received!
+     * 
+     * @param message
+     */
+    @OnMessage
+    public void onMessage(String message) {
+    }
+
+    public void sendMessage(String message) {
+        this.userSession.getAsyncRemote().sendText(message);
+    }
 
 	private Consumer<UpdateBlockCompleted> consumer;
 
