f8e427a5a052c17c0d351accbbb9a7616e835ed6
Dimitry Polivaev <dpolivaev@gmx.de>
2018-05-29 16:51:27 +0200

    create hidden map from template

--- freeplane_plugin_configurationservice/src/main/java/org/freeplane/plugin/configurationservice/ConfigurationSession.java
+++ freeplane_plugin_configurationservice/src/main/java/org/freeplane/plugin/configurationservice/ConfigurationSession.java
@@ -8,25 +8,26 @@ import org.freeplane.plugin.script.proxy.ScriptUtils;
 
 public class ConfigurationSession {
 
-	private Controller c; 
+	private Controller c;
+	private org.freeplane.plugin.script.proxy.Proxy.Map newHiddenMapFromTemplate;
 
 	public ConfigurationSession() {
-		c = ScriptUtils.c();		
+		c = ScriptUtils.c();
 	}
 
 	void start(String mindmapfile) {
 		File templateFile = new File(mindmapfile);
-		c.newMapFromTemplate(templateFile);
+		newHiddenMapFromTemplate = c.newHiddenMapFromTemplate(templateFile);
 	}
 
 	public void CreateAttributes(String nodeId, Map<String, Object> attributesMap) {
-		c.getSelected().getMap().node(nodeId).setAttributes(attributesMap);
+		newHiddenMapFromTemplate.node(nodeId).setAttributes(attributesMap);
 	}
 
 	public void update(String nodeId, String attributeName, int attributeValue) {
-		c.getSelected().getMap().node(nodeId).getAttributes().set(attributeName, attributeValue);
-		
+		newHiddenMapFromTemplate.node(nodeId).getAttributes().set(attributeName, attributeValue);
+
 	}
-	
+
 
 }
--- freeplane_plugin_configurationservice/src/main/java/org/freeplane/plugin/configurationservice/StartConfigurationSessionAction.java
+++ freeplane_plugin_configurationservice/src/main/java/org/freeplane/plugin/configurationservice/StartConfigurationSessionAction.java
@@ -3,22 +3,22 @@ package org.freeplane.plugin.configurationservice;
 import java.awt.event.ActionEvent;
 
 import org.freeplane.core.ui.AFreeplaneAction;
-import org.freeplane.features.map.MapModel;
-import org.freeplane.features.mode.Controller;
-import org.freeplane.view.swing.map.MapView;
 
 class StartConfigurationSessionAction extends AFreeplaneAction {
 	private static final String ACTION_NAME = "StartConfigurationSessionAction";
 	private static final long serialVersionUID = 1L;
-	private static final String mindMapFile = "C:\\neri\\mappementali\\HelloWorld.mm";
+
+	private static final String mindMapFile = "C:\\Users\\Dimitry\\Desktop\\Files\\HelloWorld.mm";
+//	private static final String mindMapFile = "C:\\neri\\mappementali\\HelloWorld.mm";
 	private static int port =0;
 	private ConfigurationSession configurationSession;
-	
+
 	public StartConfigurationSessionAction(ConfigurationSession configurationSession) {
 		super(ACTION_NAME);
 		this.configurationSession = configurationSession;
 	}
 
+	@Override
 	public void actionPerformed(final ActionEvent e) {
 		configurationSession.start(mindMapFile);
 	}
