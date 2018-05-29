3a531a2403bf42014e22cc5752396a15a06494e9
Andrea Buda <andrea.buda@configurators.eu>
2018-05-29 17:39:13 +0200

    Attribute Read Values

--- freeplane_plugin_configurationservice/src/main/java/org/freeplane/plugin/configurationservice/StartConfigurationSessionAction.java
+++ freeplane_plugin_configurationservice/src/main/java/org/freeplane/plugin/configurationservice/StartConfigurationSessionAction.java
@@ -1,8 +1,15 @@
 package org.freeplane.plugin.configurationservice;
 
 import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import org.freeplane.core.ui.AFreeplaneAction;
+import org.freeplane.core.util.LogUtils;
 import org.freeplane.features.map.MapModel;
 import org.freeplane.features.mode.Controller;
 import org.freeplane.view.swing.map.MapView;
@@ -13,6 +20,7 @@ class StartConfigurationSessionAction extends AFreeplaneAction {
 	private static final String mindMapFile = "C:\\neri\\mappementali\\HelloWorld.mm";
 	private static int port =0;
 	private ConfigurationSession configurationSession;
+	private String clientSentence;
 	
 	public StartConfigurationSessionAction(ConfigurationSession configurationSession) {
 		super(ACTION_NAME);
@@ -21,5 +29,21 @@ class StartConfigurationSessionAction extends AFreeplaneAction {
 
 	public void actionPerformed(final ActionEvent e) {
 		configurationSession.start(mindMapFile);
+		/*
+		ServerSocket welcomeSocket;
+		try {
+			welcomeSocket = new ServerSocket(6789);
+			  while (true) {
+				   Socket connectionSocket = welcomeSocket.accept();
+				   BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+				   DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+				   clientSentence = inFromClient.readLine();
+				   LogUtils.info("Received: " + clientSentence);
+				   outToClient.writeBytes("DONE");
+				  }
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}*/
 	}
 }
--- freeplane_plugin_configurationservice/src/main/java/org/freeplane/plugin/configurationservice/UpdateConfigurationAction.java
+++ freeplane_plugin_configurationservice/src/main/java/org/freeplane/plugin/configurationservice/UpdateConfigurationAction.java
@@ -1,10 +1,13 @@
 package org.freeplane.plugin.configurationservice;
 
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.freeplane.core.ui.AFreeplaneAction;
+import org.freeplane.core.util.LogUtils;
 import org.freeplane.features.map.MapModel;
 import org.freeplane.features.mode.Controller;
 import org.freeplane.view.swing.map.MapView;
@@ -21,6 +24,16 @@ class UpdateConfigurationAction extends AFreeplaneAction {
 
 	public void actionPerformed(final ActionEvent e) {
 		configurationSession.update("ID_1053277958", "a", 45);
+		
+		List<String> attributesList = new ArrayList<>();
+		attributesList.add("a");
+		attributesList.add("b");
+		attributesList.add("area");
+		
+		Map<String, Object> attributeMap = configurationSession.readValues("ID_1053277958", attributesList);
 
+		for (Map.Entry<String, Object> entry : attributeMap.entrySet()) {
+			LogUtils.info(entry.getKey() + " " + entry.getValue());
+		}
 	}
 }
