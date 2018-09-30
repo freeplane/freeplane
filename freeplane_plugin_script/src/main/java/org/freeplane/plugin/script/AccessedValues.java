10be67a694498eb768beeee7f77eae7bfe2cb3ac
Dimitry Polivaev <dpolivaev@gmx.de>
2018-09-30 12:12:22 +0200

    Simplify accessedValues

--- freeplane_plugin_script/src/main/java/org/freeplane/plugin/script/AccessedValues.java
+++ freeplane_plugin_script/src/main/java/org/freeplane/plugin/script/AccessedValues.java
@@ -1,51 +1,24 @@
 package org.freeplane.plugin.script;
 
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 import org.freeplane.features.attribute.Attribute;
 import org.freeplane.features.map.NodeModel;
 
 class AccessedValues {
-	private final Map<NodeModel, Set<Attribute>> accessedAttributes;
+	private final Map<Object, String> accessedValues;
 
 	AccessedValues() {
-		this.accessedAttributes = new HashMap<NodeModel, Set<Attribute>>();
-	}
-
-	private Set<Attribute> writeableAttributesOf(NodeModel nodeModel) {
-		Set<Attribute> attributes = accessedAttributes.get(nodeModel);
-		if(attributes == null) {
-			attributes = new HashSet<Attribute>();
-			accessedAttributes.put(nodeModel, attributes);
-		}
-		return attributes;
-	}
-
-	public Set<NodeModel> getAccessedNodes() {
-		return accessedAttributes != null ? accessedAttributes.keySet() : Collections.<NodeModel>emptySet();
-	}
-	public Set<Attribute> getAccessedAttributesOf(NodeModel node) {
-		if(accessedAttributes == null)
-			return Collections.emptySet();
-		else {
-			final Set<Attribute> attributes = accessedAttributes.get(node);
-			if(attributes == null)
-				return Collections.emptySet();
-			else
-				return attributes;
-		}
+		this.accessedValues = new HashMap<Object, String>();
 	}
 
 	void accessAttribute(NodeModel node, Attribute attribute) {
-		writeableAttributesOf(node).add(attribute);
+		accessedValues.put(attribute, node.createID());
 	}
 
 	public void accessValue(NodeModel node) {
-		if(! accessedAttributes.containsKey(node))
-			accessedAttributes.put(node, null);
+		final String id = node.createID();
+		accessedValues.put(id, id);
 	}
 }
