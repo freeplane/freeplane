7e5548a193eef2ff73480704ac0b68c30b02cfe3
Dimitry Polivaev <dpolivaev@gmx.de>
2018-10-05 14:11:47 +0200

    Change "evaluate all" formula action to work on not visible (hidden or folded) nodes too

--- freeplane_plugin_formula/src/main/java/org/freeplane/plugin/formula/EvaluateAllAction.java
+++ freeplane_plugin_formula/src/main/java/org/freeplane/plugin/formula/EvaluateAllAction.java
@@ -3,8 +3,11 @@ package org.freeplane.plugin.formula;
 import java.awt.event.ActionEvent;
 
 import org.freeplane.core.ui.AFreeplaneAction;
+import org.freeplane.features.attribute.NodeAttributeTableModel;
 import org.freeplane.features.map.MapModel;
+import org.freeplane.features.map.NodeModel;
 import org.freeplane.features.mode.Controller;
+import org.freeplane.plugin.script.FormulaUtils;
 import org.freeplane.view.swing.map.MapView;
 
 class EvaluateAllAction extends AFreeplaneAction {
@@ -17,7 +20,21 @@ class EvaluateAllAction extends AFreeplaneAction {
 	public void actionPerformed(final ActionEvent e) {
 		final MapModel map = Controller.getCurrentController().getMap();
 		org.freeplane.plugin.script.FormulaUtils.clearCache(map);
-		MapView mapView = (MapView)Controller.getCurrentController().getMapViewManager().getMapViewComponent();
-		mapView.getRoot().updateAll();
+		evaluateAll(map.getRootNode());
+	}
+
+	private void evaluateAll(NodeModel node) {
+		evaluateObject(node, node.getUserObject());
+		node.getExtension(NodeAttributeTableModel.class).getAttributes().stream().forEach(a -> evaluateObject(node, a));
+		node.getChildren().stream().forEach(this::evaluateAll);
+	}
+
+	private void evaluateObject(NodeModel node, Object userObject) {
+		try {
+			if (FormulaUtils.containsFormula(userObject)){
+				FormulaUtils.evalIfScript(node, (String) userObject);
+			}
+		} catch (Exception e) {
+		}
 	}
 }
