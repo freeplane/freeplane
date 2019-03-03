333f15f176d6a539853ab353c35ee00122e73f9d
Dimitry Polivaev <dpolivaev@gmx.de>
2019-03-03 20:34:56 +0100

    Try to catch unexpected NPE

--- freeplane/src/main/java/org/freeplane/view/swing/map/attribute/AttributePanelManager.java
+++ freeplane/src/main/java/org/freeplane/view/swing/map/attribute/AttributePanelManager.java
@@ -56,6 +56,7 @@ import org.freeplane.features.map.NodeModel;
 import org.freeplane.features.mode.Controller;
 import org.freeplane.features.mode.ModeController;
 import org.freeplane.view.swing.map.MainView;
+import org.freeplane.view.swing.map.NodeView;
 
 /**
  * @author Dimitry Polivaev
@@ -93,7 +94,13 @@ public class AttributePanelManager{
             if (mainView == null)
                 return;
             AttributeController.getController(modeController).createAttributeTableModel(node);
-            attributeView = new AttributeView(mainView.getNodeView(), false);
+            final NodeView nodeView = mainView.getNodeView();
+            // it is actually impossible but it happens
+            if(nodeView == null)  {
+            	LogUtils.severe(new RuntimeException("Node view null for mainView" + String.valueOf(mainView) + " and node " + String.valueOf(node)));
+            	return;
+            }
+			attributeView = new AttributeView(nodeView, false);
             Box buttonBox = new Box(axis);
             buttonBox.setAlignmentX(0.5f);
             tablePanel.add(buttonBox);
