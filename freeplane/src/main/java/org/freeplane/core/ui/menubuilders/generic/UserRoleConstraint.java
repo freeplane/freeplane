c0fe521beb70c39813e5f4ae4beb4fd762d7bb49
Dimitry Polivaev <dpolivaev@gmx.de>
2019-12-22 20:18:38 +0100

    user role constraints

---
+++ freeplane/src/main/java/org/freeplane/core/ui/menubuilders/generic/UserRoleConstraint.java
@@ -0,0 +1,39 @@
+/*
+ *  Freeplane - mind map editor
+ *  Copyright (C) 2019 dimitry
+ *
+ *  This file author is dimitry
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.freeplane.core.ui.menubuilders.generic;
+
+/**
+ * @author Dimitry Polivaev
+ * Dec 21, 2019
+ */
+public enum UserRoleConstraint {
+	EXPERT(false, true), EDITOR(true, false), EXPERT_EDITOR(true, true);
+	private final boolean editorRequired;
+	private final boolean expertRequired;
+	private UserRoleConstraint(boolean editorRequired, boolean expertRequired) {
+		this.editorRequired = editorRequired;
+		this.expertRequired = expertRequired;
+	}
+	
+	public boolean test(boolean isEditor, boolean isExpert) {
+		return (! editorRequired || isEditor) && (! expertRequired || isExpert);
+	}
+	
+}
