a56736ada83a86b9e68b3f8a25195f71a6d35092
dpolivaev <dpolivaev>
2020-03-19 17:10:48 +0100

    #2749 keyboard shortcuts for selected actions not working on MacOS

--- freeplane/src/main/java/org/freeplane/core/ui/components/ActionToggleButtonModel.java
+++ freeplane/src/main/java/org/freeplane/core/ui/components/ActionToggleButtonModel.java
@@ -1,10 +1,14 @@
 package org.freeplane.core.ui.components;
 
+import java.awt.AWTEvent;
+import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
+import java.awt.event.ItemEvent;
 
 import javax.swing.JToggleButton;
 
 import org.freeplane.core.ui.IFreeplaneAction;
+import org.freeplane.core.util.Compat;
 
 class ActionToggleButtonModel extends JToggleButton.ToggleButtonModel {
 	/**
@@ -36,6 +40,14 @@ class ActionToggleButtonModel extends JToggleButton.ToggleButtonModel {
 
 	@Override
     protected void fireActionPerformed(ActionEvent e) {
+		if(Compat.isMacOsX()) {
+			AWTEvent currentEvent = EventQueue.getCurrentEvent();
+			if (currentEvent instanceof ItemEvent
+					&& (((ItemEvent) currentEvent).getStateChange() == ItemEvent.SELECTED)
+					== isSelected()) {
+				return;
+			}
+		}
 		boolean setSelectedWasIgnored = ignoreSetSelected;
 		ignoreSetSelected = false;
 	    try {
