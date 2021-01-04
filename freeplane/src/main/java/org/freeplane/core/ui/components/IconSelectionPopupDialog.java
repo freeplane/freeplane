7d2d3571f39ff0fc6f168ffb67db55974e9ee8d7
Dimitry Polivaev <dpolivaev@gmx.de>
2021-01-04 18:15:00 +0100

    add icon search to icon selection

--- freeplane/src/main/java/org/freeplane/core/ui/components/IconSelectionPopupDialog.java
+++ freeplane/src/main/java/org/freeplane/core/ui/components/IconSelectionPopupDialog.java
@@ -21,9 +21,13 @@
 package org.freeplane.core.ui.components;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
@@ -32,18 +36,23 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
-import javax.swing.BorderFactory;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.WindowConstants;
 import javax.swing.border.BevelBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 import org.freeplane.core.resources.ResourceController;
 import org.freeplane.core.util.TextUtils;
@@ -52,14 +61,19 @@ import org.freeplane.features.icon.factory.IconFactory;
 
 public class IconSelectionPopupDialog extends JDialog implements KeyListener, MouseListener {
 
-	/**
+	private static final BevelBorder USUAL = new BevelBorder(BevelBorder.RAISED);
+    private static final BevelBorder HIGHLIGHTED =  new BevelBorder(BevelBorder.LOWERED, Color.CYAN,
+            Color.CYAN);
+    /**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private static String lastSearchText = "";
 	final private JLabel descriptionLabel;
 	final private List<JLabel> iconLabels;
 	final private JPanel iconPanel = new JPanel();
 	final private List<? extends IconDescription> icons;
+	private final JTextField filterTextField;
 	private int mModifiers;
 	final private int numOfIcons;
 	private int result;
@@ -67,7 +81,8 @@ public class IconSelectionPopupDialog extends JDialog implements KeyListener, Mo
 
 	public IconSelectionPopupDialog(final Frame frame, final List<? extends IconDescription> icons) {
 		super(frame, TextUtils.getText("select_icon"));
-		getContentPane().setLayout(new BorderLayout());
+		Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
 		this.icons = icons;
 		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
 		addWindowListener(new WindowAdapter() {
@@ -87,18 +102,32 @@ public class IconSelectionPopupDialog extends JDialog implements KeyListener, Mo
 		for (int i = 0; i < numOfIcons; ++i) {
 			final IconDescription icon = icons.get(i);
 			JLabel label = new JLabel(icon.getActionIcon());
+			label.putClientProperty(IconDescription.class, icon);
 			iconLabels.add(label);
             iconPanel.add(label);
-            label.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+            label.setBorder(USUAL);
             label.addMouseListener(this);
 		}
 		Dimension preferredSize = iconPanel.getPreferredSize();
 		JScrollPane scrollPane = new JScrollPane(iconPanel, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, 
 		        ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPane.setPreferredSize(new Dimension(preferredSize.width, preferredSize.width / 2));
-        getContentPane().add(scrollPane, BorderLayout.CENTER);
+		filterTextField = setupFilterTextField_and_KeyListener();
+		addWindowFocusListener(new WindowFocusListener() {
+            @Override
+            public void windowLostFocus(WindowEvent e) {
+            }
+            
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+                filterTextField.requestFocusInWindow();
+                removeWindowFocusListener(this);
+            }
+        });
+        contentPane.add(filterTextField, BorderLayout.NORTH);
+        contentPane.add(scrollPane, BorderLayout.CENTER);
 		descriptionLabel = new JLabel(" ");
-		getContentPane().add(descriptionLabel, BorderLayout.SOUTH);
+		contentPane.add(descriptionLabel, BorderLayout.SOUTH);
 		JLabel firstIcon = iconLabels.get(0);
         final JLabel label = firstIcon;
         selected = label;
@@ -106,6 +135,133 @@ public class IconSelectionPopupDialog extends JDialog implements KeyListener, Mo
 		addKeyListener(this);
 		pack();
 	}
+	
+	   private JTextField setupFilterTextField_and_KeyListener() {
+	        JTextField filterTextField = new JTextField();
+	        filterTextField.setText(lastSearchText);
+	        filterTextField.getDocument().addDocumentListener(new DocumentListener() {
+                
+                @Override
+                public void removeUpdate(DocumentEvent e) {
+                    filterIcons();
+                }
+                
+                @Override
+                public void insertUpdate(DocumentEvent e) {
+                    filterIcons();
+                }
+                
+                @Override
+                public void changedUpdate(DocumentEvent e) {
+                    filterIcons();
+                }
+            });
+	        filterTextField.addKeyListener(new KeyListener() {
+	            public void keyPressed(KeyEvent keyEvent) {
+	                String filterText = filterTextField.getText();
+	                if (filterText.length() == 0) {
+	                    if (keyEvent.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+	                        IconSelectionPopupDialog.this.keyPressed(keyEvent);
+	                    }
+	                }
+	            }
+
+	            public void keyReleased(KeyEvent keyEvent) {
+	                boolean passCharOnToDialog = false;
+	                String filterText = filterTextField.getText();
+	                lastSearchText = filterText;
+	                if (!filterText.startsWith("/")) {
+	                    filterTextField.setCaretPosition(filterText.length());
+	                }
+	                if (filterText.startsWith(".") && filterText.length() == 2) {
+	                    filterTextField.setText("");
+	                    passCharOnToDialog = true;
+	                } else {
+	                    char keyChar = keyEvent.getKeyChar();
+	                    switch (keyChar) {
+	                    case KeyEvent.VK_ESCAPE:
+	                        if (filterText.length() > 0) {
+	                            // Consume the Esc here, clear the filterTextField.
+	                            filterTextField.setText("");
+	                            keyEvent.consume();
+	                            break;
+	                        }
+	                        // else fall through to IconSelectionPopupDialog.this.keyPressed(keyEvent) below
+	                    case KeyEvent.VK_LEFT:
+	                    case KeyEvent.VK_RIGHT:
+	                    case KeyEvent.VK_UP:
+	                    case KeyEvent.VK_DOWN:
+	                    case KeyEvent.VK_ENTER:
+	                    case KeyEvent.CHAR_UNDEFINED:
+	                        passCharOnToDialog = true;
+	                        break;
+	                    }
+	                }
+	                if (passCharOnToDialog) {
+	                    // Pass on to this dialog's keyPressed(...) method.
+	                    IconSelectionPopupDialog.this.keyPressed(keyEvent);
+	                    return;
+	                } else {
+	                    if (!filterText.startsWith(".")) {
+	                        iconPanel.revalidate();
+	                        iconPanel.repaint();
+	                    }
+	                }
+	            }
+
+	            public void keyTyped(KeyEvent keyEvent) {
+	            }
+	        });
+	        return filterTextField;
+	    }
+
+	    private void filterIcons() {
+	        String filterText = filterTextField.getText().toLowerCase();
+	        Pattern regex = null;
+
+	        if (filterText.trim().length() > 0) {
+	            if (filterText.startsWith("/") && filterText.trim().length() >= 2) {
+	                regex = Pattern.compile(filterText.substring(1).trim());
+	            }
+
+	            for (JLabel label :iconLabels) {
+                    IconDescription iconDesctiprion = (IconDescription) label.getClientProperty(IconDescription.class);
+                    String iconLabel = iconDesctiprion.getTranslatedDescription();;
+	                if (iconLabel.startsWith("icon_")) {
+	                    iconLabel = iconLabel.substring(5);
+	                }
+	                boolean matches = false;
+	                if (filterText.startsWith("/")) {
+	                    matches = regex.matcher(iconLabel).matches();
+	                } else {
+	                    if (filterText.contains(" ")) {
+	                        matches = true;
+	                        StringTokenizer tokenizer = new StringTokenizer(filterText);
+	                        while (tokenizer.hasMoreTokens()) {
+	                            String token = tokenizer.nextToken();
+	                            matches = matches && iconLabel.toLowerCase().contains(token);
+	                            if (!matches) {
+	                                break;
+	                            }
+	                        }
+	                    } else {
+	                        matches = iconLabel.toLowerCase().contains(filterText);
+	                    }
+	                }
+	                label.setVisible(matches);
+	            }
+	        }
+	        else {
+	            for (Component component : iconPanel.getComponents()) {
+	                component.setVisible(true);
+	            }
+	        }
+	        if(selected == null && ! selected.isVisible()) {
+	            select(new Point(0, 0));
+	            if(selected != null)
+	                selected.scrollRectToVisible(new Rectangle());
+	        }
+	    }
 
 	private void addIcon(final int pModifiers) {
 		result =  iconLabels.indexOf(selected);
@@ -129,6 +285,8 @@ public class IconSelectionPopupDialog extends JDialog implements KeyListener, Mo
 	}
 
 	private void cursorDown() {
+	    if(selected == null)
+	        return;
 		final Point newPosition = new Point(selected.getX(), selected.getY() + selected.getWidth() + 1);
 		int newIndex = findIndex(newPosition);
 		if (newIndex >= 0) {
@@ -137,18 +295,28 @@ public class IconSelectionPopupDialog extends JDialog implements KeyListener, Mo
 	}
 
 	private void cursorLeft() {
-	    int selectedIndex = iconLabels.indexOf(selected) - 1;
-	    if(selectedIndex >= 0)
-	        select(selectedIndex);
+        if(selected == null)
+            return;
+        final Point newPosition = new Point(selected.getX() - 1, selected.getY());
+        int newIndex = findIndex(newPosition);
+        if (newIndex >= 0) {
+            select(newIndex);
+        }
 	}
 
 	private void cursorRight() {
-        int selectedIndex = iconLabels.indexOf(selected) + 1;
-        if(selectedIndex < iconLabels.size())
-            select(selectedIndex);
+        if(selected == null)
+            return;
+        final Point newPosition = new Point(selected.getX() + selected.getHeight() + 1, selected.getY());
+        int newIndex = findIndex(newPosition);
+        if (newIndex >= 0) {
+            select(newIndex);
+        }
 	}
 
 	private void cursorUp() {
+        if(selected == null)
+            return;
         final Point newPosition = new Point(selected.getX(), selected.getY() - 1);
         int newIndex = findIndex(newPosition);
         if (newIndex >= 0) {
@@ -207,7 +375,7 @@ public class IconSelectionPopupDialog extends JDialog implements KeyListener, Mo
     }
 
     private void highlight(JLabel label) {
-        label.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+        label.setBorder(HIGHLIGHTED);
     }
 
 	/*
@@ -308,25 +476,32 @@ public class IconSelectionPopupDialog extends JDialog implements KeyListener, Mo
 
 	private void select(final Point location) {
 	    int index = findIndex(location);
-		select(index);
+	    select(index);
 	}
 
-    private void select(final int index) {
-        unhighlight(this.selected);
-        JLabel newSelected = iconLabels.get(index);
-        this.selected = newSelected;
-		highlight(newSelected);
-		final IconDescription iconInformation = icons.get(index);
-		final String keyStroke = ResourceController.getResourceController().getProperty(iconInformation.getShortcutKey());
-		if (keyStroke != null) {
-			descriptionLabel.setText(iconInformation.getTranslatedDescription() + ", " + keyStroke);
-		}
-		else {
-			descriptionLabel.setText(iconInformation.getTranslatedDescription());
-		}
-    }
-
+	private void select(final int index) {
+	    unhighlight(this.selected);
+	    final String message;
+	    if(index >= 0) {
+	        JLabel newSelected = iconLabels.get(index);
+	        this.selected = newSelected;
+	        highlight(newSelected);
+	        final IconDescription iconInformation = icons.get(index);
+	        final String keyStroke = ResourceController.getResourceController().getProperty(iconInformation.getShortcutKey());
+	        if (keyStroke != null) {
+	            message = iconInformation.getTranslatedDescription() + ", " + keyStroke;
+	        }
+	        else {
+	            message = iconInformation.getTranslatedDescription();
+	        }
+	    }
+	    else {
+	        this.selected = null;
+	        message = "";
+	    }
+	    descriptionLabel.setText(message);
+	}
 	private void unhighlight(final JLabel label) {
-	    label.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+	    label.setBorder(USUAL);
 	}
 }
