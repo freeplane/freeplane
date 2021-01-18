7bb3b7d56515eca9781bdf3a563c62c27a1d0622
Dimitry Polivaev <dpolivaev@gmx.de>
2021-01-18 14:24:01 +0100

    fix LatexRenderer

--- freeplane_plugin_latex/src/main/java/org/freeplane/plugin/latex/LatexRenderer.java
+++ freeplane_plugin_latex/src/main/java/org/freeplane/plugin/latex/LatexRenderer.java
@@ -99,46 +99,46 @@ public class LatexRenderer extends AbstractContentTransformer implements IEditBa
 		if(! (content instanceof String))
 			return null;
 		MNoteController noteController = MNoteController.getController();
-		String nodeFormat;
+		String contentType;
 		if (nodeProperty instanceof NodeModel) {
 		    if (! textController.isTextFormattingDisabled(node)) {
-		        nodeFormat = textController.getNodeFormat(node);
+		        contentType = textController.getNodeFormat(node);
 		    } else
 		        return  null;
 		} else if (nodeProperty instanceof DetailModel) {
             String detailsContentType = textController.getDetailsContentType(node);
             if (LatexRenderer.LATEX_FORMAT.equals(detailsContentType)
                     || TextController.CONTENT_TYPE_AUTO.equals(detailsContentType)) {
-                nodeFormat = LatexRenderer.LATEX_FORMAT;
+                contentType = detailsContentType;
             } else
                 return  null;
         } else if (nodeProperty instanceof NoteModel) {
             String noteContentType = noteController.getNoteContentType(node);
             if (LatexRenderer.LATEX_FORMAT.equals(noteContentType)
                     || TextController.CONTENT_TYPE_AUTO.equals(noteContentType)) {
-                nodeFormat = LatexRenderer.LATEX_FORMAT;
+                contentType = noteContentType;
             } else
                 return  null;
         } else
 		    return  null;
 		String plainOrHtmlText = (String) content;
 		String text = HtmlUtils.htmlToPlain(plainOrHtmlText);
-		String latexText = getLatexText(text, nodeFormat, targetMode);
+		String latexText = getLatexText(text, contentType, targetMode);
 		return latexText;
 	}
 	
-	private String getLatexText(final String nodeText, final String nodeFormat, final TargetMode mode)
+	private String getLatexText(final String nodeText, final String contentType, final TargetMode mode)
 	{
 		boolean includePrefix = mode == TargetMode.FOR_EDITOR;
 
 		if(startsWithPrefix(nodeText, LATEX)){
 			return includePrefix ? nodeText : nodeText.substring(LATEX.length() + 1);
 		}
-		else if(LatexRenderer.LATEX_FORMAT.equals(nodeFormat)){
+		else if(LatexRenderer.LATEX_FORMAT.equals(contentType)){
 			return nodeText;
 		} else if(startsWithPrefix(nodeText, UNPARSED_LATEX) && mode == TargetMode.FOR_EDITOR) {
 			return nodeText;
-		} else if(LatexRenderer.UNPARSED_LATEX_FORMAT.equals(nodeFormat) && mode == TargetMode.FOR_EDITOR) {
+		} else if(LatexRenderer.UNPARSED_LATEX_FORMAT.equals(contentType) && mode == TargetMode.FOR_EDITOR) {
 			return nodeText;
 		} else {
 			return null;
