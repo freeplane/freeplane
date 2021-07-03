088e284e042bee5f59addb71fc21da3c583ac3c4
Dimitry Polivaev <dpolivaev@gmx.de>
2021-07-03 13:30:53 +0200

    #2971 Details don't respect max node width

--- freeplane/src/main/java/org/freeplane/core/ui/components/html/SynchronousScaledEditorKit.java
+++ freeplane/src/main/java/org/freeplane/core/ui/components/html/SynchronousScaledEditorKit.java
@@ -1,10 +1,13 @@
 package org.freeplane.core.ui.components.html;
 
+import javax.swing.SizeRequirements;
 import javax.swing.text.Element;
 import javax.swing.text.View;
 import javax.swing.text.ViewFactory;
 import javax.swing.text.html.HTMLEditorKit;
 import javax.swing.text.html.ImageView;
+import javax.swing.text.html.InlineView;
+import javax.swing.text.html.ParagraphView;
 
 @SuppressWarnings("serial")
 public class SynchronousScaledEditorKit extends ScaledEditorKit {
@@ -15,8 +18,43 @@ public class SynchronousScaledEditorKit extends ScaledEditorKit {
 			synchronousFactory = new HTMLEditorKit.HTMLFactory(){
 				public View create(Element elem) {
 					View view = super.create(elem);
+                    if(view instanceof InlineView){ 
+                        return new InlineView(elem){ 
+                            public View breakView(int axis, int p0, float pos, float len) { 
+                                View fragment = super.breakView(axis, p0, pos, len);
+                                if(this != fragment)
+                                    return fragment;
+                                if(axis == View.X_AXIS) { 
+                                    checkPainter(); 
+                                    int p1 = getGlyphPainter().getBoundedPosition(this, p0, pos, len); 
+                                    if(p0 == getStartOffset() && p1 == getEndOffset()) { 
+                                        return this; 
+                                    }
+                                    return createFragment(p0, p1); 
+                                } 
+                                return this; 
+                              } 
+                          }; 
+                    } 
+                    else if (view instanceof ParagraphView) { 
+                        return new ParagraphView(elem) { 
+                            protected SizeRequirements calculateMinorAxisRequirements(int axis, SizeRequirements r) { 
+                                if (r == null) { 
+                                      r = new SizeRequirements(); 
+                                } 
+                                float pref = layoutPool.getPreferredSpan(axis); 
+                                float min = layoutPool.getMinimumSpan(axis); 
+                                // Don't include insets, Box.getXXXSpan will include them. 
+                                  r.minimum = (int)min; 
+                                  r.preferred = Math.max(r.minimum, (int) pref); 
+                                  r.maximum = Integer.MAX_VALUE; 
+                                  r.alignment = 0.5f; 
+                                return r; 
+                              } 
 
-					if (view instanceof ImageView) {
+                          }; 
+                      } 
+                    else if (view instanceof ImageView) {
 						((ImageView)view).setLoadsSynchronously(true);
 					}
 					return view;
