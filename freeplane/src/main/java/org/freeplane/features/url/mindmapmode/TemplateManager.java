4158fd3605a8fb44f362997d134a768b783739cc
Dimitry Polivaev <dpolivaev@gmx.de>
2021-08-01 13:21:02 +0200

    refactor, add methods

--- freeplane/src/main/java/org/freeplane/features/url/mindmapmode/TemplateManager.java
+++ freeplane/src/main/java/org/freeplane/features/url/mindmapmode/TemplateManager.java
@@ -2,36 +2,94 @@ package org.freeplane.features.url.mindmapmode;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.TreeSet;
 
 import org.freeplane.core.resources.ResourceController;
+import org.freeplane.core.util.LogUtils;
 
 public class TemplateManager {
     public static final TemplateManager INSTANCE = new TemplateManager();
 
 
-    File templateFile(final String userDefinedTemplateFilePath) {
-        final ResourceController resourceController = ResourceController.getResourceController();
-        final File userDefinedTemplateFile = new File(userDefinedTemplateFilePath);
-        if (userDefinedTemplateFile.isAbsolute() && userDefinedTemplateFile.exists()
-                && !userDefinedTemplateFile.isDirectory()) {
-            return userDefinedTemplateFile;
+    File existingTemplateFile(final String filePath) {
+        final File userDefinedTemplateFile = new File(filePath);
+        if (userDefinedTemplateFile.isAbsolute()
+                && userDefinedTemplateFile.exists() && !userDefinedTemplateFile.isDirectory()) {
+                return userDefinedTemplateFile;
         }
-        for (final String filePath : new String[] { userDefinedTemplateFilePath,
-                resourceController.getDefaultProperty(MFileManager.STANDARD_TEMPLATE) }) {
-            for (final File userTemplates : new File[] { defaultUserTemplateDir(), defaultStandardTemplateDir() }) {
-                if (userTemplates.isDirectory()) {
-                    final File userStandard = new File(userTemplates, filePath);
-                    if (userStandard.exists() && !userStandard.isDirectory()) {
-                        if (!filePath.equals(userDefinedTemplateFilePath))
-                            resourceController.setProperty(MFileManager.STANDARD_TEMPLATE, filePath);
-                        return userStandard;
-                    }
+        else {
+            File templateFile = templateFile(filePath);
+            if(templateFile.exists())
+                return templateFile;
+        }
+        return fallbackTemplate();
+    }
+
+
+    File writeableTemplateFile(final String filePath) {
+        final File userDefinedTemplateFile = new File(filePath);
+        if (userDefinedTemplateFile.isAbsolute()) {
+                return userDefinedTemplateFile;
+        }
+        else {
+            return new File(defaultUserTemplateDir(), filePath);
+        }
+    }
+    
+    private File fallbackTemplate() {
+        final ResourceController resourceController = ResourceController.getResourceController();
+        String fallback = resourceController.getDefaultProperty(MFileManager.STANDARD_TEMPLATE);
+        File fallbackFile = templateFile(fallback);
+        Objects.requireNonNull(fallbackFile);
+        return fallbackFile;
+    }
+
+    private File templateFile(final String filePath) {
+        File defaultUserTemplateDir = defaultUserTemplateDir();
+        for (final File userTemplates : new File[] { defaultUserTemplateDir, defaultStandardTemplateDir() }) {
+            if (userTemplates.isDirectory()) {
+                final File fullPath = new File(userTemplates, filePath);
+                if (fullPath.exists() && !fullPath.isDirectory()) {
+                    return fullPath;
                 }
             }
         }
-        return null;
+        return new File(defaultUserTemplateDir, filePath);
+    }
+    
+    URI normalizeTemplateLocation(URI locationUri) {
+        if(! "file".equals(locationUri.getScheme()))
+            return locationUri;
+        String location = locationUri.toString();
+        String userTemplateLocation = defaultUserTemplateDir().toURI().toString();
+        String normalizedLocation;
+        if(location.startsWith(userTemplateLocation)) {
+            normalizedLocation = "template:/" + location.substring(userTemplateLocation.length());
+        } else {
+            String standardTemplateLocation = defaultStandardTemplateDir().toURI().toString();
+            if(location.startsWith(standardTemplateLocation)) {
+                normalizedLocation = "template:/" + location.substring(standardTemplateLocation.length());
+            } 
+            else
+                return locationUri;
+        }
+        try {
+            return new URI(normalizedLocation);
+        } catch (URISyntaxException e) {
+            LogUtils.severe(e);
+            return locationUri;
+        }
+    }
+    
+    URI expandTemplateLocation (URI locationUri) {
+        if(! "template".equals(locationUri.getScheme()))
+            return locationUri;
+        String path = locationUri.getPath().substring(1);
+        return existingTemplateFile(path).toURI();
     }
 
     public File defaultUserTemplateDir() {
