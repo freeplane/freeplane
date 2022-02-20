93f8cfaac8b96677a36ffe5ee8375c561393e555
Dimitry Polivaev <dpolivaev@gmx.de>
2022-02-20 20:36:40 +0100

    Improve representation of transparent colors on color button

--- freeplane/src/main/java/org/freeplane/core/resources/components/JColorButton.java
+++ freeplane/src/main/java/org/freeplane/core/resources/components/JColorButton.java
@@ -4,8 +4,12 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Rectangle;
 import java.awt.RenderingHints;
+import java.awt.TexturePaint;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JButton;
 
@@ -18,6 +22,16 @@ class JColorButton extends JButton {
 
 	private static final long serialVersionUID = 1L;
 	private static final int COLOR_ICON_BORDER_SIZE = (int) (4 * UITools.FONT_SCALE_FACTOR);
+	private static final Paint TEXTURE = createTexture();
+	private static TexturePaint createTexture() {
+		int cellSize = COLOR_ICON_BORDER_SIZE;
+		BufferedImage image = new BufferedImage(cellSize, cellSize, BufferedImage.TYPE_INT_RGB);
+		Graphics graphics = image.getGraphics();
+		graphics.setColor(Color.WHITE);
+		graphics.fillRect(0, 0, cellSize/2, cellSize/2);
+		graphics.fillRect(cellSize/2, cellSize/2, cellSize/2, cellSize/2);
+		return new TexturePaint(image,  new Rectangle(cellSize, cellSize));
+	}
 	private Color color;
 	private String text;
 	int textWidth;
@@ -57,19 +71,31 @@ class JColorButton extends JButton {
 	}
 	private void paintColor(Graphics g) {
 	    if (color != null) {
-	    	calculateTextSize();
-	        final Color backgroundColor = ColorUtils.makeNonTransparent(color);
-	        g.setColor(backgroundColor);
-	        g.setFont(getFont());
-	        g.fillRect(COLOR_ICON_BORDER_SIZE , 
-	        		COLOR_ICON_BORDER_SIZE, 
-	        		getWidth() - COLOR_ICON_BORDER_SIZE*2, 
-	        		getHeight() - COLOR_ICON_BORDER_SIZE*2);
 	        Graphics2D g2 = (Graphics2D) g;
+	    	if(color.getAlpha() < 255){
+	    		g2.setPaint(TEXTURE);
+	    		g.fillRect(COLOR_ICON_BORDER_SIZE , 
+	    				COLOR_ICON_BORDER_SIZE, 
+	    				getWidth() - COLOR_ICON_BORDER_SIZE*2, 
+	    				getHeight() - COLOR_ICON_BORDER_SIZE*2);
+	    	}
+    		g.setColor(color);
+    		g.setFont(getFont());
+    		g.fillRect(COLOR_ICON_BORDER_SIZE , 
+    				COLOR_ICON_BORDER_SIZE, 
+    				getWidth() - COLOR_ICON_BORDER_SIZE*2, 
+    				getHeight() - COLOR_ICON_BORDER_SIZE*2);
 	        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+	    	calculateTextSize();
 			int xText = (getWidth() - textWidth) / 2;
-			int yText = (getHeight() + textHeight - COLOR_ICON_BORDER_SIZE) / 2;
+			int yText = (getHeight() + 3 * textHeight/4) / 2;
 	        /* Draw the Text */
+			final Color backgroundColor = ColorUtils.makeNonTransparent(color);
+			if(color.getAlpha() < 255){
+	    		g.setColor(backgroundColor);
+	    		g.fillRect(xText - COLOR_ICON_BORDER_SIZE/2,  (getHeight() - textHeight - COLOR_ICON_BORDER_SIZE)/2, 
+	    				textWidth + COLOR_ICON_BORDER_SIZE, textHeight +  COLOR_ICON_BORDER_SIZE);
+			}
 	        if(! getModel().isEnabled()) {
 	            g.setColor(backgroundColor.brighter());
 	            HiDPIUtils.drawStringUnderlineCharAtWithYCorrection( this, g2, text, -1, xText, yText);
