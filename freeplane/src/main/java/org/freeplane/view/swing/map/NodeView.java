8e72a535c4301170ea8e7110b4739ca7eb6f1c30
Dimitry Polivaev <dpolivaev@gmx.de>
2022-08-20 14:05:55 +0200

    refresh node view after side change

--- freeplane/src/main/java/org/freeplane/view/swing/map/NodeView.java
+++ freeplane/src/main/java/org/freeplane/view/swing/map/NodeView.java
@@ -907,7 +907,6 @@ public class NodeView extends JComponent implements INodeView {
 		if(property == Side.class) {
 			invalidateAll();
 			revalidate();
-			return;
 		}
 
 		// is node is not fully initialized, skip the rest.
