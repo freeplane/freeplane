488be0322653a997268ffe430409d883dfccec5a
Dimitry Polivaev <dpolivaev@gmx.de>
2022-11-18 14:33:46 +0100

    remove unused import

--- freeplane/src/main/java/org/freeplane/features/map/NodeModel.java
+++ freeplane/src/main/java/org/freeplane/features/map/NodeModel.java
@@ -31,7 +31,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import org.freeplane.api.ChildNodesLayout;
 import org.freeplane.api.ChildrenSides;
 import org.freeplane.core.extension.ExtensionContainer;
 import org.freeplane.core.extension.IExtension;
