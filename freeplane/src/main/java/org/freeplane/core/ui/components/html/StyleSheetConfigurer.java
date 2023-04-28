5956287aab60a40e09fe45eccadaf4f34075ac64
Dimitry Polivaev <dpolivaev@gmx.de>
2023-04-28 19:24:07 +0200

    fix java.util.ConcurrentModificationException
    	at java.base/java.util.LinkedHashMap$LinkedHashIterator.nextNode(Unknown Source)
    
    	at java.base/java.util.LinkedHashMap$LinkedKeyIterator.next(Unknown Source)
    
    	at java.base/java.util.Collections$3.nextElement(Unknown Source)
    
    	at org.freeplane.core.ui.components.html.StyleSheetConfigurer.resetStyles(StyleSheetConfigurer.java:12)
    
    	at org.freeplane.features.note.mindmapmode.NotePanel.updateStyleSheet(NotePanel.java:321)
    
    	at org.freeplane.features.note.mindmapmode.MNoteController.setDefaultStyle(MNoteController.java:300)
    
    	at org.freeplane.features.note.mindmapmode.NoteManager.updateEditor(NoteManager.java:111)
    
    	at org.freeplane.features.note.mindmapmode.NoteManager.onSelect(NoteManager.java:63)
    
    	at org.freeplane.features.mode.SelectionController.onSelect(SelectionController.java:64)

--- freeplane/src/main/java/org/freeplane/core/ui/components/html/StyleSheetConfigurer.java
+++ freeplane/src/main/java/org/freeplane/core/ui/components/html/StyleSheetConfigurer.java
@@ -1,27 +1,34 @@
 package org.freeplane.core.ui.components.html;
 
+import java.util.ArrayList;
 import java.util.Enumeration;
 
 import javax.swing.text.StyleContext;
 import javax.swing.text.html.StyleSheet;
 
 public class StyleSheetConfigurer {
-	public static void resetStyles(StyleSheet parent,  int keptChildCount) {
-		Enumeration<?> styleNames = parent.getStyleNames();
-		while(styleNames.hasMoreElements()) {
-			String ruleName = (String) styleNames.nextElement();
-			 if (!ruleName.equalsIgnoreCase(StyleContext.DEFAULT_STYLE))
-				 parent.removeStyle(ruleName);
-		}
-		StyleSheet[] styleSheets = parent.getStyleSheets();
-		if(styleSheets != null) {
+    public static void resetStyles(StyleSheet parent,  int keptChildCount) {
+        Enumeration<?> styleNamesEnumeration = parent.getStyleNames();
+        ArrayList<String> styleNames = new ArrayList<String>();
+        while(styleNamesEnumeration.hasMoreElements()) {
+            String styleName = (String) styleNamesEnumeration.nextElement();
+            if (!styleName.equalsIgnoreCase(StyleContext.DEFAULT_STYLE))
+                styleNames.add(styleName);
+        }
+
+        for(String styleName : styleNames) {
+            parent.removeStyle(styleName);
+        }
+
+        StyleSheet[] styleSheets = parent.getStyleSheets();
+        if(styleSheets != null) {
 			int removedSheetsCount = styleSheets.length - keptChildCount;
 			for(int i = 0; i < removedSheetsCount; i++ ) {
 				parent.removeStyleSheet(styleSheets[i]);
 			}
 		}
 	}
-	
+
 	public static StyleSheet createDefaultStyleSheet() {
 		StyleSheet styleSheet = new StyleSheet();
 		styleSheet.addRule("table {border: 0; border-spacing: 0;}");
