4322d4fcaf780e9062888f4066a1d0bf8c861662
Dimitry Polivaev <dpolivaev@gmx.de>
2023-06-18 14:15:39 +0200

    remove unused imports

--- freeplane/src/main/java/org/freeplane/features/attribute/AttributeSelection.java
+++ freeplane/src/main/java/org/freeplane/features/attribute/AttributeSelection.java
@@ -4,8 +4,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
-import org.freeplane.features.attribute.AttributeSelection.SelectedAttribute;
-
 public interface AttributeSelection {
 	public static final AttributeSelection EMPTY = new AttributeSelection() {
 		@Override
