41ff0177e5c9f61d0930d6c0554010ffba4902f3
dpolivaev <dpolivaev@gmx.de>
2023-09-15 17:53:17 +0200

    Issue #1370 Error opening the linked file if the file name or file path is not in English

--- freeplane/src/main/java/org/freeplane/main/application/Browser.java
+++ freeplane/src/main/java/org/freeplane/main/application/Browser.java
@@ -1,94 +1,31 @@
 package org.freeplane.main.application;
 
-import java.io.IOException;
-import java.text.MessageFormat;
+import java.awt.Desktop;
+import java.net.URI;
 
-import org.freeplane.core.resources.ResourceController;
-import org.freeplane.core.ui.components.UITools;
-import org.freeplane.core.util.Compat;
 import org.freeplane.core.util.Hyperlink;
-import org.freeplane.features.mode.Controller;
+import org.freeplane.core.util.LogUtils;
 
 public class Browser {
 	public void openDocument(final Hyperlink link) {
+		Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+		if (desktop == null || !desktop.isSupported(Desktop.Action.BROWSE)) {
+			LogUtils.warn("Opening URI is not supported by the OS");
+			return;
+		}
 		String uriString = link.toString();
 		final String UNC_PREFIX = "file:////";
-		if (uriString.startsWith(UNC_PREFIX)) {
-			uriString = "file://" + uriString.substring(UNC_PREFIX.length());
-		}
-		final String osName = System.getProperty("os.name");
-		if (osName.substring(0, 3).equals("Win")) {
-			String propertyString = "default_browser_command_windows";
-			if (osName.indexOf("9") != -1 || osName.indexOf("Me") != -1) {
-				propertyString += "_9x";
+		try {
+			URI uri;
+			if (uriString.startsWith(UNC_PREFIX)) {
+				uriString = "file://" + uriString.substring(UNC_PREFIX.length());
+				uri = new URI(uriString);
 			}
-			else {
-				propertyString += "_nt";
-			}
-			String[] command = null;
-			try {
-				final Object[] messageArguments = { uriString };
-				final MessageFormat formatter = new MessageFormat(ResourceController.getResourceController()
-				    .getProperty(propertyString));
-				final String browserCommand = formatter.format(messageArguments);
-				final String scheme = link.getScheme();
-                if (scheme.equals("file") || scheme.equals("smb")) {
-                    if(scheme.equals("smb")){
-                        uriString = Compat.smbUri2unc(link.getUri());
-                    }
-					if (System.getProperty("os.name").startsWith("Windows 2000"))
-						command = new String[] { "rundll32", "shell32.dll,ShellExec_RunDLL", uriString };
-					else
-	                    command = new String[] { "rundll32", "url.dll,FileProtocolHandler", uriString };
-				}
-				else if (uriString.startsWith("mailto:")) {
-					command = new String[] { "rundll32", "url.dll,FileProtocolHandler", uriString };
-				}
-				else {
-					Controller.exec(browserCommand);
-					return;
-				}
-				Controller.exec(command);
-			}
-			catch (final IOException x) {
-				UITools
-				    .errorMessage("Could not invoke browser.\n\nFreeplane executed the following statement on a command line:\n\""
-				            + command
-				            + "\".\n\nYou may look at the user or default property called '"
-				            + propertyString
-				            + "'.");
-				System.err.println("Caught: " + x);
-			}
-		}
-		else if (osName.startsWith("Mac OS")) {
-			String browserCommand = null;
-			try {
-				if(link.getScheme().equals("file"))
-					uriString = link.getUri().getPath();
-				browserCommand = ResourceController.getResourceController().getProperty("default_browser_command_mac");
-				Controller.exec(new String[]{browserCommand, uriString});
-			}
-			catch (final IOException ex2) {
-				UITools
-				    .errorMessage("Could not invoke browser.\n\nFreeplane executed the following statement on a command line:\n\""
-				            + browserCommand
-				            + "\".\n\nYou may look at the user or default property called 'default_browser_command_mac'.");
-				System.err.println("Caught: " + ex2);
-			}
-		}
-		else {
-			String browserCommand = null;
-			try {
-				browserCommand = ResourceController.getResourceController().getProperty("default_browser_command_other_os");
-				Controller.exec(new String[]{browserCommand, uriString});
-			}
-			catch (final IOException ex2) {
-				UITools
-				    .errorMessage("Could not invoke browser.\n\nFreeplane executed the following statement on a command line:\n\""
-				            + browserCommand
-				            + "\".\n\nYou may look at the user or default property called 'default_browser_command_other_os'.");
-				System.err.println("Caught: " + ex2);
-			}
+			else
+				uri = link.getUri();
+			desktop.browse(uri);
+		} catch (Exception e) {
+			LogUtils.severe("Can not open URI" + link, e);
 		}
 	}
 }
