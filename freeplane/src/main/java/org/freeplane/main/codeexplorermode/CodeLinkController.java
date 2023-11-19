57360bd587337277b87250cae168170c67b8b6f1
Dimitry Polivaev <dimitry.polivaev@unite.eu>
2023-11-19 13:35:23 +0100

    remove unused import

--- freeplane/src/main/java/org/freeplane/main/codeexplorermode/CodeLinkController.java
+++ freeplane/src/main/java/org/freeplane/main/codeexplorermode/CodeLinkController.java
@@ -13,7 +13,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
