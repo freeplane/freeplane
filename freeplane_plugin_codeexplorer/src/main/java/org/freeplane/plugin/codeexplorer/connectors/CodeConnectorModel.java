9633957bf9996bc479a5a87c9e12ea5c6e1a7342
Dimitry Polivaev <dimitry.polivaev@unite.eu>
2023-11-28 20:19:48 +0100

    generate hashcode and equals for CodeConnectorModel

--- freeplane_plugin_codeexplorer/src/main/java/org/freeplane/plugin/codeexplorer/connectors/CodeConnectorModel.java
+++ freeplane_plugin_codeexplorer/src/main/java/org/freeplane/plugin/codeexplorer/connectors/CodeConnectorModel.java
@@ -5,6 +5,8 @@
  */
 package org.freeplane.plugin.codeexplorer.connectors;
 
+import java.util.Objects;
+
 import org.freeplane.features.link.ConnectorModel;
 import org.freeplane.features.map.NodeModel;
 import org.freeplane.plugin.codeexplorer.dependencies.DependencyVerdict;
@@ -32,4 +34,26 @@ class CodeConnectorModel extends ConnectorModel {
     public DependencyVerdict dependencyVerdict() {
         return dependencyVerdict;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + Objects.hash(dependencyVerdict);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CodeConnectorModel other = (CodeConnectorModel) obj;
+        return dependencyVerdict == other.dependencyVerdict;
+    }
+
+
 }
