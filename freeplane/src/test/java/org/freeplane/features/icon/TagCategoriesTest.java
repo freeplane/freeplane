4fae51baa9b056bb270a6e54ec2f7b1f4c452f40
Dimitry Polivaev <dimitry.polivaev@unite.eu>
2024-08-24 14:30:50 +0200

    fix test

--- freeplane/src/test/java/org/freeplane/features/icon/TagCategoriesTest.java
+++ freeplane/src/test/java/org/freeplane/features/icon/TagCategoriesTest.java
@@ -22,23 +22,23 @@ public class TagCategoriesTest {
                     + " tag2\n"
                     + "  tag3\n"
                     + "tag4\n";
-    
+
             tagCategories.load(input);
-    
+
             assertThat(rootNode.getChildCount()).isEqualTo(3);
-    
+
             DefaultMutableTreeNode firstChild = (DefaultMutableTreeNode) rootNode.getChildAt(0);
             assertThat(((Tag) firstChild.getUserObject()).getContent()).isEqualTo("tag1");
-    
+
             DefaultMutableTreeNode secondChild = (DefaultMutableTreeNode) rootNode.getChildAt(1);
             assertThat(((Tag) secondChild.getUserObject()).getContent()).isEqualTo("tag4");
-    
+
             DefaultMutableTreeNode grandChild = (DefaultMutableTreeNode) firstChild.getChildAt(0);
-            assertThat(((Tag) grandChild.getUserObject()).getContent()).isEqualTo("tag2");
-    
+            assertThat(((Tag) grandChild.getUserObject()).getContent()).isEqualTo("tag1::tag2");
+
             DefaultMutableTreeNode greatGrandChild = (DefaultMutableTreeNode) grandChild.getChildAt(0);
-            assertThat(((Tag) greatGrandChild.getUserObject()).getContent()).isEqualTo("tag3");
-    
+            assertThat(((Tag) greatGrandChild.getUserObject()).getContent()).isEqualTo("tag1::tag2::tag3");
+
             assertThat(tagCategories.serialize()
             		.replaceAll("#.*", "")
             		.replace(System.lineSeparator(), "\n"))
